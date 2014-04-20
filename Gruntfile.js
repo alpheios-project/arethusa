0d818611597241659e7cab6f4a1bbfe2e4725bb4
Christof Sirk <christof.sirk@gmail.com>
2014-04-20 17:41:09 +0200

    Try to use protractor (not working yet)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,16 @@
 var srcFiles = 'app/**/*.js';
 var specFiles = 'spec/**/*.js';
+var specE2eFiles = 'spec-e2e/**/*.js';
 
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-protractor-webdriver');
+  grunt.loadNpmTasks('grunt-protractor-runner');
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     jasmine: {
       src: srcFiles,
       options: {
@@ -43,7 +47,34 @@ module.exports = function(grunt) {
           ],
         }
       },
+    },
+    protractor: {
+      options: {
+        keepAlive: true, // If false, the grunt process stops when the test fails.
+        noColor: false, // If true, protractor will not use colors in its output.
+        args: {
+          specs: [ specE2eFiles ],
+          capabilities: {
+            'browserName': 'chrome'
+          },
+          seleniumAddress: "127.0.0.1:4444/wd/hub",
+          // seleniumServerJar: ""
+          // Arguments passed to the command
+          framework: "jasmine",
+          jasmineNodeOpts: {
+            showColors: true, // Use colors in the command line report.
+          }
+        }
+      },
+    },
+    protractor_webdriver: {
+      startSelenium: {
+        options: {
+          command: 'webdriver-manager start'
+        },
+      },
     }
   });
+
   grunt.registerTask('default', ['karma:spec', 'jshint']);
 };
