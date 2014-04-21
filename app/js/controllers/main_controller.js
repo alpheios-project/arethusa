bb2340ae88fcb5dbbfea3b2bdadbbda6a52992e7
LFDM <1986gh@gmail.com>
2014-04-21 16:17:49 +0200

    Move state to main controller

--- app/js/controllers/main_controller.js
+++ app/js/controllers/main_controller.js
@@ -1,2 +1,10 @@
 annotationApp.controller('MainController', function($scope) {
+  $scope.tokens = [
+    { 'string' : 'Marcus', 'id' : '1' },
+    { 'string' : 'rosam', 'id' : '2' },
+    { 'string' : 'videt', 'id' : '3' },
+    { 'string' : '.', 'id' : '4' }
+  ];
+
+  $scope.selectedToken = ''
 });
--- app/js/controllers/text_controller.js
+++ app/js/controllers/text_controller.js
@@ -1,12 +1,4 @@
 var annotationApp = angular.module('annotationApp');
 
 annotationApp.controller('TextController', function($scope) {
-  $scope.tokens = [
-    { 'string' : 'Marcus', 'id' : '1' },
-    { 'string' : 'rosam', 'id' : '2' },
-    { 'string' : 'videt', 'id' : '3' },
-    { 'string' : '.', 'id' : '4' }
-  ];
-
-  $scope.selectedToken = ''
 });
