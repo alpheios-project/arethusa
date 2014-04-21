8ed5ebff7d467915f13f3e5b605619bf7f47f29f
LFDM <1986gh@gmail.com>
2014-04-21 16:02:48 +0200

    Selection of tokens by a dropdown menu

--- app/js/controllers/text_controller.js
+++ app/js/controllers/text_controller.js
@@ -1,10 +1,13 @@
 var annotationApp = angular.module('annotationApp', []);
+angular.module('annotationApp');
 
 annotationApp.controller('TextController', function($scope) {
   $scope.tokens = [
-    { 'string' : 'Marcus' },
-    { 'string' : 'rosam' },
-    { 'string' : 'videt' },
-    { 'string' : '.' }
+    { 'string' : 'Marcus', 'id' : '1' },
+    { 'string' : 'rosam', 'id' : '2' },
+    { 'string' : 'videt', 'id' : '3' },
+    { 'string' : '.', 'id' : '4' }
   ];
+
+  $scope.selectedToken = ''
 });
