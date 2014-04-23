38adc67552510cce1ce0c47d529a3bd5f984481e
lichtr <robert.lichtensteiner@gmail.com>
2014-04-23 15:37:00 +0200

    Adds method to request local json file via .get.

--- app/js/controllers/morph_controller.js
+++ app/js/controllers/morph_controller.js
@@ -1,10 +1,8 @@
-annotationApp.controller('MorphController', function($scope) {
-  $scope.analyses = [
-    { 'id' : '1', 'pos' : 'noun' },
-    { 'id' : '2', 'pos' : 'noun' },
-    { 'id' : '3', 'pos' : 'pred' },
-    { 'id' : '4', 'pos' : 'punct' }
-  ];
+annotationApp.controller('MorphController', function($scope, $http) {
+  $http.get('./js/controllers/analyses.json')
+       .then(function(res){
+    $scope.analyses = res.data;
+  });
 
   $scope.currentAnalysis = function() {
     return $scope.analyses[$scope.selectedToken.id - 1];
