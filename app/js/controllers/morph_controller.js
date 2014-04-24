b0560957e5ccae89206561bee7a2a65b826b15c5
LFDM <1986gh@gmail.com>
2014-04-24 20:26:54 +0200

    Dynamically retrieve morphRetriever

--- app/js/controllers/morph_controller.js
+++ app/js/controllers/morph_controller.js
@@ -1,4 +1,7 @@
-annotationApp.controller('MorphController', function($scope, morphRetriever) {
+annotationApp.controller('MorphController', function($scope, configurator) {
+  var conf = configurator.conf_for('MorphController');
+  morphRetriever = configurator.getService(conf.retriever);
+
   morphRetriever.getAnalyses(function(res) {
     $scope.analyses = res.data;
   });
