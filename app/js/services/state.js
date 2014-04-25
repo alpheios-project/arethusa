01aa6eb1de699928ca2675979cd624323a292642
LFDM <1986gh@gmail.com>
2014-04-25 09:47:48 +0200

    Move state to service

---
+++ app/js/services/state.js
@@ -0,0 +1,21 @@
+annotationApp.service('state', function(configurator) {
+  var conf = configurator.conf_for('state');
+  tokenRetriever = configurator.getService(conf.retriever);
+
+  var tokens;
+  tokenRetriever.getData(function(res) {
+    console.log(res);
+    tokens = res;
+  });
+  this.tokens = tokens;
+
+  this.selectedToken = { id: '1' };
+
+  this.currentToken = function() {
+    return this.tokens[this.selectedToken.id - 1];
+  };
+
+  this.selectToken = function(id) {
+    this.selectedToken.id = id;
+  };
+});
