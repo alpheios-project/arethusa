066e7d2b852d67e9e03fb2c6dabf457601d3f05f
LFDM <1986gh@gmail.com>
2014-04-26 21:25:45 +0200

    Distinct between main and sub plugins

--- app/js/controllers/main_controller.js
+++ app/js/controllers/main_controller.js
@@ -3,11 +3,25 @@
 annotationApp.controller('MainController', function($scope, state, configurator) {
   var conf = configurator.conf_for('MainController');
 
-  $scope.state = state;
-  $scope.plugins = Object.keys(conf.plugins);
-  $scope.template = conf.template;
+  $scope.partitionPlugins = function() {
+    $scope.mainPlugins = [];
+    $scope.subPlugins = [];
+
+    angular.forEach(conf.plugins, function(el, name) {
+      if (el.main) {
+        $scope.mainPlugins.push(name);
+      } else {
+        $scope.subPlugins.push(name);
+      }
+    });
+  };
 
   $scope.addPlugin = function() {
     $scope.plugins.push("comment");
   };
+
+  $scope.state = state;
+  $scope.plugins = Object.keys(conf.plugins);
+  $scope.partitionPlugins();
+  $scope.template = conf.template;
 });
