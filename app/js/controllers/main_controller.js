f7ba703ce179d06a759a9c1f32f0ac0290427bf8
LFDM <1986gh@gmail.com>
2014-04-26 21:13:25 +0200

    Inline plugin configuration

--- app/js/controllers/main_controller.js
+++ app/js/controllers/main_controller.js
@@ -4,7 +4,7 @@ annotationApp.controller('MainController', function($scope, state, configurator)
   var conf = configurator.conf_for('MainController');
 
   $scope.state = state;
-  $scope.plugins = conf.plugins;
+  $scope.plugins = Object.keys(conf.plugins);
   $scope.template = conf.template;
 
   $scope.addPlugin = function() {
