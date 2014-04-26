7d079d5d8b723dea45cc869d31ab8b0529058c1f
Christof Sirk <christof.sirk@gmail.com>
2014-04-26 22:15:10 +0200

    Test setting of scope values on main controller

--- spec/controllers/main_controller_spec.js
+++ spec/controllers/main_controller_spec.js
@@ -1,9 +1,17 @@
 describe('MainController', function() {
   beforeEach(module('annotationApp'));
-  xit('loads tokens', inject(function($controller) {
-    var scope = {};
-    var ctrl = $controller('MainController', {$scope:scope});
 
-    expect(scope.tokens.length).toBe(4);
+  it('sets scope values', inject(function($controller) {
+    var scope = {};
+    var mystate = { "mystate": "state"};
+    var ctrl = $controller('MainController', {$scope:scope, state:mystate, configurator: {
+      conf_for : function(name) {
+        return { plugins: "plugins", template: "template"};
+      }
+    }});
+
+    expect(scope.state).toBe(mystate);
+    expect(scope.plugins).toEqual("plugins");
+    expect(scope.template).toBe("template");
   }));
 });
