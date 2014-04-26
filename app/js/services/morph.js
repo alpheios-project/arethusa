30ddc28b43e16cffab6330f65345405edc7f1aad
LFDM <1986gh@gmail.com>
2014-04-26 17:00:03 +0200

    Load analyses by word in morph service

--- app/js/services/morph.js
+++ app/js/services/morph.js
@@ -4,11 +4,34 @@ annotationApp.service('morph', function(state, configurator) {
   this.conf = configurator.conf_for('morph');
   var morphRetriever = configurator.getService(this.conf.retriever);
 
-  var analyses;
-  morphRetriever.getData(function(res) {
-    analyses = res;
-  });
-  this.analyses = analyses;
+  this.seedAnalyses = function(tokens) {
+    var analyses = {};
+    angular.forEach(tokens, function(token, id) {
+      analyses[id] = { string: token.string, forms: [], analyzed: false };
+    });
+    return analyses;
+  };
+
+  this.getAnalyses = function(string) {
+    var result;
+    morphRetriever.getData(string, function(res) {
+      result = res;
+    });
+    return result;
+  };
+
+  this.loadInitalAnalyses = function(that) {
+    var analyses = that.seedAnalyses(state.tokens);
+    angular.forEach(analyses, function(val, id) {
+      var forms = that.getAnalyses(val.string);
+      val.analyzed = true;
+      val.forms = val.forms.concat(forms);
+    });
+    return analyses;
+  };
+
+  this.analyses = this.loadInitalAnalyses(this);
+  console.log(this.analyses);
 
   this.currentAnalysis = function() {
     return this.analyses[state.selectedToken.id];
