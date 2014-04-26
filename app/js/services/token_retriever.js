0d05c67fcf23c69af73b437c4c88beac7a57a39e
LFDM <1986gh@gmail.com>
2014-04-26 16:05:05 +0200

    Add comment about synchronous token retrieval

--- app/js/services/token_retriever.js
+++ app/js/services/token_retriever.js
@@ -1,6 +1,8 @@
 "use strict";
 
 annotationApp.service('tokenRetriever', function($http) {
+  // tokens should always be loaded synchronous - the app should
+  // not start anything without knowing an initial state
   this.getData = function(callback) {
     var request = $.ajax({
       url: './static/tokens.json',
