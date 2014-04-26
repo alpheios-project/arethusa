71b336dfba5a6690d4bcaf7e4ca286b9a10524cb
LFDM <1986gh@gmail.com>
2014-04-26 16:59:28 +0200

    Make morphRetriever work more like a real service

--- app/js/services/morph_retriever.js
+++ app/js/services/morph_retriever.js
@@ -2,11 +2,22 @@
 
 annotationApp.service('morphRetriever', function($http) {
   this.getData = function(callback) {
+    var result;
     var request = $.ajax({
       url: './static/analyses.json',
       async: false
     });
 
     request.done(callback);
+  };
+
+  var stubData;
+  this.getStubData(function(res) {
+    stubData = res;
+  });
+
+  this.getData = function(string, callback) {
+    var result = stubData[string] || [];
+    callback(result);
   };
 });
