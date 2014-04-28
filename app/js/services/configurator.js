764ff37bca320cceced0058630070932227018b0
lichtr <robert.lichtensteiner@gmail.com>
2014-04-29 01:32:49 +0200

    Registers loaded plugins in Maincontroller.

--- app/js/services/configurator.js
+++ app/js/services/configurator.js
@@ -3,30 +3,47 @@
 annotationApp.factory('configurator', function($http, $injector) {
   var fac = {};
 
-  var confs = ['text', 'morph', 'comment', 'main'];
-  angular.forEach(confs, function(conf, key){
-    // creating paths
-    fac[conf + "_path"] = './static/' + conf + '_conf.json';
+  var main = ['main'];
+  var confs = ['text', 'morph', 'comment'];
 
-    // requesting files
-    var request = $.ajax({
-      url: fac[conf + "_path"],
-      async: false
-    });
+  // loads main and plugin config files
+  // params: confs is array of plugins; if sync is false,
+  // files are loaded asynchronously (obliged for main!)
+  var loadConfiguration = function(confs, sync) {
+    angular.forEach(confs, function(conf, key){
+      // creating paths
+      fac[conf + "_path"] = './static/' + conf + '_conf.json';
 
-    // store responses
-    request.done(function(data) {
-      fac[conf] = data;
+      // requesting files
+      var request = $.ajax({
+        url: fac[conf + "_path"],
+        async: sync
+      });
+
+      // store responses
+      request.done(function(data) {
+        fac[conf] = data;
+      });
     });
-  });
-  console.log(fac);
+  };
+
+  // registers all loaded plugins in MainController
+  var registerPlugins = function(plugins){
+    angular.forEach(plugins, function(plugin, key) {
+      fac.main.MainController.plugins[plugin] = fac[plugin];
+    });
+  };
+
+  loadConfiguration(main, false);
+  loadConfiguration(confs, false);
+  registerPlugins(confs);
 
   fac.getService = function(serviceName) {
     return $injector.get(serviceName);
   };
 
   fac.conf_for = function(plugin) {
-    var conf = fac.configuration;
+    var conf = fac.main;
     return conf[plugin] || conf.MainController.plugins[plugin];
   };
 
