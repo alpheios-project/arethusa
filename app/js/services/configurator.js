04a986144cad9a0230704b1c6fb921589e3745d8
lichtr <robert.lichtensteiner@gmail.com>
2014-04-29 15:12:51 +0200

    Adds loadPlugin() to configurator facotry.

--- app/js/services/configurator.js
+++ app/js/services/configurator.js
@@ -9,34 +9,33 @@ annotationApp.factory('configurator', function($http, $injector) {
   // loads main and plugin config files
   // params: confs is array of plugins; if sync is false,
   // files are loaded asynchronously (obliged for main!)
-  var loadConfiguration = function(confs, sync) {
-    angular.forEach(confs, function(conf, key){
-      // creating paths
-      fac[conf + "_path"] = './static/' + conf + '_conf.json';
+  var loadConfiguration = function(conf, sync) {
+    // creating paths
+    fac[conf + "_path"] = './static/' + conf + '_conf.json';
 
-      // requesting files
-      var request = $.ajax({
-        url: fac[conf + "_path"],
-        async: sync
-      });
+    // requesting files
+    var request = $.ajax({
+      url: fac[conf + "_path"],
+      async: sync
+    });
 
-      // store responses
-      request.done(function(data) {
-        fac[conf] = data;
-      });
+    // store responses
+    request.done(function(data) {
+      fac[conf] = data;
     });
   };
 
   // registers all loaded plugins in MainController
-  var registerPlugins = function(plugins){
-    angular.forEach(plugins, function(plugin, key) {
-      fac.main.MainController.plugins[plugin] = fac[plugin];
-    });
+  var registerPlugins = function(plugin) {
+    fac.main.MainController.plugins[plugin] = fac[plugin];
   };
 
-  loadConfiguration(main, false);
-  loadConfiguration(confs, false);
-  registerPlugins(confs);
+  loadConfiguration('main', false);
+
+  fac.loadPlugin = function(plugin) {
+    loadConfiguration(plugin, false);
+    registerPlugins(plugin);
+  };
 
   fac.getService = function(serviceName) {
     return $injector.get(serviceName);
