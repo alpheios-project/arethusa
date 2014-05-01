f3981f42665020ac7a7354256afe7d9f3dc637f1
LFDM <1986gh@gmail.com>
2014-05-01 22:47:00 +0200

    More methods to the HistoryObj

--- app/js/other/history_obj.js
+++ app/js/other/history_obj.js
@@ -3,14 +3,29 @@
 var HistoryObj= function(maxSize) {
   this.maxSize = maxSize;
   this.elements = [];
+  this.isFull = function() {
+    return this.maxSize == this.elements.length;
+  };
+
   this.push = function(obj) {
-    if (this.maxSize == this.elements.length) {
-      this.elements.shift();
+    if (this.isFull()) {
+      this.shift();
     }
     this.elements.push(obj);
   };
 
-  this.last = function() {
-    return this.elements[this.elements.length - 1];
+  this.shift = function() {
+    this.elements.shift();
+  };
+
+  this.pop = function() {
+    this.elements.pop();
+  };
+
+  this.unshift = function(obj) {
+    if (this.isFull()) {
+      this.pop();
+    }
+    this.elements.unshift(obj);
   };
 };
