67f595d7b7a471c41ebafbe5d4d391afb5ec13b8
LFDM <1986gh@gmail.com>
2014-05-01 14:15:52 +0200

    Fix listener bug in form-selector
    The text is not updated when a second form is selected in the old
    version.

--- app/js/directives/form_selector.js
+++ app/js/directives/form_selector.js
@@ -7,19 +7,22 @@ annotationApp.directive('formSelector', function() {
     controller: function($scope, $element, $attrs) {
       var id = $scope.id;
       var form = $scope.form;
-      $scope.text = 'Select';
+      $scope.selected = function() {
+        return $scope.plugin.isFormSelected(id, form);
+      };
+      $scope.text = function() {
+        return $scope.selected() ? 'Deselect' : 'Select';
+      };
       $scope.action = function(event) {
         event.stopPropagation();
-        if ($scope.plugin.isFormSelected(id, form)) {
-          $scope.text = "Select";
+        if ($scope.selected()) {
           $scope.plugin.unsetState(id);
         } else {
-          $scope.text = "Deselect";
           $scope.plugin.setState(id, form);
         }
       };
     },
-    template: '<span ng-click="action($event)">{{ text }}</span>'
+    template: '<span ng-click="action($event)">{{ text() }}</span>'
   };
   
 });
