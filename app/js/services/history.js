888c580dfccfae7bc47301590088822db69f2d20
LFDM <1986gh@gmail.com>
2014-05-01 22:38:45 +0200

    Use new HistoryObj in history service

--- app/js/services/history.js
+++ app/js/services/history.js
@@ -3,4 +3,7 @@
 angular.module('arethusa').service('history', function(state, configurator) {
   this.conf = configurator.configurationFor('history');
   this.template = this.conf.template;
+  this.maxSize = this.conf.maxSize || 20;
+  /* global HistoryObj */
+  this.history = new HistoryObj(this.maxSize);
 });
