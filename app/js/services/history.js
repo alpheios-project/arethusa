ba8cc188220806e1c31a9ab598c172b7ddb9dfa9
LFDM <1986gh@gmail.com>
2014-05-01 22:08:46 +0200

    Oh, configurator has changed as well

--- app/js/services/history.js
+++ app/js/services/history.js
@@ -1,6 +1,6 @@
 "use strict";
 
 angular.module('arethusa').service('history', function(state, configurator) {
-  this.conf = configurator.conf_for('history');
+  this.conf = configurator.configurationFor('history');
   this.template = this.conf.template;
 });
