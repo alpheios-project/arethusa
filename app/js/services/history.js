c8c839482b77a40b99b6e2c30f81f27e9164761d
LFDM <1986gh@gmail.com>
2014-05-01 23:49:20 +0200

    Get ahold of the inital state

--- app/js/services/history.js
+++ app/js/services/history.js
@@ -6,4 +6,9 @@ angular.module('arethusa').service('history', function(state, configurator) {
   this.maxSize = this.conf.maxSize || 20;
   /* global HistoryObj */
   this.history = new HistoryObj(this.maxSize);
+  this.save = function(event) {
+    this.history.unshift(event);
+  };
+
+  this.save(state.tokens); // hold the intial state
 });
