e222d73aa5f2bd6fb2dd0f0ef79adb51dfbe9eed
LFDM <1986gh@gmail.com>
2014-05-02 23:59:41 +0200

    Skip a save cycle in history
    Watches that fire events won't trigger a save() twice then. Check the
    comment for more info.

--- app/js/services/history.js
+++ app/js/services/history.js
@@ -8,13 +8,25 @@ angular.module('arethusa').service('history', function(configurator) {
   /* global HistoryObj */
   var hist = new HistoryObj(this.maxSize);
 
+  /* If the history is triggered by a watch, undo/redo will immediately
+   * fire another event once it's done - it's just another change after
+   * all. That's not helpful for our cause though, we therefore skip one
+   * digest cycle and make changes without propagating the second change
+   * to history. The event still fires - others might want to listen. */
+  var skipCycle = false;
+
   this.history = hist.elements;
 
   this.save = function(event) {
-    hist.save(event);
+    if (skipCycle) {
+      skipCycle = false;
+    } else {
+      hist.save(event);
+    }
   };
 
   this.undo = function() {
+    skipCycle = true;
     hist.undo();
   };
 
@@ -23,6 +35,7 @@ angular.module('arethusa').service('history', function(configurator) {
   };
 
   this.redo = function() {
+    skipCycle = true;
     hist.redo();
   };
 
