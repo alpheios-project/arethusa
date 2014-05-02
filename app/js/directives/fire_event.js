e41ff27034483fb2d2859084e7e1b3f0cd880210
LFDM <1986gh@gmail.com>
2014-05-02 17:27:27 +0200

    Generalize trackHistory directive to fireEvent

--- app/js/directives/fire_event.js
+++ app/js/directives/fire_event.js
@@ -1,15 +1,16 @@
 "use strict";
 
-angular.module('arethusa').directive('trackHistory', function(history) {
+angular.module('arethusa').directive('fireEvent', function(state) {
   return {
     restrict: 'A',
-    controller: function($scope, $element, $attrs, $parse) {
-      var attrs = $scope.$eval($attrs.trackHistory);
+    controller: function($scope, $element, $attrs) {
+      var attrs = $scope.$eval($attrs.fireEvent);
       var target = $scope[attrs.target];
+      console.log(target);
       var property = $scope[attrs.property];
       $scope.$watch(attrs.value, function(newVal, oldVal) {
         if (oldVal !== newVal) {
-          history.save(target, property, oldVal, newVal);
+          state.fireEvent(target, property, newVal);
         }
       });
     },
