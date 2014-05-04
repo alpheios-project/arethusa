87622bd64941a9562836a9cfd0d966da7582bdf9
LFDM <1986gh@gmail.com>
2014-05-04 10:32:27 +0200

    Add specs for historyObj

--- spec/other/history_obj_spec.js
+++ spec/other/history_obj_spec.js
@@ -11,10 +11,37 @@ describe('HistoryObj', function(){
 
   describe('push', function(){
     it('adds an element to the history', function() {
-      /* global HistoryObj */
       var hist = new HistoryObj(2);
       hist.push({});
       expect(hist.elements[0]).toEqual({});
+    });
+  });
+
+  describe('canBeUndone', function(){
+    it('returns false when there is no history to be undone', function() {
+      var hist = new HistoryObj(2);
+      expect(hist.canBeUndone()).toBeFalsy();
+    });
+
+    it('returns true when there is something to be undone', function() {
+      var hist = new HistoryObj(2);
+      hist.unshift({});
+      expect(hist.canBeUndone()).toBeTruthy();
+    });
+  });
+
+  describe('canBeRedone', function(){
+    it('returns false when nothing can be redone', function() {
+      var hist = new HistoryObj(2);
+      expect(hist.canBeRedone()).toBeFalsy();
+    });
+
+    it('returns true when something can be redone', function() {
+      var hist = new HistoryObj(2);
+      var event = { target : {}, property: 'x', oldVal: 0, newVal: 1};
+      hist.save({target: {}, property: 'x', });
+      hist.undo();
+      expect(hist.canBeRedone()).toBeTruthy();
     });
   });
 });
