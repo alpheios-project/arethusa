48dc5e84d094b29427117fda82101a9321511529
LFDM <1986gh@gmail.com>
2014-05-05 12:03:42 +0200

    Remove handling of faulty hist in history service

--- app/js/services/history.js
+++ app/js/services/history.js
@@ -8,25 +8,13 @@ angular.module('arethusa').service('history', function(configurator) {
   /* global HistoryObj */
   var hist = new HistoryObj(this.maxSize);
 
-  /* If the history is triggered by a watch, undo/redo will immediately
-   * fire another event once it's done - it's just another change after
-   * all. That's not helpful for our cause though, we therefore skip one
-   * digest cycle and make changes without propagating the second change
-   * to history. The event still fires - others might want to listen. */
-  var skipCycle = false;
-
   this.history = hist.elements;
 
   this.save = function(event) {
-    if (skipCycle) {
-      skipCycle = false;
-    } else {
-      hist.save(event);
-    }
+    hist.save(event);
   };
 
   this.undo = function() {
-    skipCycle = true;
     hist.undo();
   };
 
@@ -35,7 +23,6 @@ angular.module('arethusa').service('history', function(configurator) {
   };
 
   this.redo = function() {
-    skipCycle = true;
     hist.redo();
   };
 
