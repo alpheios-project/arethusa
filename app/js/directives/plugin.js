4bf4e86a2ddbd40f17a542277008300dca4cf9d8
LFDM <1986gh@gmail.com>
2014-05-05 22:34:20 +0200

    Update plugin directive to handle external plugins

--- app/js/directives/plugin.js
+++ app/js/directives/plugin.js
@@ -5,9 +5,17 @@ angular.module('arethusa').directive('plugin', function ($injector) {
     restrict: 'E',
     scope: true,
     link: function(scope, element, attrs) {
-      var name = scope.$eval(attrs.name);
-      scope.plugin = $injector.get(name);
+      // obj is either a string  and refers to an angular service or
+      // an object - an external service itself
+      var obj = scope.$eval(attrs.name);
+      if (angular.isString(obj)) {
+        scope.plugin = $injector.get(obj);
+        scope.pluginName = obj;
+      } else {
+        scope.plugin = obj;
+        scope.pluginName = obj.name;
+      }
     },
-    template: '<div ng-include="plugin.template"></div>'
+    template: '<div id="pluginName" ng-include="plugin.template"></div>'
   };
 });
