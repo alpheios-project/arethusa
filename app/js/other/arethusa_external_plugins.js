72cde832b54bc059fb8745d2740b87c729825868
LFDM <1986gh@gmail.com>
2014-05-05 23:15:34 +0200

    Extract external plugins container

---
+++ app/js/other/arethusa_external_plugins.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// Defines a global object where external plugins can register themselves.
+// Very raw and sketchy, we'll see how this evolves.
+window.ArethusaExternalPlugins = {};
+window.ArethusaExternalPlugins.add = function(plugin) {
+  this[plugin.name] = plugin;
+};
