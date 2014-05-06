fd6765df5529a6bdf22ec45e488234983c2b356a
LFDM <1986gh@gmail.com>
2014-05-06 19:14:30 +0200

    Ugly update of external history plugin

--- app/js/other/external_history.js
+++ app/js/other/external_history.js
@@ -1,23 +1,32 @@
 "use strict";
 
-window.ArethusaExternalPlugins.add(function() {
+window.ArethusaExternalPlugins.add('external_history', function() {
   var obj = {};
   obj.name = "external_history";
-  obj.api = function() {
-    var api;
-    if (api == 'undefined') {
-      api = window.arethusaExternalApi();
-    }
-    return api;
-  };
-
-  obj.container = $('#' + obj.name);
-
+  obj.api = window.arethusaExternalApi();
   /* global HistoryObj */
   obj.hist = new HistoryObj(2);
+  obj.history = obj.hist.elements;
+  obj.containerId = '#' + obj.name;
+  obj.container = function() {
+    return $(obj.containerId);
+  };
+  obj.select = function(selector) {
+    return obj.container().find(selector);
+  };
+
+  obj.renderHistory = function() {
+    var items = [];
+    $.each(obj.history, function(i, item) {
+      items.push('<li>' + JSON.stringify(item) + '</li>');
+    });
+    obj.select('#ext-hist-elements').html('<ul>' + items.join('') + '</ul>');
+  };
+
   obj.catchArethusaEvent = function(event) {
     obj.hist.save(event);
+    obj.renderHistory();
   };
 
   return obj;
-}());
+});
