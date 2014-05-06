792d367e780e204f712769658e14dad2fdab5dae
LFDM <1986gh@gmail.com>
2014-05-07 00:44:56 +0200

    Remove arethusaExternalPlugins
    Found a better way to handle this.

--- app/js/other/arethusa_external_plugins.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// Defines a global object where external plugins can register themselves.
-// Very raw and sketchy, we'll see how this evolves.
-window.ArethusaExternalPlugins = {};
-window.ArethusaExternalPlugins.add = function(name, pluginFn) {
-  this[name] = pluginFn;
-};
