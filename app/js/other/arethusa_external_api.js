61c16106ed406f0811a1cae0cd5d8b2a5b569aca
LFDM <1986gh@gmail.com>
2014-05-06 19:10:42 +0200

    Expose scope in eternal api + minor fixes

--- app/js/other/arethusa_external_api.js
+++ app/js/other/arethusa_external_api.js
@@ -15,10 +15,12 @@ window.arethusaExternalApi = function () {
   // I guess it might come to this sort of guarding close, so that other plugin
   // can implement this safely. We just ask if arethusa is loaded and proceed -
   // if it's not, we provide a mock object that just does nothing.
-  if (obj.isArethusaLoaded) {
+  if (obj.isArethusaLoaded()) {
     angular.element(document.body).ready(function() {
       obj.injector = angular.element(document.body).injector();
       obj.state = obj.injector.get('state');
+      obj.scope = angular.element(document.getElementById('arethusa-main-scope')).scope();
+      obj.apply = obj.scope.$apply;
       obj.fireEvent = function(token, category, oldVal, newVal) {
         obj.state.fireEvent(token, category, oldVal, newVal);
       };
