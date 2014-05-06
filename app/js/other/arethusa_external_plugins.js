5296f0a645939a85e979d025028fa2ec3cf02776
LFDM <1986gh@gmail.com>
2014-05-06 19:11:24 +0200

    External plugins need to provide a factory fn

--- app/js/other/arethusa_external_plugins.js
+++ app/js/other/arethusa_external_plugins.js
@@ -3,6 +3,6 @@
 // Defines a global object where external plugins can register themselves.
 // Very raw and sketchy, we'll see how this evolves.
 window.ArethusaExternalPlugins = {};
-window.ArethusaExternalPlugins.add = function(plugin) {
-  this[plugin.name] = plugin;
+window.ArethusaExternalPlugins.add = function(name, pluginFn) {
+  this[name] = pluginFn;
 };
