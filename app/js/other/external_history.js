148f5df51aa661b7f615f848a18434a0f12a08c9
LFDM <1986gh@gmail.com>
2014-05-06 17:33:16 +0200

    Some testing inside the external history plugin

--- app/js/other/external_history.js
+++ app/js/other/external_history.js
@@ -3,6 +3,21 @@
 window.ArethusaExternalPlugins.add(function() {
   var obj = {};
   obj.name = "external_history";
+  obj.api = function() {
+    var api;
+    if (api == 'undefined') {
+      api = window.arethusaExternalApi();
+    }
+    return api;
+  };
+
+  obj.container = $('#' + obj.name);
+
+  /* global HistoryObj */
+  obj.hist = new HistoryObj(2);
+  obj.catchArethusaEvent = function(event) {
+    obj.hist.save(event);
+  };
 
   return obj;
 }());
