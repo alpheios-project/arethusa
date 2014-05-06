e74e61e86b867ce2f6ba0cd50333b0da1de47817
LFDM <1986gh@gmail.com>
2014-05-07 00:52:57 +0200

    Refactor external history plugin

--- app/js/other/external_history.js
+++ app/js/other/external_history.js
@@ -1,32 +1,37 @@
 "use strict";
 
 window.arethusaInitPlugin('external_history', function() {
-  var obj = {};
-  obj.name = 'external_history';
-  obj.api = window.arethusaExternalApi();
   /* global HistoryObj */
-  obj.hist = new HistoryObj(2);
-  obj.history = obj.hist.elements;
-  obj.containerId = '#' + obj.name;
-  obj.container = function() {
-    return $(obj.containerId);
-  };
-  obj.select = function(selector) {
-    return obj.container().find(selector);
+  var obj = {
+    name: 'external_history',
+
+    api: window.arethusaExternalApi(),
+
+    hist: new HistoryObj(2),
+
+    container: function() {
+      return $('#' + obj.name);
+    },
+
+    select: function(selector) {
+      return obj.container().find(selector);
+    },
+
+    renderHistory: function() {
+      var items = [];
+      $.each(obj.hist.elements, function(i, item) {
+        items.push('<li>' + JSON.stringify(item) + '</li>');
+      });
+      obj.select('#ext-hist-elements').html('<ul>' + items.join('') + '</ul>');
+    },
+
+    catchArethusaEvent: function(event) {
+      obj.hist.save(event);
+      obj.renderHistory();
+    },
   };
 
-  obj.renderHistory = function() {
-    var items = [];
-    $.each(obj.history, function(i, item) {
-      items.push('<li>' + JSON.stringify(item) + '</li>');
-    });
-    obj.select('#ext-hist-elements').html('<ul>' + items.join('') + '</ul>');
-  };
-
-  obj.catchArethusaEvent = function(event) {
-    obj.hist.save(event);
-    obj.renderHistory();
-  };
+  // Event handling code
 
   $('#undo').click(function(e) {
     obj.api.apply(obj.hist.undo());
