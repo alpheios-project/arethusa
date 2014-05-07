5d986eadfa559a3ac9be57b94863004a13c360ae
LFDM <1986gh@gmail.com>
2014-05-07 16:45:56 +0200

    Add undo and redo directives for history
    Not really working how they should though - haven't found a clever way
    that they can show and hide themselves so far when used as attribute.
    Wouldn't be a problem if used as an element though.

---
+++ app/js/arethusa.core/directives/history_redo.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// Hm how to implement ng-show here?!
+// Right now we determine this through a function call - so
+// we cannot watch a simple expression.
+angular.module('arethusa.hist').directive('historyRedo', function(history) {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      element.bind('click', function() {
+        history.redo();
+        scope.$apply();
+      });
+    },
+  };
+});
+
