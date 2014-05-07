2f2435b59601f79388e664e918f54dc25cfe4177
LFDM <1986gh@gmail.com>
2014-05-07 14:46:14 +0200

    Add some xml functions to arethusaUtil

--- app/js/arethusa_util.js
+++ app/js/arethusa_util.js
@@ -3,12 +3,19 @@
 // Provides the global arethusaUtil object which comes with several
 // utility functions
 
-window.arethusaUtil = {
+var arethusaUtil = {
   // Pads a number with zeros
   formatNumber: function(number, length) {
     // coerce a fixnum to a string
     var n = "" + number;
     while (n.length < length) { n = "0" + n; }
     return n;
+  },
+
+  /* global X2JS */
+  xmlParser: new X2JS(),
+
+  xml2json: function(xml) {
+    return arethusaUtil.xmlParser.xml2json(xml);
   }
 };
