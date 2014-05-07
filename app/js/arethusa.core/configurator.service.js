7d601b0fc664fcd585bd516b55eea36c5f14a8c3
LFDM <1986gh@gmail.com>
2014-05-07 14:45:54 +0200

    Allow multi-retrieval of services in conf

--- app/js/arethusa.core/configurator.service.js
+++ app/js/arethusa.core/configurator.service.js
@@ -5,6 +5,15 @@ angular.module('arethusa.core').service('configurator', function($injector) {
     return $injector.get(serviceName);
   };
 
+  this.getServices = function(serviceNames) {
+    var res = [];
+    var that = this;
+    angular.forEach(serviceNames, function(name, i) {
+      res.push(that.getService(name));
+    });
+    return res;
+  };
+
   // this.configuration is set from outside on page load
   this.configurationFor = function(plugin) {
     var conf = this.configuration;
