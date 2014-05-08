304f76554ea14a3d671c8400d98773f509ac7609
LFDM <1986gh@gmail.com>
2014-05-08 09:32:15 +0200

    More flexible deselector directive

--- app/js/arethusa.core/directives/deselector.directive.js
+++ app/js/arethusa.core/directives/deselector.directive.js
@@ -1,11 +1,11 @@
 "use strict";
 
-angular.module('arethusa.core').directive('deselector', function() {
+angular.module('arethusa.core').directive('deselector', function(state) {
   return {
-    restrict: 'E',
+    restrict: 'AE',
     link: function(scope, element, attrs) {
       element.bind('click', function(e) {
-        scope.state.deselectAll();
+        state.deselectAll();
         scope.$apply();
       });
     }
