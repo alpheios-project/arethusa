8fbb7af1a87f30ccc87cbf94b4640488598dcfb0
LFDM <1986gh@gmail.com>
2014-05-08 23:35:24 +0200

    Update morphFormCreate directive

--- app/js/arethusa.morph/directives/morph_form_create.js
+++ app/js/arethusa.morph/directives/morph_form_create.js
@@ -3,6 +3,19 @@
 angular.module('arethusa.morph').directive('morphFormCreate', function() {
   return {
     restrict: 'E',
+    scope: true,
+    link: function(scope, element, attrs) {
+      scope.forms = scope.analysis.forms;
+      scope.form = {
+        postag: scope.plugin.emptyPostag(),
+        attributes: {}
+      };
+      // watch changes to update the postag
+      // save button to create the form formally
+      // watch click events in upper scopes which
+      // want to edit a form - replace the form in this scope
+      // then and we're all good.
+    },
     templateUrl: 'templates/morph_form_create.html'
   };
 });
