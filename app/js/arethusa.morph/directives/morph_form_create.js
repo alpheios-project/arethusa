99f9f8b71e3784fa0071bc656e6aa0c1850596a2
LFDM <1986gh@gmail.com>
2014-05-09 08:52:23 +0200

    Update morphFromCreate directive

--- app/js/arethusa.morph/directives/morph_form_create.js
+++ app/js/arethusa.morph/directives/morph_form_create.js
@@ -1,16 +1,21 @@
 "use strict";
 
+/* global arethusaUtil */
 angular.module('arethusa.morph').directive('morphFormCreate', function() {
   return {
     restrict: 'E',
     scope: true,
     link: function(scope, element, attrs) {
+      // need to prefill the attributes, so that we can set watches on all
+      // attribute changes - we need it to update the postag in synchronizePostag
+      var a = arethusaUtil.inject({}, scope.plugin.postagSchema, function(memo, el) {
+        memo[el] = undefined;
+      });
       scope.forms = scope.analysis.forms;
       scope.form = {
         postag: scope.plugin.emptyPostag(),
-        attributes: {}
+        attributes: a
       };
-      // watch changes to update the postag
       // save button to create the form formally
       // watch click events in upper scopes which
       // want to edit a form - replace the form in this scope
