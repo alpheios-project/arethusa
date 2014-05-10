85a16b43bec92ef5635ccac5f5110fbfec405741
LFDM <1986gh@gmail.com>
2014-05-10 15:01:14 +0200

    Use implicit paths to predefined conf files

--- app/js/arethusa.core/routes/main.constant.js
+++ app/js/arethusa.core/routes/main.constant.js
@@ -5,21 +5,18 @@ angular.module('arethusa.core').constant('MAIN_ROUTE', {
   template: '<div ng-include="template"></div>',
   resolve: {
     loadConfiguration: function($q, $http, $route, configurator) {
-      var files = {
-        default: './static/configs/default.json',
-        staging: './static/configs/staging.json'
-      };
       var params = $route.current.params;
+      var confPath = './static/configs/';
       var confUrl;
 
       // Fall back to default and wrong paths to conf files
       // need to be handled separately eventually
       if (params.conf) {
-        confUrl = files[params.conf] || files.default;
+        confUrl = confPath + params.conf + '.json';
       } else if (params.conf_file) {
         confUrl = params.conf_file;
       } else {
-        confUrl = files.default;
+        confUrl = confPath + 'default.json';
       }
 
       return $http({
