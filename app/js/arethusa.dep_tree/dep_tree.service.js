4191fc42882fe171912ed02745e7fbd2fca618d1
LFDM <1986gh@gmail.com>
2014-05-10 15:56:55 +0200

    Add depTree plugin to tree config file

--- app/js/arethusa.dep_tree/dep_tree.service.js
+++ app/js/arethusa.dep_tree/dep_tree.service.js
@@ -1,5 +1,7 @@
 "use strict";
 
 angular.module('arethusa.depTree').service('depTree', function(state, configurator) {
-
+  this.conf = configurator.configurationFor('depTree');
+  this.template = this.conf.template;
+  this.main = this.conf.main;
 });
