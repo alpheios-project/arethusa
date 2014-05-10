bb5242aa2b09b0ddacdd31a6b479fa44909b0bb7
LFDM <1986gh@gmail.com>
2014-05-10 11:50:58 +0200

    Use MAIN_ROUTE to define / route

--- app/js/arethusa.js
+++ app/js/arethusa.js
@@ -8,20 +8,7 @@ angular.module(
     'arethusa.morph',
     'arethusa.hist'
   ],
-  function($routeProvider) {
-    $routeProvider.when('/', {
-      controller: 'MainCtrl',
-      template: '<div ng-include="template"></div>',
-      resolve: {
-        loadConfiguration: function($q, $http, $route, configurator) {
-          return $http({
-            method: 'GET',
-            url: $route.current.params.conf || './static/configuration_default.json'
-          }).then(function(result) {
-            configurator.configuration = result.data;
-          });
-        }
-      }
-    }
-  );
-});
+  function($routeProvider, MAIN_ROUTE) {
+    $routeProvider.when('/', MAIN_ROUTE);
+  }
+);
