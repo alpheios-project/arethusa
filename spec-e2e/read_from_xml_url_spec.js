10f73e454028c97e46b896bf9b625990ff08b094
Christof Sirk <christof.sirk@gmail.com>
2014-05-11 22:15:09 +0200

    Some more e2e tests

--- spec-e2e/read_from_xml_url_spec.js
+++ spec-e2e/read_from_xml_url_spec.js
@@ -1,23 +1,38 @@
 "use strict";
 
 describe('Read tokens from xml url', function(){
-  var url = 'http://services.perseids.org/llt-data/1999.02.0002.2.1.1.xml';
+  var sentence1Url = 'http://services.perseids.org/llt-data/1999.02.0002.2.1.1.xml';
+  var sentence2Url = 'http://services.perseids.org/llt-data/1999.02.0002.2.1.2.xml';
   beforeEach(function() {
-    browser.get('/app/index.html');
+    browser.get('/app/#/staging');
 
-    element(by.model('query')).sendKeys(url);
-    element(by.buttonText('Search')).click();
   });
 
-  it('changes the url', function() {
-    // change %3A to : because angular doesn't encode it
-    var expectedParameterUrl = encodeURIComponent(url).replace("%3A", ":");
-    expect(browser.getCurrentUrl()).toContain("?input=" + expectedParameterUrl);
+  describe('Sentence 1', function() {
+    beforeEach(function() {
+      element(by.model('query')).sendKeys(sentence1Url);
+      element(by.buttonText('Search')).click();
+    });
+
+    it('changes the url', function() {
+      // change %3A to : because angular doesn't encode it
+      var expectedParameterUrl = encodeURIComponent(sentence1Url).replace("%3A", ":");
+      expect(browser.getCurrentUrl()).toContain("?treebank=" + expectedParameterUrl);
+    });
+
+    it('displays the data from the given xml', function() {
+      expect(element(by.css("token")).getText()).toEqual("Cum");
+    });
   });
 
-  xit('displays the data from the given xml', function() {
-    expect(element(by.css("token")).getText()).toEqual("Cum");
-  });
+  describe('Sentence 2', function() {
+    beforeEach(function() {
+      element(by.model('query')).sendKeys(sentence2Url + '\n');
+    });
 
+    it('displays the data from the given xml', function() {
+      expect(element(by.css("token")).getText()).toEqual("Coniurandi");
+    });
+ });
 });
 
