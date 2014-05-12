1c6dc2d16403ac7e434858bfa6b1485daca4cc78
Christof Sirk <christof.sirk@gmail.com>
2014-05-12 20:16:33 +0200

    First try at a e2e test for a tree

---
+++ spec-e2e/tree_spec.js
@@ -0,0 +1,28 @@
+"use strict";
+
+describe('Read tokens from xml url', function(){
+  var sentence1Url = 'http://services.perseids.org/llt-data/1999.02.0002.2.1.1.xml';
+  var sentence2Url = 'http://services.perseids.org/llt-data/1999.02.0002.2.1.2.xml';
+  beforeEach(function() {
+    browser.get('/app/#/tree');
+  });
+
+  describe('Sentence 1', function() {
+    beforeEach(function() {
+      element(by.model('query')).sendKeys(sentence1Url);
+      element(by.buttonText('Search')).click();
+    });
+
+    iit('displays a dependency tree', function() {
+      var nodes = element.all(by.css("token"));
+      var cum = nodes.first();
+      var que = nodes.get(0);
+      que.getCssValue("top").then(function(queTop) {
+        cum.getCssValue("top").then(function(cumTop) {
+          expect(queTop).toBeGreaterThan(cumTop);
+        });
+      });
+    });
+  });
+});
+
