fa96ea0095f588fa1e8106d3e71c42dc874a90d5
LFDM <1986gh@gmail.com>
2014-05-12 13:46:15 +0200

    Fix head id in treebank retriever
    They also need to prefixed with leading zeros to be compliant with our
    internal ids.

--- app/js/services/treebank_retriever.js
+++ app/js/services/treebank_retriever.js
@@ -4,12 +4,16 @@ angular.module('arethusa').service('treebankRetriever', function($http) {
   // tokens should always be loaded synchronous - the app should
   // not start anything without knowing an initial state
 
+  var formatId = function(id) {
+    return arethusaUtil.formatNumber(id, 4);
+  };
+
   var xmlToState = function(token) {
     // One could formalize this to real rules that are configurable...
     //
     // Remember that attributes of the converted xml are prefixed with underscore
     return {
-      id: arethusaUtil.formatNumber(token._id, 4),
+      id: formatId(token._id),
       string: token._form,
       morphology: {
         lemma: token._lemma,
@@ -19,7 +23,7 @@ angular.module('arethusa').service('treebankRetriever', function($http) {
         label: token._relation
       },
       head: {
-        id: token._head
+        id: formatId(token._head)
       }
     };
   };
