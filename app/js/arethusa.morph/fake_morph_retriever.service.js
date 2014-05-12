fbd1c65cc2bdd2e5f0b106f58e146add5855fa8a
LFDM <1986gh@gmail.com>
2014-05-12 13:58:52 +0200

    Add deprecation comment to fakeMorphRetriever

--- app/js/arethusa.morph/fake_morph_retriever.service.js
+++ app/js/arethusa.morph/fake_morph_retriever.service.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// Deprecated and not working, we don't need this anymore as we can just use the real deal.
+// Kept for sudden mind changes.
+
 angular.module('arethusa.morph').service('fakeMorphRetriever', function() {
   this.getStubData = function(callback) {
     var result;
