81bbf2f4482ba98e9177dd7fc54a54193b02c64b
LFDM <1986gh@gmail.com>
2014-05-12 13:49:06 +0200

    Deprecation comment in tokenRetriever

--- app/js/services/token_retriever.js
+++ app/js/services/token_retriever.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// Deprecated and not working - we won't need this anymore I guess.
+// Keeping it for now in case I change my mind.
+
 angular.module('arethusa').factory('tokenRetriever', function($http) {
   return {
     getData:  function(callback) {
