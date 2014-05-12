c19cd8e12e09022ff04cda6df8378ce5225ed719
Christof Sirk <christof.sirk@gmail.com>
2014-05-12 21:35:45 +0200

    Create dagreD3 Digraph with depTree service

--- app/js/arethusa.dep_tree/dep_tree.service.js
+++ app/js/arethusa.dep_tree/dep_tree.service.js
@@ -4,4 +4,23 @@ angular.module('arethusa.depTree').service('depTree', function(state, configurat
   this.conf = configurator.configurationFor('depTree');
   this.template = this.conf.template;
   this.main = this.conf.main;
+
+  this.createDigraph = function() {
+    /* global dagreD3 */
+    var g = new dagreD3.Digraph();
+
+    g.addNode("0000", { label: "[-]"});
+    angular.forEach(state.tokens, function(word, index) {
+      g.addNode(word.id, { label: word.string });
+    });
+
+    angular.forEach(state.tokens, function(word, index) {
+      if (word.head) {
+        g.addEdge(word.id, word.id, word.head.id, { label: word.relation.label });
+      }
+    });
+
+    return g;
+  };
+
 });
