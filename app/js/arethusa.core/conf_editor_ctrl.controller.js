00482645dc23282505c466a67725732e7a1d810d
LFDM <1986gh@gmail.com>
2014-05-13 13:00:20 +0200

    Include debug directive in confEditor

--- app/js/arethusa.core/conf_editor_ctrl.controller.js
+++ app/js/arethusa.core/conf_editor_ctrl.controller.js
@@ -1,4 +1,14 @@
 "use strict";
 
 angular.module('arethusa.core').controller('ConfEditorCtrl', function($scope) {
+  $scope.debug = false;
+  $scope.toggleDebugMode = function() {
+    $scope.debug = !$scope.debug;
+  };
+  $scope.conf = {
+    state: {
+      retrievers: {},
+      savers: {},
+    }
+  };
 });
