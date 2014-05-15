11e31d036f357765f062fb3a515e438d2edcce26
LFDM <1986gh@gmail.com>
2014-05-15 11:04:50 +0200

    Add some toggle vars in navbar directive

--- app/js/arethusa.core/directives/arethusa_navbar.js
+++ app/js/arethusa.core/directives/arethusa_navbar.js
@@ -5,9 +5,13 @@ angular.module('arethusa.core').directive('arethusaNavbar', function(configurato
     restrict: 'AE',
     scope: true,
     link: function(scope, element, attrs) {
+      var conf = configurator.configurationFor('navbar');
       scope.template = 'templates/navbar1.html';
+      scope.search = conf.search;
+      scope.hide   = conf.hide;
+      scope.navigation = conf.navigation;
     },
-    template: '<div ng-include="template"></div>'
+    template: '<div ng-hide="hide" ng-include="template"></div>'
   };
 
 });
