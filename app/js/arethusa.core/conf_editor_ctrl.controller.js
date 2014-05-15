fae8a33814e34f0c169c140f73abdad41f200d2e
LFDM <1986gh@gmail.com>
2014-05-15 09:51:03 +0200

    Mild first try at a conf editor GUI

--- app/js/arethusa.core/conf_editor_ctrl.controller.js
+++ app/js/arethusa.core/conf_editor_ctrl.controller.js
@@ -1,14 +1,39 @@
 "use strict";
 
 angular.module('arethusa.core').controller('ConfEditorCtrl', function($scope) {
-  $scope.debug = false;
+  $scope.debug = true;
   $scope.toggleDebugMode = function() {
     $scope.debug = !$scope.debug;
   };
+
+  // we might preload a conf file here, we need to parse this and populate these
+  // variables with data from this file then.
   $scope.conf = {
     state: {
       retrievers: {},
-      savers: {},
-    }
+      savers: {}
+    },
+
+   MainCtrl: {
+     template: '',
+     plugins: {}
+   }
+  };
+
+  $scope.main = $scope.conf.MainCtrl;
+  $scope.state = $scope.conf.state;
+
+  // we need this to sort plugins in specific orders
+  $scope.pluginList = [];
+
+  $scope.getPlugin = function(name) {
+    return $scope.main.plugins[name];
+  };
+
+  $scope.addPlugin = function() {
+    var name = $scope.pluginInput;
+    $scope.main.plugins[name] = { name: name, main: false };
+    $scope.pluginList.push(name);
+    $scope.pluginInput = '';
   };
 });
