492022e1d1ae0bcbe439a81a9cfbc3f74af2f5eb
LFDM <1986gh@gmail.com>
2014-05-15 19:39:37 +0200

    Make resource a factory-like service
    Check the comment in resource.service.js.

---
+++ app/js/arethusa.core/resource.service.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// A service that acts like a factory. The create functions spawns new resource
+// objects, that are a wrapper around ngResource
+
+angular.module('arethusa.core').service('resource', function($resource, $location) {
+  var paramsToObj = function(params) {
+    return arethusaUtil.inject({}, params, function(obj, param, i) {
+      obj[param] = $location.search()[param];
+    });
+  };
+
+  this.create = function(conf) {
+    var obj = {
+      // Our custom get function reads params from the route as specified in the
+      // configuration of a resource and takes additional params if need be.
+      get: function(otherParams) {
+        var params = angular.extend(paramsToObj(obj.params), otherParams || {});
+        return obj.resource.get(params).$promise;
+      }
+    };
+
+    obj.route = conf.route;
+    obj.params = conf.params || [];
+
+    obj.resource = $resource(obj.route, null, {
+      // This might look exceedingly stupid, but it is not:
+      // We override the usual get method ngResource, so that we can handle
+      // xml here as well, something the original struggles a bit with, as it
+      // always awaits a JSON response. In the transformResponse function we
+      // have access to the real response data - we set this right onto a new
+      // object together with the original headers.
+      // This way the xml string of the http call is not mangled up by angular.
+      // We'll see if this leads to troubles with real JSON calls. If it does,
+      // we just add a configuration setting of xml: true, which triggers this
+      // behaviour.
+      // The only downside is, that we have to call res.data instead of just res
+      // in the callback, but that is something we can live with.
+      'get' : {
+        method: 'GET',
+        transformResponse: function(data, headers) {
+          var res = {};
+          res.data = data;
+          res.headers = headers;
+          return res;
+        }
+      }
+    });
+
+    return obj;
+  };
+});
