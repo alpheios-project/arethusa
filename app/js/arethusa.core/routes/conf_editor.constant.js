96796f76f6de4345e4f3afc2d608bec4f1c2754b
LFDM <1986gh@gmail.com>
2014-05-17 10:06:08 +0200

    Update of conf editor route

--- app/js/arethusa.core/routes/conf_editor.constant.js
+++ app/js/arethusa.core/routes/conf_editor.constant.js
@@ -2,5 +2,15 @@
 
 angular.module('arethusa.core').constant('CONF_ROUTE', {
   controller: 'ConfEditorCtrl',
-  templateUrl: 'templates/conf_editor.html'
+  templateUrl: 'templates/conf_editor.html',
+  resolve: {
+    load: function($http, confUrl, configurator) {
+      var url = confUrl();
+      if (url) {
+        return $http.get(url).then(function(res) {
+          configurator.defineConfiguration(res.data);
+        });
+      }
+    }
+  }
 });
