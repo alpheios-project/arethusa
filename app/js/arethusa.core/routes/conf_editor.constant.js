b4ebbfb74c9355e842d520bb698c73762b94d1e4
LFDM <1986gh@gmail.com>
2014-05-17 13:39:26 +0200

    Load a specific confEditor conf file in conf route

--- app/js/arethusa.core/routes/conf_editor.constant.js
+++ app/js/arethusa.core/routes/conf_editor.constant.js
@@ -4,15 +4,11 @@ angular.module('arethusa.core').constant('CONF_ROUTE', {
   controller: 'ConfEditorCtrl',
   templateUrl: 'templates/conf_editor.html',
   resolve: {
-    load: function($http, confUrl, configurator) {
-      var url = confUrl();
-      if (url) {
-        return $http.get(url).then(function(res) {
-          configurator.defineConfiguration(res.data, url);
-        });
-      } else {
-        return configurator.loadConfTemplate;
-      }
+    load: function($http, configurator) {
+      var url = './static/configs/conf_editor/1.json';
+      return $http.get(url).then(function(res) {
+        configurator.defineConfiguration(res.data, url);
+      });
     }
   }
 });
