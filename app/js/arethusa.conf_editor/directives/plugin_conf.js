c5459f38a2a4f56fb70b8f0b61cc27c96c73750c
LFDM <1986gh@gmail.com>
2014-05-18 01:33:18 +0200

    Add conf scope var to pluginConf directive

--- app/js/arethusa.conf_editor/directives/plugin_conf.js
+++ app/js/arethusa.conf_editor/directives/plugin_conf.js
@@ -10,6 +10,7 @@ angular.module('arethusa.confEditor').directive('pluginConf', function() {
       // Right now paths to such configuration are hardcoded to a specific
       // folder. This will be much more dynamic in the future.
       scope.template = 'templates/configs/' + name + '.html';
+      scope.conf = scope.pluginConf(name);
     },
     templateUrl: 'templates/plugin_conf.html'
   };
