662aaae249f1411499aaf21b02c6527ceec1cca4
LFDM <1986gh@gmail.com>
2014-05-18 01:00:32 +0200

    Add comment to plugin conf

--- app/js/arethusa.conf_editor/directives/plugin_conf.js
+++ app/js/arethusa.conf_editor/directives/plugin_conf.js
@@ -6,6 +6,9 @@ angular.module('arethusa.confEditor').directive('pluginConf', function() {
     scope: true,
     link: function(scope, element, attrs) {
       var name = scope.$eval(attrs.name);
+
+      // Right now paths to such configuration are hardcoded to a specific
+      // folder. This will be much more dynamic in the future.
       scope.template = 'templates/configs/' + name + '.html';
     },
     templateUrl: 'templates/plugin_conf.html'
