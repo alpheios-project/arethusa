681654b442f71337ccb6ca647d0908ee19b56e86
LFDM <1986gh@gmail.com>
2014-05-17 12:37:18 +0200

    Add a save function to the conf editor

--- app/js/arethusa.core/conf_editor_ctrl.controller.js
+++ app/js/arethusa.core/conf_editor_ctrl.controller.js
@@ -1,6 +1,6 @@
 "use strict";
 
-angular.module('arethusa.core').controller('ConfEditorCtrl', function($scope, configurator) {
+angular.module('arethusa.core').controller('ConfEditorCtrl', function($scope, configurator, $http) {
   $scope.debug = false;
   $scope.toggleDebugMode = function() {
     $scope.debug = !$scope.debug;
@@ -28,4 +28,25 @@ angular.module('arethusa.core').controller('ConfEditorCtrl', function($scope, co
   $scope.isMainPlugin = function(name) {
     return $scope.pluginConf(name).main;
   };
+
+  $scope.save = function(filename) {
+    var request = {
+      path: $scope.filePath,
+      data: $scope.conf
+    };
+
+    // This is currently hardcoded and awaits the llt-arethusa-file_server
+    // running locally on port 8086.
+    // Check http://github.com/latin-language-toolkit/llt-arethusa-file_server
+    $http.post('http://localhost:8086/write', request).
+      // will go in the notification bar once we have it
+      success(function(res) {
+        arethusaLogger.log('File saved');
+      }).
+      error(function(res) {
+        arethusaLogger.log('Error while saving file');
+      });
+  };
+
+
 });
