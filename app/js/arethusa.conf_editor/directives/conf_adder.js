3c9b7be1657053db90457c3e3a1b966a77a0d625
LFDM <1986gh@gmail.com>
2014-05-19 13:10:39 +0200

    Add a comment about behaviour of delegated fns
    ... inside of directives. Not entirely sure what's happening here.

--- app/js/arethusa.conf_editor/directives/conf_adder.js
+++ app/js/arethusa.conf_editor/directives/conf_adder.js
@@ -9,6 +9,8 @@ angular.module('arethusa.confEditor').directive('confAdder', function() {
     },
     link: function(scope, element, attrs) {
       scope.submitInput = function() {
+        // Quite interesting how we need to pass the argument as
+        // an object here... Can't really explain why atm.
         scope.submitter({ input: scope.input });
         scope.input = '';
       };
