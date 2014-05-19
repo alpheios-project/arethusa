dc5f300f7426cb93736bb9bbd391ead5931347d1
LFDM <1986gh@gmail.com>
2014-05-19 11:16:41 +0200

    Refactor plugin-conf directive
    Use an isolated scope for it.

--- app/js/arethusa.conf_editor/directives/plugin_conf.js
+++ app/js/arethusa.conf_editor/directives/plugin_conf.js
@@ -3,14 +3,18 @@
 angular.module('arethusa.confEditor').directive('pluginConf', function() {
   return {
     restrict: 'AE',
-    scope: true,
+    scope: {
+      // name needs a two-way binding, @ won't cut it, as we need its evaluated
+      // value inside the link function already. @ would only resolve it during
+      // the next compile cycle - the template would still be able to read it.
+      name: '=',
+      conf: '=',
+      remover: '&'
+    },
     link: function(scope, element, attrs) {
-      var name = scope.$eval(attrs.name);
-
       // Right now paths to such configuration are hardcoded to a specific
       // folder. This will be much more dynamic in the future.
-      scope.template = 'templates/configs/' + name + '.html';
-      scope.conf = scope.pluginConf(name);
+      scope.template = 'templates/configs/' + scope.name + '.html';
     },
     templateUrl: 'templates/plugin_conf.html'
   };
