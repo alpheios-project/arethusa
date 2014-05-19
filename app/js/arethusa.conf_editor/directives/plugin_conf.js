74e2612af15ce08073f7f5e61eff35bfc3dc1124
LFDM <1986gh@gmail.com>
2014-05-19 14:57:45 +0200

    Use a child scope in pluginConf again

--- app/js/arethusa.conf_editor/directives/plugin_conf.js
+++ app/js/arethusa.conf_editor/directives/plugin_conf.js
@@ -3,20 +3,14 @@
 angular.module('arethusa.confEditor').directive('pluginConf', function() {
   return {
     restrict: 'AE',
-    scope: {
-      // name needs a two-way binding, @ won't cut it, as we need its evaluated
-      // value inside the link function already. @ would only resolve it during
-      // the next compile cycle - the template would still be able to read it.
-      //
-      // As a nested directive is also evaluated before the next compile cycle,
-      // the same principle applies. Two-way binding is needed to make this work.
-      name: '=',
-      conf: '=',
-      remover: '&'
-    },
+    // This was an isolated scope before. Plugin conf might be more complicated,
+    // so we use a child scope. We want access to all goodies the ConfEditorCtrl
+    // might provide.
+    scope: true,
     link: function(scope, element, attrs) {
       // Right now paths to such configuration are hardcoded to a specific
       // folder. This will be much more dynamic in the future.
+      scope.name = scope.$eval(attrs.name)
       scope.template = 'templates/configs/' + scope.name + '.html';
     },
     templateUrl: 'templates/conf_editor/plugin_conf.html'
