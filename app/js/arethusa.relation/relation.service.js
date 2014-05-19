9e613162ac10721a1f428960b91b36dae6e267ba
lichtr <robert.lichtensteiner@gmail.com>
2014-05-20 00:49:26 +0200

    Starts relation plugin.

--- app/js/arethusa.relation/relation.service.js
+++ app/js/arethusa.relation/relation.service.js
@@ -6,6 +6,14 @@ angular.module('arethusa.relation').service('relation', function(state, configur
   this.name = this.conf.name;
   this.relations = this.conf.relations;
 
+  this.currentLabels = function() {
+    var label = {};
+    angular.forEach(state.selectedTokens, function(val, id) {
+      label[id] = state.tokens[id].relation.label;
+    });
+    return label;
+  };
+
   this.init = function() {
     // tbd
   };
