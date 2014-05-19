a74c34a0ad18a71471ffc5ac29f3d168a47cacd0
LFDM <1986gh@gmail.com>
2014-05-19 14:01:49 +0200

    Add retrieverConf directive

---
+++ app/js/arethusa.conf_editor/directives/retriever_conf.js
@@ -0,0 +1,22 @@
+"use strict";
+
+angular.module('arethusa.confEditor').directive('retrieverConf', function() {
+  return {
+    restrict: 'AE',
+    scope: {
+      name: '=',
+      retriever: '='
+    },
+    link: function(scope, element, attrs) {
+      var retr = scope.retriever;
+      scope.addResource = function(name) {
+        scope.retr.push(name);
+      };
+
+      scope.removeResource = function(name) {
+        retr.splice(retr.indexOf(name), 1);
+      };
+    },
+    templateUrl: 'templates/conf_editor/retriever_conf.html'
+  };
+});
