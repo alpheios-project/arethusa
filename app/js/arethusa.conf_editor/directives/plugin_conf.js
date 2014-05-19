9b22d72657faf6ea1151b6482eb110168cdc9186
LFDM <1986gh@gmail.com>
2014-05-19 15:36:58 +0200

    Fix pluginConf directive

--- app/js/arethusa.conf_editor/directives/plugin_conf.js
+++ app/js/arethusa.conf_editor/directives/plugin_conf.js
@@ -10,7 +10,8 @@ angular.module('arethusa.confEditor').directive('pluginConf', function() {
     link: function(scope, element, attrs) {
       // Right now paths to such configuration are hardcoded to a specific
       // folder. This will be much more dynamic in the future.
-      scope.name = scope.$eval(attrs.name)
+      scope.name = scope.$eval(attrs.name);
+      scope.conf = scope.pluginConf(scope.name);
       scope.template = 'templates/configs/' + scope.name + '.html';
     },
     templateUrl: 'templates/conf_editor/plugin_conf.html'
