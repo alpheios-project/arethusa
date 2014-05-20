b1da9d0e4e68fe8be870ec5ba95295312a8ed485
lichtr <robert.lichtensteiner@gmail.com>
2014-05-20 20:08:09 +0200

    Resolves abbreviated label names.

--- app/js/arethusa.relation/relation.service.js
+++ app/js/arethusa.relation/relation.service.js
@@ -7,11 +7,35 @@ angular.module('arethusa.relation').service('relation', function(state, configur
   this.relations = this.conf.relations;
 
   this.currentLabels = function() {
-    var tokens = state.tokens;
+    return this.analyzeLabel(state.tokens);
+  };
+
+  this.analyzeLabel = function(tokens) {
     var res = {};
     angular.forEach(state.selectedTokens, function(val, id) {
       res[id] = { string: tokens[id].string, label: tokens[id].relation.label };
     });
+    return res;
+  };
+
+  this.selectLabel = function(attr) {
+    return this.relations[attr] || {};
+  };
+
+  this.longLabelName = function(attr) {
+    var that = this;
+    var res = [];
+    var label = this.selectLabel(attr).long;
+    if ( label ) {
+      res.push(label);
+    } else {
+      // if two labels are combined to one with a _
+      // it needs to be split, e.g. PRED_CO
+      var labels = attr.split('_');
+      angular.forEach(labels, function(val, key) {
+        res = res.concat(that.longLabelName(val));
+      });
+    }
     return res;
   };
 
