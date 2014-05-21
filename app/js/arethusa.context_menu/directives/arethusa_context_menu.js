5b14ac634b920f39277f2759537e35ab1e54d95d
LFDM <1986gh@gmail.com>
2014-05-21 12:29:13 +0200

    Pass plugins and state to arethusaContextMenu

--- app/js/arethusa.context_menu/directives/arethusa_context_menu.js
+++ app/js/arethusa.context_menu/directives/arethusa_context_menu.js
@@ -1,10 +1,15 @@
 "use strict";
 
-angular.module('arethusa.contextMenu').directive('arethusaContextMenu', function() {
+angular.module('arethusa.contextMenu').directive('arethusaContextMenu', function(state) {
   return {
     restrict: 'AE',
     scope: {
-      tokenId: '='
+      tokenId: '=',
+      plugins: "="
+    },
+    link: function(scope, element, attrs) {
+      scope.state = state;
+      scope.token = state.getToken(scope.tokenId);
     },
     templateUrl: 'templates/arethusa.context_menu/arethusa_context_menu.html'
   };
