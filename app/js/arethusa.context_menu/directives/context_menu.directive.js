9ae11ff493ca5d7bb7bc0c71761faf4b83659608
LFDM <1986gh@gmail.com>
2014-05-21 11:13:34 +0200

    Hold a reference to the current menu in a factory
    Because we now inject an element, we have a real reference to it and
    can detect when another menu is open. Before this change we would open
    up several ones at the same time.

--- app/js/arethusa.context_menu/directives/context_menu.directive.js
+++ app/js/arethusa.context_menu/directives/context_menu.directive.js
@@ -1,12 +1,17 @@
 "use strict";
 
-angular.module('arethusa.contextMenu').directive('contextMenu', function($document, $parse) {
+angular.module('arethusa.contextMenu')
+
+.factory('menuElement', function() {
+  return { element: null };
+})
+
+.directive('contextMenu', function($document, $parse, menuElement) {
   return {
     restrict: 'A',
     link: function(scope, element, attrs) {
       var openMenu;
       var opened = false;
-      var contextMenu = { element: null };
       var eventFn = $parse(attrs.contextMenu);
 
       function open(event, menu, parent) {
@@ -29,28 +34,32 @@ angular.module('arethusa.contextMenu').directive('contextMenu', function($docume
         opened = false;
       }
 
+      function closeAndApply() {
+        scope.$apply(function() {
+          close(menuElement.element);
+        });
+      }
+
       // need this to make sure we close the menu all the time
       function handleOtherClick(event) {
         if (opened && (event.button !== 2 || event.target !== openMenu)) {
-          scope.$apply(function() {
-            close(contextMenu.element);
-          });
+          closeAndApply();
         }
       }
 
       element.bind('contextmenu', function(event) {
         // If another menu is open while we want to open a new one,
         // we have to close the old one beforehand
-        if (contextMenu.element) {
-          close(contextMenu.element);
+        if (menuElement.element) {
+          close(menuElement.element);
         }
         // Find the context menu in the DOM
-        contextMenu.element = angular.element(document.getElementById(attrs.menuId));
+        menuElement.element = angular.element(document.getElementById(attrs.menuId));
 
         // Hold a reference to the current element for which we open
         // the context menu, so that we can detect if we right-click the
         // same element twice
-        openMenu = event.target;
+        //openMenu = event.target;
 
         // Disable the browser's default context menu
         event.preventDefault();
@@ -58,13 +67,18 @@ angular.module('arethusa.contextMenu').directive('contextMenu', function($docume
 
         scope.$apply(function() {
           eventFn(scope, { $event: event });
-          open(event, contextMenu.element, element);
+          open(event, menuElement.element, element);
         });
       });
 
       $document.bind('click', handleOtherClick);
       $document.bind('contextmenu', handleOtherClick);
-      // Remember ESC hits
+      // Close when ESC is hit
+      $document.bind('keyup', function(event) {
+        if (opened && event.keyCode === 27) {
+          closeAndApply();
+        }
+      });
     }
   };
 
