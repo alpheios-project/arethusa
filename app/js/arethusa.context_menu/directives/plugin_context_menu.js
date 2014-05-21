c9056062b31f36b7783e1f13feea3e01815eccf1
LFDM <1986gh@gmail.com>
2014-05-21 12:29:48 +0200

    Add pluginContextMenu directive

---
+++ app/js/arethusa.context_menu/directives/plugin_context_menu.js
@@ -0,0 +1,17 @@
+"use strict";
+
+angular.module('arethusa.contextMenu').directive('pluginContextMenu', function() {
+  return {
+    restrict: 'E',
+    scope: true,
+    link: function(scope, element, attrs) {
+      scope.plugin = scope.$eval(attrs.name);
+    },
+    template: '\
+      <div id="{{ plugin.name }}-context-menu"\
+         ng-include="plugin.contextMenuTemplate">\
+      </div>\
+    '
+  };
+});
+
