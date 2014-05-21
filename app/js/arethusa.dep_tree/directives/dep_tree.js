2dbac513f7941834d184d94ff96dcdd16930213b
LFDM <1986gh@gmail.com>
2014-05-21 13:43:59 +0200

    Cleaning up depTree directive a little bit

--- app/js/arethusa.dep_tree/directives/dep_tree.js
+++ app/js/arethusa.dep_tree/directives/dep_tree.js
@@ -1,24 +1,16 @@
 "use strict";
 
+/* global dagreD3 */
+
 angular.module('arethusa.depTree').directive('tree', function(depTree, state, $compile) {
   return {
     restrict: 'E',
     scope: true,
     link: function(scope, element, attrs) {
-      // global dagreD3 
       var g = new dagreD3.Digraph();
 
       g.addNode("0000", { label: "[-]"});
       angular.forEach(state.tokens, function(token, index) {
-        // scope.token = token;
-        // var tokenHtml = '<token ng-class="{selected: state.isSelected(token.id)}">sdfdsf</token>';
-        // tokenHtml = '<div>sdf</div>';
-        // var parent = angular.element('<div/>');
-        // var label = $compile(tokenHtml)(scope);
-        // parent.append(function () { return label[0]; });
-        // console.log(parent[0].innerHTML);
-        // g.addNode(token.id, { label: '<token ng-class="{selected: state.isSelected(token.id)}></token>' });
-        // g.addNode(token.id, { label: '<div><token ng-class="{selected: state.isSelected(token.id)}>' + token.string + '</token></div>' });
         g.addNode(token.id, { label: '<div id="' + token.id + '" class="node">' + token.string + '</div>' /*parent[0].innerHTML*/ });
       });
 
@@ -27,19 +19,22 @@ angular.module('arethusa.depTree').directive('tree', function(depTree, state, $c
           g.addEdge(word.id, word.id, word.head.id, { label: word.relation.label });
         }
       });
+
       var layout = depTree.createGraphLayout();
       var vis = d3.select('svg g');
       var renderer = new dagreD3.Renderer();
+
       renderer.layout(layout).run(g, vis);
-      // $compile(element)(scope);
+
       var tokenHtml = '<token ng-mouseenter="state.selectToken(token.id, \'hover\')" ng-mouseleave="state.deselectToken(token.id, \'hover\')" ng-class="{selected: state.isSelected(token.id)}"></token>';
 
-      vis.selectAll("div.node").append(function() { 
+      vis.selectAll("div.node").append(function() {
         var parent = angular.element(this)[0];
-        scope.token = state.tokens[parent.id];
-        console.log(angular.element(this));
+        var childScope = scope.$new();
+        childScope.token = state.tokens[parent.id];
         parent.textContent = "";
-        return $compile(tokenHtml)(scope)[0];
+
+        return $compile(tokenHtml)(childScope)[0];
       });
     },
     template: '<svg><g transform="translate(20,20)"/></svg>'
