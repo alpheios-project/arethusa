f70e43dbd50575a8c792f75a162c6442bacca4c5
LFDM <1986gh@gmail.com>
2014-05-21 19:51:05 +0200

    Refactor the dependencyTree directive

--- app/js/arethusa.dep_tree/directives/dependency_tree.js
+++ app/js/arethusa.dep_tree/directives/dependency_tree.js
@@ -8,40 +8,23 @@ angular.module('arethusa.depTree').directive('dependencyTree', function(depTree,
     scope: true,
     link: function(scope, element, attrs) {
       var tokens = scope.$eval(attrs.tokens);
-      var g = new dagreD3.Digraph();
+      // This will be needed once we manage to get this into an isolated scope
+      //scope.state = state;
 
-
-      scope.state = state;
-
-      g.addNode("0000", { label: "[-]"});
-      angular.forEach(tokens, function(token, index) {
-        g.addNode(token.id, { label: '<div id="' + token.id + '" class="node">' + token.string + '</div>' /*parent[0].innerHTML*/ });
-      });
-
+      function tokenPlaceholder(token) {
+        return '<div class="node" id="tph' + token.id + '">' + token.string + '</div>';
+      }
 
       function edgePlaceholder(label) {
         return '<div class="tree-label">' + label + '</div>';
       }
 
-      angular.forEach(tokens, function(word, index) {
-        if (word.head) {
-          g.addEdge(word.id, word.id, word.head.id, { label: edgePlaceholder(word.relation.label) });
-        }
-      });
+      function compiledToken(token) {
+        var childScope = scope.$new();
+        childScope.token = token;
 
-      var layout = depTree.createGraphLayout();
-
-      var svg = d3.select('svg')
-        .call(d3.behavior.zoom().on("zoom", function() {
-           var ev = d3.event;
-           svg.select("g")
-              .attr("transform", "translate(" + ev.translate + ") scale(" + ev.scale + ")");
-      }));
-      var vis = svg.select('g');
-
-      var renderer = new dagreD3.Renderer();
-
-      renderer.layout(layout).run(g, vis);
+        return $compile(tokenHtml)(childScope)[0];
+      }
 
       var tokenHtml = '\
         <token\
@@ -52,13 +35,46 @@ angular.module('arethusa.depTree').directive('dependencyTree', function(depTree,
         </token>\
       ';
 
-      vis.selectAll("div.node").append(function() {
-        var parent = angular.element(this)[0];
-        var childScope = scope.$new();
-        childScope.token = tokens[parent.id];
-        parent.textContent = "";
+      // Creating the node set
 
-        return $compile(tokenHtml)(childScope)[0];
+      var g = new dagreD3.Digraph();
+
+      g.addNode("0000", { label: "[-]"});
+      angular.forEach(tokens, function(token, index) {
+        g.addNode(token.id, { label: tokenPlaceholder(token) });
+      });
+
+      angular.forEach(tokens, function(token, index) {
+        if (token.head) {
+          g.addEdge(token.id, token.id, token.head.id, { label: edgePlaceholder(token.relation.label) });
+        }
+      });
+
+      // Initialize the graph
+
+      var layout = depTree.createGraphLayout();
+      var svg = d3.select('svg');
+      svg.call(d3.behavior.zoom().on("zoom", function() {
+         var ev = d3.event;
+         svg.select("g")
+           .attr("transform",
+                 "translate(" + ev.translate + ") scale(" + ev.scale + ")");
+      }));
+
+      var vis = svg.select('g');
+      var renderer = new dagreD3.Renderer();
+      renderer.layout(layout).run(g, vis);
+
+      // Customize the graph so that it holds our directives
+
+      vis.selectAll("div.node").append(function() {
+        // This is the element we append to and we created as a placeholder
+        // We clear out its text content so that we can display the content
+        // of our compiled token directive.
+        // The placholder has an id in the format of tphXXXX where XXXX is the id.
+
+        this.textContent = "";
+        return compiledToken(tokens[this.id.slice(3)]);
       });
 
       // Not very elegant, but we don't want marker-end arrowheads right now
