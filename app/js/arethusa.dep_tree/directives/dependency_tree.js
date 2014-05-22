2e387e87336ed60bb5b63b3bbe6d5cf0a7ebb296
LFDM <1986gh@gmail.com>
2014-05-23 00:02:14 +0200

    Allow custom styling for dependencyTree directive

--- app/js/arethusa.dep_tree/directives/dependency_tree.js
+++ app/js/arethusa.dep_tree/directives/dependency_tree.js
@@ -7,6 +7,7 @@ angular.module('arethusa.depTree').directive('dependencyTree', function(state, $
     restrict: 'A',
     scope: {
       tokens: '=',
+      styles: '='
     },
     link: function(scope, element, attrs) {
       // This will be needed once we manage to get this into an isolated scope
@@ -16,21 +17,77 @@ angular.module('arethusa.depTree').directive('dependencyTree', function(state, $
         return '<div class="node" id="tph' + token.id + '">' + token.string + '</div>';
       }
 
-      function edgePlaceholder(label) {
-        return '<div class="tree-label">' + label + '</div>';
+      function edgePlaceholder(token) {
+        var label = token.relation.label;
+        var id = token.id
+        // tel is for token edge label
+        return '<div id="tel' + id + '" class="tree-label">' + label + '</div>';
+      }
+
+      function tokenHasCustomStyling(token) {
+        var t = scope.styles[token.id] || {};
+        return t.token;
+      }
+
+      function applyTokenStyling(childScope, token) {
+        childScope.style = scope.styles[token.id].token;
+      }
+
+      // Struggling d3 a bit here...
+      //
+      // Right now this function is responsible for managing the label and
+      // edge colors when a custom styling is available.
+      //
+      // Once the relation plugin is ready, the label will be handled by
+      // a directive that is coming from there.
+      //
+      // The edges will stay the responsibility of this directive - and
+      // they are really solved messy right now.
+      // Guess one more look into the d3 API is needed.
+      function applyCustomStyling() {
+        var edges = vis.selectAll("g.edgePath path");
+        angular.forEach(scope.styles, function(value, key) {
+          if ('label' in value) {
+            vis.select('#tel' + key).style({ color: 'red' });
+          }
+          if ('edge' in value) {
+            edges.each(function(d) {
+              if (key == d) {
+                angular.element(this).css({ 'stroke': 'red', 'stroke-width': '1px' });
+              }
+            });
+          }
+        });
       }
 
       function compiledToken(token) {
         var childScope = scope.$new();
         childScope.token = token;
 
-        return $compile(tokenHtml)(childScope)[0];
+        // Ugly but working...
+        // We replace the colorize value in our token template string.
+        // If custom styles are given, we check if one is available for
+        // this token. If yes, we use it, otherwise we just pass one
+        // undefined which leaves the token unstyled.
+        //
+        // Without custom styles we let the token itself decide what color
+        // it has.
+        var style;
+        if (scope.styles) {
+          if (tokenHasCustomStyling(token)) {
+            applyTokenStyling(childScope, token);
+          } // else we just stay undefined
+          style = 'style';
+        } else {
+          style = 'true';
+        }
+        return $compile(tokenHtml.replace('STYLE', style))(childScope)[0];
       }
 
       var tokenHtml = '\
         <span\
           token="token"\
-          colorize="true"\
+          colorize="STYLE"\
           click="true"\
           hover="true">\
         </span>\
@@ -38,8 +95,10 @@ angular.module('arethusa.depTree').directive('dependencyTree', function(state, $
 
       // Creating the node set
 
-      // g will hold the graph and be set when new tokens are loaded
+      // g will hold the graph and be set when new tokens are loaded,
+      // vis will be it's d3 representation
       var g;
+      var vis;
 
       function createNodes() {
         g.addNode("0000", { label: "[-]"});
@@ -57,7 +116,7 @@ angular.module('arethusa.depTree').directive('dependencyTree', function(state, $
       }
 
       function drawEdge(token) {
-        g.addEdge(token.id, token.id, token.head.id, { label: edgePlaceholder(token.relation.label) });
+        g.addEdge(token.id, token.id, token.head.id, { label: edgePlaceholder(token) });
       }
 
       function updateEdge(token) {
@@ -86,7 +145,7 @@ angular.module('arethusa.depTree').directive('dependencyTree', function(state, $
       var renderer = new dagreD3.Renderer();
 
       function render() {
-        var vis = svg.select('g');
+        vis = svg.select('g');
         renderer.layout(layout).run(g, vis);
 
         // Customize the graph so that it holds our directives
@@ -120,6 +179,15 @@ angular.module('arethusa.depTree').directive('dependencyTree', function(state, $
 
       scope.$watch('tokens', function(newVal, oldVal) {
         createGraph();
+      });
+
+      scope.$watch('styles', function(newVal, oldVal) {
+        if (newVal !== oldVal) {
+          render();
+          if (newVal) {
+            applyCustomStyling();
+          }
+        }
       });
     },
     template: '<g transform="translate(20,20)"/>'
