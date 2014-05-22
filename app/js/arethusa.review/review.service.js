1853eb35e435875682dbc85f746545887f4d539d
LFDM <1986gh@gmail.com>
2014-05-22 22:37:40 +0200

    Mockup diff response in review service

--- app/js/arethusa.review/review.service.js
+++ app/js/arethusa.review/review.service.js
@@ -1,6 +1,6 @@
 "use strict";
 
-angular.module('arethusa.review').service('review', function(configurator, morph) {
+angular.module('arethusa.review').service('review', function(configurator, state, morph, $rootScope) {
   var self = this;
 
   var conf = configurator.configurationFor('review');
@@ -22,8 +22,67 @@ angular.module('arethusa.review').service('review', function(configurator, morph
     });
   }
 
+  function broadcast() {
+    $rootScope.$broadcast('diffLoaded');
+  }
+
   retriever.getData(function(res) {
     self.goldTokens = res[0].tokens;
     addStyleInfo(self.goldTokens);
   });
+
+  this.compare = function() {
+    // mockup object right now
+    var obj = {
+      '0002' : {
+        relation: {
+          label: {
+            original: 'OBJ',
+            new: 'PNOM'
+          }
+        }
+      },
+      '0005' : {
+        head: {
+          id: {
+            original: 7,
+            new: 6
+          }
+        }
+      },
+      '0011' : {
+        head: {
+          id: {
+            original: 16,
+            new: 18
+          }
+        }
+      },
+      '0016' : {
+        relation: {
+          label: {
+            original: 'OBJ',
+            new: 'SBJ'
+          }
+        },
+        lemma: {},
+        postag: {}
+      },
+      '0017' : {
+        relation: {
+          label: {
+            original: 'SBJ',
+            new: 'ATR'
+          }
+        }
+      }
+    };
+
+    if (obj) {
+      angular.forEach(obj, function(diff, id) {
+        state.setState(id, 'diff', diff);
+      });
+      broadcast();
+    }
+  };
 });
