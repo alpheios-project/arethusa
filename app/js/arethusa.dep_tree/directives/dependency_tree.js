f1f785d2ede29a8184e3524baa3d23090221c667
LFDM <1986gh@gmail.com>
2014-05-22 17:45:21 +0200

    Refactor and update dependencyTree directive
    We know listen to changes of head ideas and can re-render the tree...

--- app/js/arethusa.dep_tree/directives/dependency_tree.js
+++ app/js/arethusa.dep_tree/directives/dependency_tree.js
@@ -7,7 +7,7 @@ angular.module('arethusa.depTree').directive('dependencyTree', function(state, $
     restrict: 'A',
     scope: true,
     link: function(scope, element, attrs) {
-      var tokens = scope.$eval(attrs.tokens);
+      scope.tokens = scope.$eval(attrs.tokens);
       // This will be needed once we manage to get this into an isolated scope
       //scope.state = state;
 
@@ -39,16 +39,29 @@ angular.module('arethusa.depTree').directive('dependencyTree', function(state, $
 
       var g = new dagreD3.Digraph();
 
-      g.addNode("0000", { label: "[-]"});
-      angular.forEach(tokens, function(token, index) {
-        g.addNode(token.id, { label: tokenPlaceholder(token) });
-      });
+      function createNodes() {
+        g.addNode("0000", { label: "[-]"});
+        angular.forEach(scope.tokens, function(token, index) {
+          g.addNode(token.id, { label: tokenPlaceholder(token) });
+        });
+      }
 
-      angular.forEach(tokens, function(token, index) {
-        if (token.head) {
-          g.addEdge(token.id, token.id, token.head.id, { label: edgePlaceholder(token.relation.label) });
-        }
-      });
+      function createEdges() {
+        angular.forEach(scope.tokens, function(token, index) {
+          if (token.head) {
+            drawEdge(token);
+          }
+        });
+      }
+
+      function drawEdge(token) {
+        g.addEdge(token.id, token.id, token.head.id, { label: edgePlaceholder(token.relation.label) });
+      }
+
+      function updateEdge(token) {
+        g.delEdge(token.id);
+        drawEdge(token);
+      }
 
       // Initialize the graph
 
@@ -61,24 +74,47 @@ angular.module('arethusa.depTree').directive('dependencyTree', function(state, $
                  "translate(" + ev.translate + ") scale(" + ev.scale + ")");
       }));
 
-      var vis = svg.select('g');
       var renderer = new dagreD3.Renderer();
-      renderer.layout(layout).run(g, vis);
 
-      // Customize the graph so that it holds our directives
+      function render() {
+        var vis = svg.select('g');
+        renderer.layout(layout).run(g, vis);
 
-      vis.selectAll("div.node").append(function() {
-        // This is the element we append to and we created as a placeholder
-        // We clear out its text content so that we can display the content
-        // of our compiled token directive.
-        // The placholder has an id in the format of tphXXXX where XXXX is the id.
+        // Customize the graph so that it holds our directives
 
-        this.textContent = "";
-        return compiledToken(tokens[this.id.slice(3)]);
+        vis.selectAll("div.node").append(function() {
+          // This is the element we append to and we created as a placeholder
+          // We clear out its text content so that we can display the content
+          // of our compiled token directive.
+          // The placholder has an id in the format of tphXXXX where XXXX is the id.
+
+          this.textContent = "";
+          return compiledToken(scope.tokens[this.id.slice(3)]);
+        });
+
+        // Not very elegant, but we don't want marker-end arrowheads right now
+        vis.selectAll("g.edgePath path").attr('marker-end', '');
+      }
+
+      angular.forEach(scope.tokens, function(token, id) {
+        var childScope = scope.$new();
+        childScope.token = token.id;
+        childScope.head =  token.head;
+        childScope.$watch('head.id', function(newVal, oldVal) {
+          // Very important to do here, otherwise the tree will
+          // be render a little often on startup...
+          if (newVal !== oldVal) {
+            updateEdge(token);
+            render();
+          }
+        });
       });
 
-      // Not very elegant, but we don't want marker-end arrowheads right now
-      vis.selectAll("g.edgePath path").attr('marker-end', '');
+      scope.$watch('tokens', function(newVal, oldVal) {
+        createNodes();
+        createEdges();
+        render();
+      });
     },
     template: '<g transform="translate(20,20)"/>'
   };
