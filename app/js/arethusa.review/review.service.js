22ee4ff61de09cdedae6c65c9c4f615a84097bf9
LFDM <1986gh@gmail.com>
2014-05-22 20:12:36 +0200

    Use morph service to colorize the review tree

--- app/js/arethusa.review/review.service.js
+++ app/js/arethusa.review/review.service.js
@@ -1,6 +1,6 @@
 "use strict";
 
-angular.module('arethusa.review').service('review', function(configurator) {
+angular.module('arethusa.review').service('review', function(configurator, morph) {
   var self = this;
 
   var conf = configurator.configurationFor('review');
@@ -12,7 +12,18 @@ angular.module('arethusa.review').service('review', function(configurator) {
   var retrievers = configurator.getRetrievers(conf.retrievers);
   var retriever = retrievers.TreebankRetriever;
 
+  function addStyleInfo(tokens) {
+    angular.forEach(tokens, function(token, id) {
+      var form = token.morphology;
+      if (form) {
+        morph.postagToAttributes(form);
+        token.style = morph.styleOf(form);
+      }
+    });
+  }
+
   retriever.getData(function(res) {
     self.goldTokens = res[0].tokens;
+    addStyleInfo(self.goldTokens);
   });
 });
