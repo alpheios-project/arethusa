0120f9c39c71718847eb8c268e6fdf07e14f6450
LFDM <1986gh@gmail.com>
2014-05-23 15:32:21 +0200

    Clean up relation plugin

--- app/js/arethusa.relation/relation.service.js
+++ app/js/arethusa.relation/relation.service.js
@@ -9,60 +9,7 @@ angular.module('arethusa.relation').service('relation', function(state, configur
   this.relationValues = this.conf.relations;
   this.relations = {};
 
-  this.selectLabel = function(attr) {
-    return this.relations[attr] || {};
-  };
-
-  this.longLabelName = function(attr) {
-    var that = this;
-    var res = [];
-    var label = this.selectLabel(attr).long;
-    if ( label ) {
-      res.push(label);
-    } else {
-      // if two labels are combined to one with a _
-      // it needs to be split, e.g. PRED_CO
-      var labels = attr.split('_');
-      angular.forEach(labels, function(val, id) {
-        res = res.concat(that.longLabelName(val));
-      });
-    }
-    return res;
-  };
-
-  this.setState = function(id, label) {
-    state.setState(id, 'relation', label);
-  };
-
-  this.unsetState = function(id) {
-    state.unsetState(id, 'relation');
-  };
-
-  this.isLabelSelected = function(id, label) {
-    return state.tokens[id].relation == label;
-  };
-
-  this.buildTokens = function() {
-    var tokens = state.tokens;
-    var res = {};
-    angular.forEach(tokens, function(val, id) {
-      res[id] = tokens[id];
-    });
-    return res;
-  };
-
   this.currentLabels = function() {
-    //var that = this;
-    //var res = {};
-    //angular.forEach(state.selectedTokens, function(token, id) {
-      //res[id] = that.rels[id];
-      //var labels = res[id].label.split('_');
-      //res[id].combinedLabels = {};
-      //angular.forEach(labels, function(lab, i) {
-        //res[id].combinedLabels[i] = lab;
-      //});
-    //});
-    //return res;
     return arethusaUtil.inject({}, state.selectedTokens, function(memo, id, event) {
       memo[id] = self.relations[id];
     });
