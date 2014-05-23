a4fcfbfcad15eae4e4ecaa3e64ebee565debb5f5
LFDM <1986gh@gmail.com>
2014-05-23 15:04:24 +0200

    Start refactor of relation plugin

--- app/js/arethusa.relation/relation.service.js
+++ app/js/arethusa.relation/relation.service.js
@@ -1,25 +1,13 @@
 "use strict";
 
 angular.module('arethusa.relation').service('relation', function(state, configurator) {
+  var self = this;
+
   this.conf = configurator.configurationFor('relation');
   this.template = this.conf.template;
   this.name = this.conf.name;
-  this.relations = this.conf.relations;
-  this.rels = {};
-
-  this.currentLabels = function() {
-    var that = this;
-    var res = {};
-    angular.forEach(state.selectedTokens, function(token, id) {
-      res[id] = that.rels[id];
-      var labels = res[id].label.split('_');
-      res[id].combinedLabels = {};
-      angular.forEach(labels, function(lab, i) {
-        res[id].combinedLabels[i] = lab;
-      });
-    });
-    return res;
-  };
+  this.relationValues = this.conf.relations;
+  this.relations = {};
 
   this.selectLabel = function(attr) {
     return this.relations[attr] || {};
@@ -61,6 +49,53 @@ angular.module('arethusa.relation').service('relation', function(state, configur
       res[id] = tokens[id];
     });
     return res;
+  };
+
+  this.currentLabels = function() {
+    //var that = this;
+    //var res = {};
+    //angular.forEach(state.selectedTokens, function(token, id) {
+      //res[id] = that.rels[id];
+      //var labels = res[id].label.split('_');
+      //res[id].combinedLabels = {};
+      //angular.forEach(labels, function(lab, i) {
+        //res[id].combinedLabels[i] = lab;
+      //});
+    //});
+    //return res;
+    return arethusaUtil.inject({}, state.selectedTokens, function(memo, id, event) {
+      memo[id] = self.relations[id];
+    });
+  };
+
+  function splitLabel(relation) {
+    var split = relation.label.split('_');
+    relation.prefix = split[0];
+    relation.suffix = split[1];
+  }
+
+  this.buildLabel = function(relation) {
+    var elements = [relation.prefix, relation.suffix];
+    var clean = arethusaUtil.inject([], elements, function(memo, el) {
+      if (el) {
+        memo.push(el);
+      }
+    });
+    relation.label = clean.join('_');
+  };
+
+  this.expandRelation = function(relation) {
+    splitLabel(relation);
+    return relation;
+  };
+
+  this.createInternalState = function() {
+    return arethusaUtil.inject({}, state.tokens, function(memo, id, token) {
+      memo[id] = {
+        string: token.string,
+        relation: self.expandRelation(token.relation)
+      };
+    });
   };
 
   this.init = function() {
