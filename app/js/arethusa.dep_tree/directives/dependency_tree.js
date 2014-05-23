ec294cde16e6e0b453f59ef9bb0fadb57526d3d8
LFDM <1986gh@gmail.com>
2014-05-23 11:51:15 +0200

    Add a introductory comment to dependencyTree

--- app/js/arethusa.dep_tree/directives/dependency_tree.js
+++ app/js/arethusa.dep_tree/directives/dependency_tree.js
@@ -1,5 +1,43 @@
 "use strict";
 
+/* Draws a dependencyTree
+ *
+ * Can be used in isolation and awaits tokens passed into it, which have
+ * a head.id property.
+ *
+ * The directive needs to be set on an svg element as attribute.
+ *
+ * Styling of the tree can be customized by passing a styles object as
+ * attribute.
+ * This object should be a dictionary of token ids with style information
+ * in three categories: edge, label and token.
+ * These properties should hold regular css style information. Example
+ *
+ *   {
+ *     '0001' : {
+ *       edge: { stroke: 'blue' },
+ *       token: { color: 'white' }
+ *     }
+ *   }
+ *
+ * This would draw the edge of the token 0001 one blue and the textual
+ * representation of the token itself in white.
+ *
+ * Tokens are rendered by the token directive. If no styles object has
+ * been passed to the dependencyTree directive, the token itself will
+ * decide its styling.
+ * Hover and click events for tokens are activated. An option to customize
+ * this will follow.
+ *
+ * Watches are in effect for changes of a tokens head.id value, changes
+ * in the styles object, as well as in the tokens object.
+ *
+ * Example, given a tokens and a styles object in the current scope:
+ *
+ *   <svg dependency-tree tokens="tokens" styles="styles/>
+ *
+ */
+
 /* global dagreD3 */
 
 angular.module('arethusa.depTree').directive('dependencyTree', function($compile) {
