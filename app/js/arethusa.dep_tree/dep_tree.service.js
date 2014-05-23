53f0712f66320b5909d48468ee1b1e2d1026b544
LFDM <1986gh@gmail.com>
2014-05-23 12:12:36 +0200

    Add introductory comment to depTree

--- app/js/arethusa.dep_tree/dep_tree.service.js
+++ app/js/arethusa.dep_tree/dep_tree.service.js
@@ -1,5 +1,26 @@
 "use strict";
 
+/* Dependency Tree Handler with Diff capabilities
+ *
+ * This service has not much to do - the tree itself is handled
+ * completely by the dependencyTree directive.
+ *
+ * It has however additional diff capabilities, that are triggered
+ * by a global diffLoaded event.
+ * Knows how to pass style information to the tree visualization in
+ * case a comparison/review was done.
+ *
+ * One could experiment here that this code should go back to a diff plugin
+ * itself.
+ * A diff plugin would calculate style information and pass this data load
+ * through the diffLoaded event. The depTree service would listen to this
+ * and pass the style info to its tree.
+ *
+ * Let's wait on a decision for this until we have done more work on the
+ * diff plugin itself and resolved issue #80.
+ * (https://github.com/latin-language-toolkit/llt-annotation_environment/issues/80)
+ */
+
 angular.module('arethusa.depTree').service('depTree', function(state, configurator, $rootScope) {
   var self = this;
   this.conf = configurator.configurationFor('depTree');
