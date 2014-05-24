592844122cbf23f8445bd84189ffb365c066c11d
Christof Sirk <christof.sirk@gmail.com>
2014-05-24 11:01:38 +0200

    Add test for head change to e2e tree spec

--- spec-e2e/tree_spec.js
+++ spec-e2e/tree_spec.js
@@ -5,34 +5,43 @@ describe('Dependency tree', function(){
     browser.get('/app/#/staging2?doc=1&s=1');
   });
 
-  describe('Sentence 1', function() {
-    beforeEach(function() {
-    });
-
-    it('displays a dependency tree (que is above cum)', function() {
-      var nodes = element.all(by.css("div span span"));
-      nodes.map(function(elm, index) {
-        return {
-          index: index,
-          text: elm.getText(),
-          location: elm.getLocation()
-        };
-      }).then(function(nodeInfos) {
-        var que;
-        var cum;
-        for (var index = 0; index < nodeInfos.length; ++index) {
-          var element = nodeInfos[index];
-          if (element.text === "que") {
-            que = element;
-          }
-          if (element.text === "Cum") {
-            cum = element;
-          }
+  var que;
+  var cum;
+  beforeEach(function() {
+    var nodes = element.all(by.css("div span span"));
+    nodes.map(function(elm, index) {
+      return {
+        index: index,
+        text: elm.getText(),
+        location: elm.getLocation(),
+        element: elm
+      };
+    }).then(function(nodeInfos) {
+      for (var index = 0; index < nodeInfos.length; ++index) {
+        var element = nodeInfos[index];
+        if (element.text === "que") {
+          que = element;
         }
-        expect(que).toBeDefined();
-        expect(cum).toBeDefined();
-        expect(que.location.y).toBeGreaterThan(cum.location.y);
-      });
+        if (element.text === "Cum") {
+          cum = element;
+        }
+      }
+    });
+  });
+
+  it('displays a dependency tree (que is above cum)', function() {
+    expect(que).toBeDefined();
+    expect(cum).toBeDefined();
+    expect(que.location.y).toBeGreaterThan(cum.location.y);
+  });
+
+  describe('head change', function() {
+    xit('displays a dependency tree (que is above cum)', function() {
+      expect(que).toBeDefined();
+      expect(cum).toBeDefined();
+      que.element.click();
+      cum.element.click();
+      expect(que.location.y).toBeLessThan(cum.location.y);
     });
   });
 });
