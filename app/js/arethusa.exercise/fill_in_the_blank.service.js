15cba1b8b4e987e6ecbfb7d4eae0ecd0f5a99a26
LFDM <1986gh@gmail.com>
2014-05-26 21:39:04 +0200

    Add start and stop functionality

--- app/js/arethusa.exercise/fill_in_the_blank.service.js
+++ app/js/arethusa.exercise/fill_in_the_blank.service.js
@@ -1,9 +1,11 @@
 "use strict";
 
-angular.module('arethusa.exercise').service('fillInTheBlank', function(configurator, morph) {
+angular.module('arethusa.exercise').service('fillInTheBlank', function(configurator, morph, state) {
   var self = this;
   this.conf = configurator.configurationFor('fillInTheBlank');
   this.template = this.conf.template;
   this.name = this.conf.name;
   this.main = this.conf.main;
+
+  this.started = false;
 });
--- app/js/arethusa.exercise/instructor.service.js
+++ app/js/arethusa.exercise/instructor.service.js
@@ -5,4 +5,16 @@ angular.module('arethusa.exercise').service('instructor', function(fillInTheBlan
   this.conf = configurator.configurationFor('instructor');
   this.template = this.conf.template;
   this.name = this.conf.name;
+
+  this.started = false;
+  this.start = function() {
+    // Track time here
+    fillInTheBlank.started = true;
+    self.started = true;
+  };
+
+  this.stop = function() {
+    // stop time tracking
+    self.started = false;
+  };
 });
