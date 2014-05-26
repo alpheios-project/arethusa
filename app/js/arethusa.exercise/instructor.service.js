a8677cf48c536b1ea89a237b22b4ede2bd89e902
LFDM <1986gh@gmail.com>
2014-05-26 21:54:44 +0200

    Track time in instructor

--- app/js/arethusa.exercise/instructor.service.js
+++ app/js/arethusa.exercise/instructor.service.js
@@ -9,12 +9,36 @@ angular.module('arethusa.exercise').service('instructor', function(fillInTheBlan
   this.started = false;
   this.start = function() {
     // Track time here
+    self.startedAt = new Date();
     fillInTheBlank.started = true;
     self.started = true;
   };
 
   this.stop = function() {
     // stop time tracking
+    self.stoppedAt = new Date();
     self.started = false;
+    self.done = true;
+  };
+
+  this.timeElapsed = function() {
+    return Math.abs(self.stoppedAt - self.startedAt);
+  };
+
+  var aU = arethusaUtil;
+  this.timeElapsedFormatted = function() {
+    var t = self.timeElapsed() / 60;
+    var minutes = t / 60;
+    var seconds = t % minutes;
+
+    return aU.formatNumber(minutes, 2) + ':' + aU.formatNumber(seconds, 2);
+  };
+
+  this.init = function() {
+    self.started = false;
+    self.done = false;
+    self.startedAt = false;
+    self.stoppedAt = false;
+    self.report = {};
   };
 });
