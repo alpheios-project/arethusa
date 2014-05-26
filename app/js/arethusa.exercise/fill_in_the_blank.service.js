a033744ba43715dbf3bc5a841f90030f89bedaec
LFDM <1986gh@gmail.com>
2014-05-26 22:27:44 +0200

    Create simple exercise for verbs in fITB plugin

--- app/js/arethusa.exercise/fill_in_the_blank.service.js
+++ app/js/arethusa.exercise/fill_in_the_blank.service.js
@@ -8,4 +8,32 @@ angular.module('arethusa.exercise').service('fillInTheBlank', function(configura
   this.main = this.conf.main;
 
   this.started = false;
+
+  this.fetchReport = function(args) {
+
+  };
+
+  this.answers = {};
+
+  function createExercise() {
+    return arethusaUtil.inject({}, state.tokens, function(memo, id, token) {
+      var morph = token.morphology.attributes;
+      if (morph.pos == 'verb' && morph.mood) {
+        var lemma = token.morphology.lemma.replace(/\d/g, '');
+        memo[id] = {
+          hint: lemma + ', ' + morph.num + '.',
+          answer: token.string,
+          token: token
+        };
+      }
+    });
+  }
+
+  this.isExerciseTarget = function(id) {
+    return id in self.exercises;
+  };
+
+  this.init = function() {
+    self.exercises = createExercise();
+  };
 });
