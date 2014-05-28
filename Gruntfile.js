919fb374fb5696b8f5918501d86ba1c5012b847a
LFDM <1986gh@gmail.com>
2014-05-28 16:54:29 +0200

    Add grunt-concurrent to launch multiple watches

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,6 +81,18 @@ module.exports = function(grunt) {
         tasks: 'protractor:all'
       }
     },
+    concurrent: {
+      watches: {
+        tasks: [
+          'watch:serverSource',
+          'watch:serverHtml',
+          'watch:serverCss'
+        ],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
+    },
     jshint: {
       options: {
         jshintrc: true,
