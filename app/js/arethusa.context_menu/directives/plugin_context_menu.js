a0d8ef44f65a9ee8e0b5b9b26a9404f9d6360089
LFDM <1986gh@gmail.com>
2014-05-29 17:13:48 +0200

    Minor style changes for context menu

--- app/js/arethusa.context_menu/directives/plugin_context_menu.js
+++ app/js/arethusa.context_menu/directives/plugin_context_menu.js
@@ -3,6 +3,7 @@ angular.module('arethusa.contextMenu').directive('pluginContextMenu', function (
   return {
     restrict: 'E',
     scope: true,
+    replace: true,
     link: function (scope, element, attrs) {
       scope.plugin = scope.$eval(attrs.name);
     },
