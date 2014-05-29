5013a039419ae2b7bd8cd9b352a167a6beafe8f3
LFDM <1986gh@gmail.com>
2014-05-29 21:07:32 +0200

    Nested relation label menu working

--- app/js/arethusa.relation/directives/nested_menu.js
+++ app/js/arethusa.relation/directives/nested_menu.js
@@ -2,19 +2,41 @@
 
 angular.module('arethusa.relation').directive('nestedMenu', [
   '$compile',
-  function($compile) {
+  'relation',
+  function($compile, relation) {
     return {
       restrict: 'A',
       scope: {
-        relation: '=',
-        values: '=',
+        relObj: '=',
+        labelObj: '=',
         label: '='
       },
       link: function(scope, element, attrs) {
-        var html = '<ul><li ng-repeat="(k, v) in values.nested" nested-menu relation="relation" label="k" values="v"></li></ul>';
-        if (scope.values.nested) {
+        var html = '\
+        <ul>\
+          <li\
+            ng-repeat="(k, v) in labelObj.nested"\
+            nested-menu\
+            ng-class="{hover: k === relObj.label}"\
+            rel-obj="relObj"\
+            label="k" label-obj="v">\
+          </li>\
+        </ul>';
+
+        if (scope.labelObj.nested) {
+          console.log('lig');
           element.append($compile(html)(scope));
         }
+
+        scope.selectLabel = function() {
+          scope.relObj.prefix = scope.label;
+          relation.buildLabel(scope.relObj);
+        };
+
+        element.bind('click', function(event) {
+          event.stopPropagation();
+          scope.$apply(scope.selectLabel());
+        });
       },
       template: '{{ label }}'
     };
