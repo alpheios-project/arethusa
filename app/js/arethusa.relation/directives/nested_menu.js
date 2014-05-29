7d4dfdeb1997e2ed20f4f237bb1216dad7ed448c
LFDM <1986gh@gmail.com>
2014-05-29 20:36:42 +0200

    Remove obsolete code

--- app/js/arethusa.relation/directives/nested_menu.js
+++ app/js/arethusa.relation/directives/nested_menu.js
@@ -11,19 +11,6 @@ angular.module('arethusa.relation').directive('nestedMenu', [
         label: '='
       },
       link: function(scope, element, attrs) {
-        //var nestedHtml = '<ul nested-menu relation="relation" labels="labels"></ul>';
-
-        //angular.forEach(scope.labels, function(obj, label) {
-          //if (obj.nested) {
-            //var li = angular.element('#' + label);
-            //var childScope = scope.$new();
-            //childScope.labels = obj.nested;
-            //console.log(li);
-            //console.log($compile(nestedHtml)(childScope));
-            //li.append($compile(nestedHtml)(childScope));
-          //}
-        //});
-        //console.log('1');
         var html = '<ul><li ng-repeat="(k, v) in values.nested" nested-menu relation="relation" label="k" values="v"></li></ul>';
         if (scope.values.nested) {
           element.append($compile(html)(scope));
