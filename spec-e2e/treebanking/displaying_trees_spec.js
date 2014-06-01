1b2b923c65f725e44894e4eb5c2150605921cd96
Christof Sirk <christof.sirk@gmail.com>
2014-06-01 16:34:52 +0200

    Use new data for e2e test.

---
+++ spec-e2e/treebanking/displaying_trees_spec.js
@@ -0,0 +1,17 @@
+"use strict";
+
+describe('View treebank document', function(){
+  beforeEach(function() {
+    browser.get('/app/#/e2e?doc=caesar2&s=21');
+
+  });
+
+  it('displays the sentence and a tree', function() {
+    // word in the sentence
+    expect(element(by.css("p span span")).getText()).toEqual("In");
+
+    // word in the tree
+    expect(element(by.css("div span span")).getText()).toEqual("In");
+  });
+});
+
