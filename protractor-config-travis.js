534af7c4059580b51fd815ec307b6195bfccdc81
Christof Sirk <christof.sirk@gmail.com>
2014-06-02 18:51:37 +0200

    Move tunnel-identifier and build to each capability

--- protractor-config-travis.js
+++ protractor-config-travis.js
@@ -6,10 +6,16 @@ var specE2eFiles = 'spec-e2e/**/*.js';
 exports.config = {
   sauceUser: 'arethusa',
   sauceKey: '8e76fe91-f0f5-4e47-b839-0b04305a5a5c',
-  'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-  'build': process.env.TRAVIS_BUILD_NUMBER,
   specs: [specE2eFiles],
-  multiCapabilities: [{'browserName': 'firefox'}, {'browserName': 'chrome'}],
+  multiCapabilities: [{
+    'browserName': 'firefox',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+  }, {
+    'browserName': 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+  }],
   //capabilities: {'browserName': 'firefox'},
   baseUrl: 'http://localhost:' + devServerPort
 };
