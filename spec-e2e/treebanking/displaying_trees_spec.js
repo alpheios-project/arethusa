8dcfd67c4d235b0611fc5b8dd9eb1415a3790e92
Christof Sirk <christof.sirk@gmail.com>
2014-06-03 00:21:50 +0200

    Wait for tree node

--- spec-e2e/treebanking/displaying_trees_spec.js
+++ spec-e2e/treebanking/displaying_trees_spec.js
@@ -7,14 +7,15 @@ describe('View treebank document', function(){
 
   iit('displays the sentence and a tree', function() {
     var sentenceElement = by.css("p span span");
+    var treeNode = by.css("div.node span[token] span");
     browser.wait(function() {
-      return browser.isElementPresent(sentenceElement);
+      return browser.isElementPresent(treeNode);
     }, 8000);
     // word in the sentence
     expect(element(sentenceElement).getText()).toEqual("In");
 
     // word in the tree
-    expect(element(by.css("div.node span[token] span")).getInnerHtml()).toEqual("In");
+    expect(element(treeNode).getInnerHtml()).toEqual("In");
   });
 });
 
