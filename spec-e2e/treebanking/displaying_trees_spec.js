cc40d5746432fe3aa6913c3dd00493efc02442a4
Christof Sirk <christof.sirk@gmail.com>
2014-06-02 23:44:46 +0200

    Wait for sentence to show up in e2e test

--- spec-e2e/treebanking/displaying_trees_spec.js
+++ spec-e2e/treebanking/displaying_trees_spec.js
@@ -5,9 +5,13 @@ describe('View treebank document', function(){
     browser.get('/app/#/e2e?doc=caesar2&s=21');
   });
 
-  it('displays the sentence and a tree', function() {
+  iit('displays the sentence and a tree', function() {
+    var sentenceElement = by.css("p span span");
+    browser.wait(function() {
+      return browser.isElementPresent(sentenceElement);
+    }, 8000);
     // word in the sentence
-    expect(element(by.css("p span span")).getText()).toEqual("In");
+    expect(element(sentenceElement).getText()).toEqual("In");
 
     // word in the tree
     expect(element(by.css("div.node span[token] span")).getInnerHtml()).toEqual("In");
