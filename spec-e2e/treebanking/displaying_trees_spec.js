8c08a53d412cd1f3b8778458a03051533efd2e9a
Christof Sirk <christof.sirk@gmail.com>
2014-06-03 03:57:22 +0200

    Try waitForAngular()

--- spec-e2e/treebanking/displaying_trees_spec.js
+++ spec-e2e/treebanking/displaying_trees_spec.js
@@ -3,17 +3,16 @@
 describe('View treebank document', function(){
   beforeEach(function() {
     browser.get('/app/#/e2e?doc=caesar2&s=21');
+    browser.waitForAngular();
   });
 
   iit('displays the sentence and a tree', function() {
     var sentenceElement = by.css("p span span");
     var treeNode = by.css("div.node span[token] span");
-    browser.wait(function() {
-      return browser.isElementPresent(treeNode);
-    }, 8000);
-    browser.sleep(10000);
+
     // word in the sentence
     expect(element(sentenceElement).getText()).toEqual("In");
+    browser.waitForAngular();
 
     // word in the tree
     expect(element(treeNode).getInnerHtml()).toEqual("In");
