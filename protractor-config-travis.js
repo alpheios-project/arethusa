828fd18af11e2c2c33a5f8f90ab5f2c979ae8cc4
Christof Sirk <christof.sirk@gmail.com>
2014-06-03 20:51:26 +0200

    Specify newer chrome version and disable firefox for now

--- protractor-config-travis.js
+++ protractor-config-travis.js
@@ -8,14 +8,15 @@ exports.config = {
   sauceKey: '8e76fe91-f0f5-4e47-b839-0b04305a5a5c',
   specs: [specE2eFiles],
   multiCapabilities: [{
+    'browserName': 'chrome',
+    'version': '34',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+  }/*, {
     'browserName': 'firefox',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER,
-  }, {
-    'browserName': 'chrome',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-  }],
+  }*/],
   //capabilities: {'browserName': 'firefox'},
   baseUrl: 'http://localhost:' + devServerPort
 };
