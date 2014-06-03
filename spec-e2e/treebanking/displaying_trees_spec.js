375deabf7ff704c2ccfb7665e277d493dfe260ff
Christof Sirk <christof.sirk@gmail.com>
2014-06-03 17:30:08 +0200

    Another try with wait

--- spec-e2e/treebanking/displaying_trees_spec.js
+++ spec-e2e/treebanking/displaying_trees_spec.js
@@ -3,19 +3,20 @@
 describe('View treebank document', function(){
   beforeEach(function() {
     browser.get('/app/#/e2e?doc=caesar2&s=21');
-    browser.waitForAngular();
   });
 
   iit('displays the sentence and a tree', function() {
     var sentenceElement = by.css("p span span");
     var treeNode = by.css("div.node span[token] span");
+    browser.wait(function () { return element(treeNode).isPresent() }, 3000).then(function () {
 
-    // word in the sentence
-    expect(element(sentenceElement).getText()).toEqual("In");
-    browser.waitForAngular();
+      // word in the sentence
+      expect(element(sentenceElement).getText()).toMatch("In");
+      browser.waitForAngular();
 
-    // word in the tree
-    expect(element(treeNode).getInnerHtml()).toEqual("In");
+      // word in the tree
+      expect(element(treeNode).getInnerHtml()).toMatch("In");
+    })
   });
 });
 
