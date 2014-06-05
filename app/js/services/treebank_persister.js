3cada2c6878e46a7d224d86eb35764818d69756c
Christof Sirk <christof.sirk@gmail.com>
2014-06-05 21:17:27 +0200

    Minor refactoring

--- app/js/services/treebank_persister.js
+++ app/js/services/treebank_persister.js
@@ -15,23 +15,28 @@ angular.module('arethusa').factory('TreebankPersister', [
         doc.xml = arethusaUtil.json2xml(doc.json);
       }
 
+      function updateWord(word, stateWord) {
+            word._head = arethusaUtil.formatNumber(stateWord.head.id, 0);
+            word._relation = stateWord.relation.label;
+            word._lemma = stateWord.morphology.lemma;
+            word._postag = stateWord.morphology.postag;
+            word._form = stateWord.string;
+      }
+
       function updateDocument() {
         var doc = documentStore.store.tbd; // placeholder
         var stored = arethusaUtil.toAry(doc.json.treebank.sentence);
         // navigator has to provide means to retrieve sentences by id
         // and not only through a flat array!
         var sentences = navigator.sentences;
+        console.log(sentences);
 
         angular.forEach(stored, function(sentence, i) {
+          console.log(sentence._id);
           var updated = sentences[sentence._id - 1];
           angular.forEach(sentence.word, function(word, i) {
             var stateWord = updated.tokens[arethusaUtil.formatNumber(word._id, 4)];
-
-            word._head = arethusaUtil.formatNumber(stateWord.head.id, 0);
-            word._relation = stateWord.relation.label;
-            word._lemma = stateWord.morphology.lemma;
-            word._postag = stateWord.morphology.postag;
-            word._form = stateWord.string;
+            updateWord(word, stateWord);
           });
         });
       }
