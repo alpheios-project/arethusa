076ea90ffc9f06c1d392874ec7d0d92be6e3220b
Christof Sirk <christof.sirk@gmail.com>
2014-06-05 23:39:56 +0200

    Use latest supported firefox version on OS X

--- protractor-config-travis.js
+++ protractor-config-travis.js
@@ -31,7 +31,7 @@ exports.config = {
     createCapabality('firefox', '25', 'Windows 7'),
     createCapabality('firefox', '26', 'Windows 7'),
     createCapabality('firefox', '29', 'Windows 7'),
-    createCapabality('firefox', '29', 'OS X 10.9')
+    createCapabality('firefox', '28', 'OS X 10.9')
     // createCapabality('safari', '7', 'OS X 10.9'),
     // createCapabality('internet explorer', '11', 'Windows 8.1')
   ],
