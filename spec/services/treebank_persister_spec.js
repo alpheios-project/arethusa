b3390798d6a8df78024780b56bf1684f4ebb4294
Christof Sirk <christof.sirk@gmail.com>
2014-06-05 22:06:29 +0200

    Test TreebankPersister

--- spec/services/treebank_persister_spec.js
+++ spec/services/treebank_persister_spec.js
@@ -2,25 +2,87 @@
 
 describe('treebank persister', function() {
   var $httpBackend;
+  var documentStore;
   var mockConfigurator = {
     configurationFor: function(name) {
       return {};
     },
     getServices: function(name) {
       return [];
+    },
+    provideResource: function(name) {
+      return {
+        'obj' : {
+          'save' : function() {
+          }
+        }
+      };
+    }
+  };
+  var mockNavigator = {
+    sentencesById: {
+      "2": {
+        "tokens": {
+          "0001": {
+            relation: {
+              id: 2
+            }
+          },
+          "0002": {
+            relation: {
+              id: 0
+            }
+          }
+        }
+      }
     }
   };
 
   beforeEach(module('arethusa', function($provide) {
     $provide.value('configurator', mockConfigurator);
+    $provide.value('navigator', mockNavigator);
   }));
 
-  beforeEach(inject(function($injector, documentStore) {
+  beforeEach(inject(function($injector, _documentStore_) {
     $httpBackend = $injector.get('$httpBackend');
+    documentStore = _documentStore_;
+    documentStore.addDocument('some-treebank', {
+      json: {
+        "treebank": {
+          "sentence": {
+            "_id": 2,
+            "word": [
+              {
+              "_id": "1",
+              "_form": "Coniurandi",
+              "_lemma": "conjuro1",
+              "_postag": "t-spdang-",
+              "_head": "4",
+              "_relation": "ATR"
+            },
+            {
+              "_id": "2",
+              "_form": "has",
+              "_lemma": "hic1",
+              "_postag": "p-p---fa-",
+              "_head": "3",
+              "_relation": "PNOM"
+            }]
+          }
+        }
+      }
+    });
   }));
 
   it('description', inject(function(TreebankPersister) {
-    var persister = new TreebankPersister();
-    // persister.udateDocument();
+    var conf = {
+      'resource' : 'test-resource',
+      'docIdentifier' : 'some-treebank'
+    };
+    var persister = new TreebankPersister(conf);
+
+    expect(persister).toBeDefined();
+
+    persister.saveData(function() {});
   }));
 });
