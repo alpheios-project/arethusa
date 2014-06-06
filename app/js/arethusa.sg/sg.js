49292805c7e062ea43f1aba0061595fe0f8bcae1
LFDM <1986gh@gmail.com>
2014-06-06 18:19:09 +0200

    Quick hack to start with sg
    Create an internal state, retrieve a container for morphology
    information coming from the state and selection logic.

--- app/js/arethusa.sg/sg.js
+++ app/js/arethusa.sg/sg.js
@@ -7,13 +7,49 @@ angular.module('arethusa.sg').service('sg', [
     var self = this;
 
     function configure() {
-      configurator.getConfAndDelegate('sg', self);
+      configurator.getConfAndDelegate('sg', self, ['labels']);
     }
 
     configure();
 
+    function sgTemplate() {
+      return {
+        morph: {},
+        ancestors: []
+      };
+    }
+
+    function createInternalState() {
+      return arethusaUtil.inject({}, state.tokens, function(memo, id, token) {
+        var obj = sgTemplate();
+        obj.string = token.string;
+        memo[id] = obj;
+      });
+    }
+
+    this.currentGrammar = function() {
+      return arethusaUtil.inject({}, state.selectedTokens, function(memo, id, event) {
+        var morph = state.tokens[id].morphology || {};
+        var grammar = self.grammar[id];
+        angular.extend(grammar.morph, morph.attributes);
+        memo[id] = grammar;
+      });
+    };
+
+    this.selectOptions = function(obj) {
+      var pos = obj.morph.pos;
+      var casus = obj.morph.case;
+
+      if (pos === "noun" && casus === "gen") {
+        return self.labels.GEN;
+      } else {
+        return {};
+      }
+    };
+
     this.init = function() {
       configure();
+      self.grammar = createInternalState();
     };
   }
 ]);
