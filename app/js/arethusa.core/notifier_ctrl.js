1be45aae06edd106d4bbe6d37a033bfe404d06e8
LFDM <1986gh@gmail.com>
2014-06-09 09:41:18 +0200

    Fix message watching in NotifierCtrl

--- app/js/arethusa.core/notifier_ctrl.js
+++ app/js/arethusa.core/notifier_ctrl.js
@@ -3,7 +3,8 @@ angular.module('arethusa.core').controller('NotifierCtrl', [
   '$scope',
   'notifier',
   function ($scope, notifier) {
-    $scope.$watch('notifier.messages', function (newVal, oldVal) {
+    $scope.messages = notifier.messages;
+    $scope.$watchCollection('messages', function (newVal, oldVal) {
       $scope.lastMessage = notifier.lastMessage();
       $scope.oldMessages = notifier.oldMessages();
     });
