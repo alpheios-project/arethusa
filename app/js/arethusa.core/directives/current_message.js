3e08f736a4cca5a813f8553ee51d649a3a076e26
LFDM <1986gh@gmail.com>
2014-06-09 11:11:40 +0200

    Clear message after a given time

--- app/js/arethusa.core/directives/current_message.js
+++ app/js/arethusa.core/directives/current_message.js
@@ -2,17 +2,43 @@
 
 angular.module('arethusa.core').directive('currentMessage', [
   'notifier',
-  function(notifier) {
+  '$timeout',
+  function(notifier, $timeout) {
     return {
       restrict: 'A',
       link: function(scope, element, attrs) {
+        var timer;
+
+        function setMessage(message) {
+          scope.message = message;
+        }
+        function clearMessage() {
+          scope.hide = true;
+        }
+
+        function cancelTimer() {
+          scope.hide = false;
+          $timeout.cancel(timer);
+        }
+        function setTimer() {
+          timer = $timeout(clearMessage, notifier.duration);
+        }
+
         scope.notifier = notifier;
         scope.messages = notifier.messages;
         scope.$watch('notifier.current', function(newVal, oldVal) {
-          scope.message = newVal;
+          cancelTimer();
+          setMessage(newVal);
+          setTimer();
         });
       },
-      template: '<span class="{{ message.type }}-message">{{ message.message }}</span>'
+      template: '\
+        <span\
+          ng-hide="hide"\
+          class="{{ message.type }}-message">\
+            {{ message.message }}\
+        </span>\
+      '
     };
   }
 ]);
