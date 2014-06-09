7336eba3c98efa09c09232d39ec2d28c86433a58
LFDM <1986gh@gmail.com>
2014-06-09 10:56:08 +0200

    Make notifier configurable
    And make use of default values in case such a configuration wasn't
    specified.

--- app/js/arethusa.core/notifier.js
+++ app/js/arethusa.core/notifier.js
@@ -1,39 +1,51 @@
 'use strict';
-angular.module('arethusa.core').service('notifier', function () {
-  var self = this;
-  this.messages = [];
-  this.current = {};
+angular.module('arethusa.core').service('notifier', [
+  'configurator',
+  function(configurator) {
+    var self = this;
 
-  function Message(type, message, description) {
-    this.type = type;
-    this.message = message;
-    this.description = description;
-  }
-
-  this.success = function (message) {
-    self.addMessage('success', message);
-  };
-  this.error = function (message) {
-    self.addMessage('error', message);
-  };
-
-  this.lastMessage = function () {
-    self.current = self.messages[0];
-    return self.current;
-  };
-  this.oldMessages = function () {
-    return self.messages.slice(1);
-  };
-
-
-  this.addMessage = function(type, message) {
-    if (self.messages.length === 15) {
-      self.messages.pop();
+    function configure() {
+      self.conf = configurator.configurationFor('notifier');
+      self.duration = self.conf.duration || 10000;
+      self.maxMessages = self.conf.maxMessages || 15;
     }
-    self.messages.unshift(new Message(type, message));
-    self.lastMessage();
-  };
-  this.reset = function() {
-    self.messages = [];
-  };
-});
+
+    configure();
+    this.messages = [];
+    this.current = {};
+
+    function Message(type, message, description) {
+      this.type = type;
+      this.message = message;
+      this.description = description;
+    }
+
+    this.success = function (message) {
+      self.addMessage('success', message);
+    };
+    this.error = function (message) {
+      self.addMessage('error', message);
+    };
+
+    this.lastMessage = function () {
+      self.current = self.messages[0];
+      return self.current;
+    };
+    this.oldMessages = function () {
+      return self.messages.slice(1);
+    };
+
+    this.addMessage = function(type, message) {
+      if (self.messages.length === self.maxMessages) {
+        self.messages.pop();
+      }
+      self.messages.unshift(new Message(type, message));
+      self.lastMessage();
+    };
+
+    this.init = function() {
+      configure();
+      self.messages = [];
+    };
+  }
+]);
