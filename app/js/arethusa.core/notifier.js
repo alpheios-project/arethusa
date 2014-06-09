aa624267087de9a87889cdf6ee49517fc04e4a24
LFDM <1986gh@gmail.com>
2014-06-09 10:13:42 +0200

    Fix maximum length for messages

--- app/js/arethusa.core/notifier.js
+++ app/js/arethusa.core/notifier.js
@@ -10,21 +10,27 @@ angular.module('arethusa.core').service('notifier', function () {
   }
 
   this.success = function (message) {
-    self.messages.unshift({
-      type: 'success',
-      message: message
-    });
+    self.addMessage('success', message);
   };
   this.error = function (message) {
-    self.messages.unshift({
-      type: 'error',
-      message: message
-    });
+    self.addMessage('error', message);
   };
+
   this.lastMessage = function () {
     return self.messages[0];
   };
   this.oldMessages = function () {
     return self.messages.slice(1);
+  };
+
+
+  this.addMessage = function(type, message) {
+    if (self.messages.length === 15) {
+      self.messages.pop();
+    }
+    self.messages.unshift(new Message(type, message));
+  };
+  this.reset = function() {
+    self.messages = [];
   };
 });
