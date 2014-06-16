d826c2c9d48c9746c22a6840a2b172690ac23b1a
LFDM <1986gh@gmail.com>
2014-06-16 11:34:59 +0200

    Allow nextToken directive as attribute

--- app/js/arethusa.core/directives/next_token.js
+++ app/js/arethusa.core/directives/next_token.js
@@ -1,9 +1,10 @@
 'use strict';
+
 angular.module('arethusa.core').directive('nextToken', [
   'state',
   function (state) {
     return {
-      restrict: 'E',
+      restrict: 'AE',
       link: function (scope, element, attrs) {
         element.bind('click', function (e) {
           scope.$apply(state.selectNextToken());
