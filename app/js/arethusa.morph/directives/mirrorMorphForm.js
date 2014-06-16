27ea5a897a39af64b7d0179f1927a2ae8831756a
LFDM <1986gh@gmail.com>
2014-06-16 23:42:30 +0200

    Add mirrorMorphForm directive

---
+++ app/js/arethusa.morph/directives/mirrorMorphForm.js
@@ -0,0 +1,31 @@
+"use strict";
+
+angular.module('arethusa.morph').directive('mirrorMorphForm', [
+  'morph',
+  function(morph) {
+    return {
+      restrict: 'A',
+      scope: {
+        form: '=mirrorMorphForm',
+        tokenId: '='
+      },
+      link: function(scope, element, attrs) {
+        var morphToken = morph.analyses[scope.tokenId];
+
+        function newCustomForm() {
+          var form = angular.copy(scope.form);
+
+          // We might want to clean up even more here - such as the
+          // lexical inventory information. Revisit later.
+          delete form.origin;
+
+          return form;
+        }
+
+        element.bind('click', function() {
+          scope.$apply(morphToken.customForm = newCustomForm());
+        });
+      }
+    };
+  }
+]);
