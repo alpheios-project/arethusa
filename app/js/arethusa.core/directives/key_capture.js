9fe1e732afbd9d790a0e6f363f48b26e5f5a4310
LFDM <1986gh@gmail.com>
2014-06-19 16:15:56 +0200

    Revert change of moving key-capture directive
    Configuration is not a problem - the conf file is accessed through a
    function anyway, calling it is therefore lazy and always at the right
    time.

--- app/js/arethusa.core/directives/key_capture.js
+++ app/js/arethusa.core/directives/key_capture.js
@@ -5,12 +5,10 @@ angular.module('arethusa.core').directive('keyCapture', [
     return {
       restrict: 'A',
       link: function (scope, element, attrs) {
-        var doc = angular.element(document.body);
-
-        doc.on('keydown', function (event) {
+        element.on('keydown', function (event) {
           keyCapture.keydown(event);
         });
-        doc.on('keyup', function (event) {
+        element.on('keyup', function (event) {
           keyCapture.keyup(event);
         });
       }
