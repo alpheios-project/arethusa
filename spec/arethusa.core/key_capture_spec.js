221c6bcc31147ad23ff856ebc3a58f07a59d7966
LFDM <1986gh@gmail.com>
2014-06-19 16:16:53 +0200

    Remove obsolete specs for keyCapture service

--- spec/arethusa.core/key_capture_spec.js
+++ spec/arethusa.core/key_capture_spec.js
@@ -3,33 +3,17 @@
 describe('keyCapture', function() {
   beforeEach(module('arethusa.core'));
 
-  describe('isCtrlActive', function() {
-    it('return false before any event', inject(function(keyCapture) {
-      expect(keyCapture.isCtrlActive()).toBe(false);
-    }));
-
-    it('return true if keydown for ctrl was received', inject(function(keyCapture) {
-      keyCapture.keydown({ keyCode : keyCapture.keyCodes.ctrl });
-
-      expect(keyCapture.isCtrlActive()).toBe(true);
-    }));
-
-    it('return false if keydown for ctrl was received', inject(function(keyCapture) {
-      keyCapture.keydown({ keyCode : keyCapture.keyCodes.ctrl });
-      keyCapture.keyup({ keyCode : keyCapture.keyCodes.ctrl });
-
-      expect(keyCapture.isCtrlActive()).toBe(false);
-    }));
-  });
-
   describe('onKeyPressed', function() {
     it('calls the given callback', inject(function(keyCapture) {
+      var event = {
+        keyCode: 27,
+        target: { tagname: '' }
+      };
       var callbackCalled = false;
       var callback = function() { callbackCalled = true; };
-      keyCapture.onKeyPressed(keyCapture.keyCodes.esc, callback);
+      keyCapture.onKeyPressed('esc', callback);
 
-      keyCapture.keydown({ keyCode : keyCapture.keyCodes.esc });
-      keyCapture.keyup({ keyCode : keyCapture.keyCodes.esc });
+      keyCapture.keyup(event);
 
       expect(callbackCalled).toBe(true);
     }));
