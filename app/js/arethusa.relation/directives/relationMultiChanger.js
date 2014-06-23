90395ec2a5477254c9a1f1e314b6ba9fedc43d4b
LFDM <1986gh@gmail.com>
2014-06-23 17:25:40 +0200

    Hide relationMultiChanger when we are not editing

--- app/js/arethusa.relation/directives/relationMultiChanger.js
+++ app/js/arethusa.relation/directives/relationMultiChanger.js
@@ -9,6 +9,14 @@ angular.module('arethusa.relation').directive('relationMultiChanger', [
         scope.isPossible = relation.multiChangePossible;
         scope.apply = relation.apply;
         scope.r = relation;
+
+        scope.$watch('r.mode', function(newVal, oldVal) {
+          if (relation.canEdit()) {
+            element.show();
+          } else {
+            element.hide();
+          }
+        });
       },
       templateUrl: 'templates/arethusa.relation/relation_multi_changer.html'
     };
