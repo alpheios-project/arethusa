2add7a5029fd8f8bc4624207090e09c6945a041b
LFDM <1986gh@gmail.com>
2014-06-23 10:45:07 +0200

    Refactor sidepanelFolder

--- app/js/arethusa.core/directives/sidepanel_folder.js
+++ app/js/arethusa.core/directives/sidepanel_folder.js
@@ -1,27 +1,41 @@
 'use strict';
-angular.module('arethusa.core').directive('sidepanelFolder', function () {
-  return {
-    scope: true,
-    link: function (scope, element, attrs) {
-      element.on('click', function () {
-        var get = function (el) {
-          return angular.element(document.getElementById(el));
-        };
-        var main = get('main-body');
-        var panel = get('sidepanel');
-        var width = panel.width();
-        var mainWidth = main.width();
-        if (scope.folded) {
-          main.width(mainWidth - width);
-          panel.show();
-          element.text('Fold Panel');
-        } else {
-          main.width(mainWidth + width);
-          panel.hide();
+angular.module('arethusa.core').directive('sidepanelFolder', [
+  function () {
+    return {
+      scope: true,
+      link: function (scope, element, attrs) {
+        function get(id) {
+          return angular.element(document.getElementById(id));
+        }
+
+        function addShowText() {
           element.text('Show Panel');
         }
-        scope.folded = !scope.folded;
-      });
-    }
-  };
-});
+
+        function addFoldText() {
+          element.text('Fold Panel');
+        }
+
+        function toggleFoldStatus(args) {
+          var main = get('main-body');
+          var panel = get('sidepanel');
+          var width = panel.width();
+          var mainWidth = main.width();
+          if (scope.folded) {
+            main.width(mainWidth - width);
+            panel.show();
+            addFoldText();
+          } else {
+            main.width(mainWidth + width);
+            panel.hide();
+            addShowText();
+          }
+          scope.folded = !scope.folded;
+        }
+        element.on('click', function () {
+          toggleFoldStatus();
+        });
+      }
+    };
+  }
+]);
