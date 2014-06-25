f7a849e3a7137b0f395597b115e01136f692f3e8
Christof Sirk <christof.sirk@gmail.com>
2014-06-25 23:36:01 +0200

    Quick test to get uservoice to open on custom button

--- vendor/uservoice.js
+++ vendor/uservoice.js
@@ -1,5 +1,15 @@
+"use strict";
+
 // Include the UserVoice JavaScript SDK (only needed once on a page)
-UserVoice=window.UserVoice||[];(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/5WVkNniscmKgATqHsxUQ.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
+var UserVoice = window.UserVoice || [];
+(function() {
+  var uv=document.createElement('script');
+  uv.type='text/javascript';
+  uv.async=true;
+  uv.src='//widget.uservoice.com/5WVkNniscmKgATqHsxUQ.js';
+  var s=document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(uv,s);
+})();
 
 //
 // UserVoice Javascript SDK developer documentation:
@@ -7,6 +17,7 @@ UserVoice=window.UserVoice||[];(function(){var uv=document.createElement('script
 //
 
 // Set colors
+/*
 UserVoice.push(['set', {
   accent_color: '#6aba2e',
   trigger_color: 'white',
@@ -30,12 +41,12 @@ UserVoice.push(['identify', {
   //  plan:         'Enhanced' // Plan name for the account
   //}
 }]);
-
+*/
 // Add default trigger to the bottom-right corner of the window:
-UserVoice.push(['addTrigger', { mode: 'contact', trigger_position: 'bottom-right' }]);
+//UserVoice.push(['addTrigger', { mode: 'contact', trigger_position: 'automatic' }]);
 
 // Or, use your own custom trigger:
-//UserVoice.push(['addTrigger', '#id', { mode: 'contact' }]);
+//UserVoice.push(['addTrigger', '#uservoicebutton', { mode: 'contact' }]);
 
 // Autoprompt for Satisfaction and SmartVote (only displayed under certain conditions)
-UserVoice.push(['autoprompt', {}]);
+//UserVoice.push(['autoprompt', {}]);
