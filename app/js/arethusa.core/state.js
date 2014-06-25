4a5f614bec9f87bd7c7b37d8b3f3f451feafe4de
LFDM <1986gh@gmail.com>
2014-06-25 13:44:14 +0200

    Use new keyCapture conf syntax everywhere

--- app/js/arethusa.core/state.js
+++ app/js/arethusa.core/state.js
@@ -320,7 +320,7 @@ angular.module('arethusa.core').service('state', [
 
     function initKeyCaptures() {
       var kC = keyCapture;
-      var conf = kC.conf().selections || {};
+      var conf = kC.conf('selections');
       var nextKey = conf.nextToken || 'J';
       var prevKey = conf.prevToken || 'K';
 
