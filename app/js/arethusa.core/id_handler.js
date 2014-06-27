962dfa3a921fce450c0ca7128300e43294be4a27
LFDM <1986gh@gmail.com>
2014-06-27 09:24:36 +0200

    Add idHandler.formatId()

--- app/js/arethusa.core/id_handler.js
+++ app/js/arethusa.core/id_handler.js
@@ -5,5 +5,18 @@ angular.module('arethusa.core').service('idHandler', [
     this.getId = function(id) {
       return arethusaUtil.formatNumber(id, 4);
     };
+
+    this.formatId = function(id, format) {
+      format = format ? format : '%w';
+      // in case we have an id formatted like 1-1 here
+      var idParts = parseId(id);
+      var wId = arethusaUtil.formatNumber(idParts.pop(), 0);
+      var sId = arethusaUtil.formatNumber(idParts.pop(), 0);
+      return format.replace('%w', wId);
+    };
+
+    function parseId(id) {
+      return id.split('-');
+    }
   }
 ]);
