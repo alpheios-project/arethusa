9264c3395da0bf7825af48f0687017fbdf11e72b
LFDM <1986gh@gmail.com>
2014-07-02 01:25:22 +0200

    Fix sidepanel-folder class and title text

--- app/js/arethusa.core/directives/sidepanel_folder.js
+++ app/js/arethusa.core/directives/sidepanel_folder.js
@@ -6,23 +6,25 @@ angular.module('arethusa.core').directive('sidepanelFolder', [
     return {
       scope: {},
       link: function (scope, element, attrs) {
-        function setIconClass() {
-          var icon = sidepanel.folded ? 'compress' : 'expand';
+        function setIconClassAndText() {
+          var icon = sidepanel.folded ? 'expand' : 'compress';
+          var text = sidepanel.folded ? 'Show' : 'Fold';
           scope.iconClass = 'fi-arrows-' + icon;
+          scope.text = text + " Panel";
         }
 
         element.on('click', function () {
           sidepanel.toggle();
-          setIconClass();
+          scope.$apply(setIconClassAndText());
         });
 
         scope.sp = sidepanel;
 
         scope.$watch('sp.folded', function(newVal, oldVal) {
-          setIconClass();
+          setIconClassAndText();
         });
       },
-      template: '<i ng-class="iconClass"/>'
+      template: '<i title="{{ text }}" ng-class="iconClass"/>'
     };
   }
 ]);
