494cf14955f2bec9b3dfcaebcc81e4d00c25bc1a
LFDM <1986gh@gmail.com>
2014-07-02 11:52:21 +0200

    Start to collapse navbar buttons if need be

--- app/js/arethusa.core/directives/navbar_buttons.js
+++ app/js/arethusa.core/directives/navbar_buttons.js
@@ -4,6 +4,12 @@ angular.module('arethusa.core').directive('navbarButtons', function() {
   return {
     restrict: 'A',
     replace: true,
-    templateUrl: 'templates/arethusa.core/navbar_buttons.html'
+    link: function(scope, element, attrs) {
+      scope.$watch('windowWidth', function(newVal, oldVal) {
+        var coll = newVal > 780 ? '' : '_collapsed';
+        scope.bTemplate = 'templates/arethusa.core/navbar_buttons' + coll + '.html';
+      });
+    },
+    template: '<ul class="has-form button-group right" ng-include="bTemplate"/>'
   };
 });
