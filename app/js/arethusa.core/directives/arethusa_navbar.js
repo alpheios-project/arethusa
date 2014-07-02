df0ba12a95c6f3877cf1d9167afc470ee962b0c2
LFDM <1986gh@gmail.com>
2014-07-02 10:57:46 +0200

    Start to turn off stuff in navbar on small screens

--- app/js/arethusa.core/directives/arethusa_navbar.js
+++ app/js/arethusa.core/directives/arethusa_navbar.js
@@ -20,22 +20,43 @@
  */
 angular.module('arethusa.core').directive('arethusaNavbar', [
   'configurator',
-  function (configurator) {
+  '$window',
+  function (configurator, $window) {
     return {
       restrict: 'AE',
       scope: true,
       link: function (scope, element, attrs) {
         var conf = configurator.configurationFor('navbar');
+
+        var win = angular.element($window);
+        var windowSize;
+
+        function setWindowSize() {
+          windowSize = win.width();
+        }
+
+        function isVisible(threshold, defaultVal) {
+          if (defaultVal) {
+            return windowSize > threshold;
+          }
+        }
+
+        setWindowSize();
+        win.on('resize', setWindowSize);
+
         scope.template = conf.template;
         scope.disable = conf.disable;
+
         scope.showSearch = function () {
           return conf.search;
         };
+
         scope.showNavigation = function () {
           return conf.navigation;
         };
+
         scope.showNotifier = function () {
-          return conf.notifier;
+          return isVisible(1200, conf.notifier);
         };
 
         // Foundation's topbar doesn't seem to work properly - could be an issue
@@ -43,7 +64,7 @@ angular.module('arethusa.core').directive('arethusaNavbar', [
         // is added. We do it manually through this directive.
         angular.element(document.body).css({ padding: '45px'});
       },
-      template: '<div ng-if="! disable" ng-include="template"></div>'
+      template: '<div ng-if="!disable" ng-include="template"></div>'
     };
   }
 ]);
