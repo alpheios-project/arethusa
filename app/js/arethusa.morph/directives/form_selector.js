5f3ac7d74c302bb2a053f7e4c7dac7102cdf5906
LFDM <1986gh@gmail.com>
2014-07-02 18:44:25 +0200

    Refactor formSelector

--- app/js/arethusa.morph/directives/form_selector.js
+++ app/js/arethusa.morph/directives/form_selector.js
@@ -1,31 +1,38 @@
 'use strict';
 angular.module('arethusa.morph').directive('formSelector', function () {
   return {
-    restrict: 'AE',
-    replace: true,
-    controller: [
-      '$scope',
-      '$element',
-      '$attrs',
-      function ($scope, $element, $attrs) {
-        var id = $scope.id;
-        var form = $scope.form;
-        $scope.selected = function () {
-          return $scope.plugin.isFormSelected(id, form);
-        };
-        $scope.text = function () {
-          return $scope.selected() ? 'Deselect' : 'Select';
-        };
-        $scope.action = function (event) {
-          event.stopPropagation();
-          if ($scope.selected()) {
-            $scope.plugin.unsetState(id);
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      var id = scope.id;
+      var form = scope.form;
+
+      scope.isSelected = function() {
+        return scope.plugin.isFormSelected(id, form);
+      };
+
+      scope.text = function () {
+        return scope.isSelected() ? 'D' : 'S';
+      };
+
+      function action(event) {
+        event.stopPropagation();
+        scope.$apply(function() {
+          if (scope.isSelected()) {
+            scope.plugin.unsetState(id);
           } else {
-            $scope.plugin.setState(id, form);
+            scope.plugin.setState(id, form);
           }
-        };
+        });
       }
-    ],
-    template: '<span class="button micro radius"' + 'ng-click="action($event)"' + 'ng-class="{success: selected()}">' + '{{ text() }}' + '</span>'
+
+      element.bind('click', action);
+    },
+    template: '\
+      <span\
+        class="button micro radius"\
+        ng-class="{success: isSelected()}">\
+         {{ text() }}\
+      </span>\
+    '
   };
 });
