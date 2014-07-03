0559a96d2d1b66aa7cb01b292fbc77a20b6fa030
lichtr <robert.lichtensteiner@gmail.com>
2014-07-03 23:16:32 +0200

    Create greek-key directive

---
+++ app/js/arethusa.core/directives/greek_keys.js
@@ -0,0 +1,16 @@
+'use strict';
+angular.module('arethusa.core').directive('greekKeys',[
+  'keyCapture',
+  function (keyCapture) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        element.on('keydown', function (event) {
+          var input = event.target.value;
+          var gr = keyCapture.getGreekKey(event);
+          event.target.value = input + gr;
+        });
+      }
+    };
+  }
+]);
