ae8e63a8d1a3ac515f6a3a5f7ee6865d7ff28461
Christof Sirk <christof.sirk@gmail.com>
2014-07-06 17:55:48 +0200

    Load grunt tasks automatically with load-grunt-tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,16 +27,7 @@ function pluginFiles(name) {
 }
 
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-karma');
-  grunt.loadNpmTasks('grunt-protractor-runner');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-coveralls');
-  grunt.loadNpmTasks('grunt-sauce-connect-launcher');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-concurrent');
+  require('load-grunt-tasks')(grunt);
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jasmine: {
