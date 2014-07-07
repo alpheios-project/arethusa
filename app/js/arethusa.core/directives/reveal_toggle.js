eda9e7dd01f7fb4beeedeefbc5fd9837e64c8d58
LFDM <1986gh@gmail.com>
2014-07-07 10:30:27 +0200

    Add optional slide attribute in revealToggle
    Used jQuery to add a slideDown/slideUp animation.

--- app/js/arethusa.core/directives/reveal_toggle.js
+++ app/js/arethusa.core/directives/reveal_toggle.js
@@ -6,6 +6,8 @@ angular.module('arethusa.core').directive('revealToggle', function() {
     link: function(scope, element, attrs) {
       var tId = attrs.revealToggle;
       var alwaysReveal = attrs.alwaysReveal;
+      var slide = attrs.slide;
+
 
       function el() {
         return angular.element(document.getElementById(tId));
@@ -14,9 +16,11 @@ angular.module('arethusa.core').directive('revealToggle', function() {
       element.bind('click', function() {
         var t = el();
         if (alwaysReveal || t.hasClass('hide')) {
+          if (slide) t.slideDown();
           t.removeClass('hide');
           t.trigger('show-' + tId);
         } else {
+          if (slide) t.slideUp();
           t.addClass('hide');
           t.trigger('hide-' + tId);
         }
