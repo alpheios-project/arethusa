6c6706af9000854eef78641ea52d3b540091ea14
LFDM <1986gh@gmail.com>
2014-07-08 19:35:11 +0200

    More ugly id parsing

--- app/js/arethusa.core/id_handler.js
+++ app/js/arethusa.core/id_handler.js
@@ -14,15 +14,21 @@ angular.module('arethusa.core').service('idHandler', [
       format = format ? format : '%w';
       // in case we have an id formatted like 1-1 here
       var idParts = parseId(id);
-      var wId = arethusaUtil.formatNumber(idParts.pop(), 0);
+      var wId = idParts.pop();
+      var wParts = wIdParts(wId);
+      var wIdNumericPart = arethusaUtil.formatNumber(wParts[1]);
       var sId = arethusaUtil.formatNumber(idParts.pop(), 0);
-      return format.replace('%w', wId);
+      return format.replace('%w', wIdNumericPart + (wParts[2] || ''));
     };
+
+    function wIdParts(wId) {
+      return /(\d*)(\w*)?/.exec(wId);
+    }
 
     this.decrement = function(id) {
       var idParts = parseId(id);
       var wId = idParts.pop();
-      var wParts = /(\d*)(\w*)?/.exec(wId);
+      var wParts = wIdParts(wId);
       var newWId = wParts[1] - 1;
       return self.getId(newWId) + (wParts[2] || '');
     };
