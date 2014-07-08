eacb962c93f4ed68838ea50c53e0744af52e67c5
LFDM <1986gh@gmail.com>
2014-07-08 12:12:19 +0200

    Tiny steps in artificialToken

--- app/js/arethusa.artificial_token/artificial_token.js
+++ app/js/arethusa.artificial_token/artificial_token.js
@@ -8,9 +8,29 @@ angular.module('arethusa.artificialToken').service('artificialToken', [
 
     function configure() {
       configurator.getConfAndDelegate('artificialToken', self);
+      self.model = new ArtificialToken();
     }
 
     configure();
+
+    function ArtificialToken (string, type) {
+      this.string = string;
+      this.type   = type || 'elliptic';
+    }
+
+    this.supportedTypes = [
+      'elliptic'
+    ];
+
+
+    this.setType = function(type) {
+      self.model.type = type;
+    };
+
+    this.hasType = function(type) {
+      return self.model.type === type;
+    };
+
 
     this.init = function() {
       configure();
