102d9a08afd8202134830bb11c4d4a2eca4d52ee
LFDM <1986gh@gmail.com>
2014-07-11 22:34:34 +0200

    Add watches to searchByString directive

--- app/js/arethusa.search/directives/search_by_string.js
+++ app/js/arethusa.search/directives/search_by_string.js
@@ -2,12 +2,54 @@
 
 angular.module('arethusa.search').directive('searchByString', [
   'search',
-  function(search) {
+  'state',
+  function(search, state) {
     return {
       restrict: 'A',
       scope: {},
       link: function(scope, element, attrs) {
         scope.search = search;
+        scope.state = state;
+
+        // Mind that the following watches aren't active all the time!
+        // When it is used from within the search plugins template, and it
+        // is inactive through ngIf, they won't fire. This is generally a good
+        // thing and we won't delete this code: We can still use it on isolation,
+        // or we might at some point display it together with plugins that can
+        // edit text.
+        // Right now only the artificialToken plugin does this: Both are never
+        // shown at the same time, which means this watches actually never fire
+        // right now.
+
+        var stringWatches = {};
+        function initStringWatch(token, id) {
+          var childScope = scope.$new();
+          childScope.token = token;
+          childScope.$watch('token.string', function(newVal, oldVal) {
+            if (newVal !== oldVal) {
+              search.removeTokenFromIndex(token.id, oldVal);
+              search.collectTokenString(search.strings, id, token);
+            }
+          });
+          stringWatches[id] = childScope;
+        }
+
+        function initStringWatches() {
+          angular.forEach(state.tokens, initStringWatch);
+        }
+
+        function removeStringWatch(scope) {
+          scope.$destroy();
+        }
+
+        function destroyStringWatch() {
+          angular.forEach(stringWatches, removeStringWatch);
+          stringWatches = {};
+        }
+
+        scope.$watch('state.tokens', function(newVal, oldVal) {
+          initStringWatches();
+        });
       },
       templateUrl: 'templates/arethusa.search/search_by_string.html'
     };
