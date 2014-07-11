39c50e3a03cd4c51b86203f1b29b3370fc58cb41
LFDM <1986gh@gmail.com>
2014-07-11 22:51:18 +0200

    Update text service

--- app/js/arethusa.text/text.js
+++ app/js/arethusa.text/text.js
@@ -4,6 +4,28 @@ angular.module('arethusa.text').service('text', [
   'configurator',
   function (state, configurator) {
     var self = this;
-    configurator.getConfAndDelegate('text', self);
+
+    function configure() {
+      configurator.getConfAndDelegate('text', self);
+      self.hideArtificialTokens = false;
+    }
+
+    configure();
+
+    function selectRealTokens() {
+      return arethusaUtil.inject({}, state.tokens, function(memo, id, token) {
+        if (!token.artificial) {
+          memo[id] = token;
+        }
+      });
+    }
+
+    function setTokens() {
+      self.tokens = self.hideArtificialTokens ? selectRealTokens() : state.tokens;
+    }
+
+    this.init = function() {
+      setTokens();
+    };
   }
 ]);
