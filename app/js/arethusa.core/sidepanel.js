c154c174dcd1391603d369ac51492da05dc40564
LFDM <1986gh@gmail.com>
2014-07-12 12:36:26 +0200

    Fix #196
    Check the long comment contained in this commit for an explanation.

--- app/js/arethusa.core/sidepanel.js
+++ app/js/arethusa.core/sidepanel.js
@@ -29,6 +29,32 @@ angular.module('arethusa.core').service('sidepanel', [
     }
 
     this.toggle = function() {
+
+      // This very weird looking line countering a spacing bug
+      // in Foundation.
+      // When the sidepanel is placed in a columns row together
+      // with a main-body no explicit width is assigned to it at
+      // first. The main-body gets a width, the sidepanel just
+      // takes the rest of the available space.
+      // This works fine - problems arise when the sidepanel is
+      // hidden. As no explicit width has been set, Foundation
+      // tries to recalculate the size of the sidepanel when it
+      // gets visible again - and it does this calculation wrong.
+      //
+      // This workaround sets the width explicitly. panel.width()
+      // returns the initial width value Foundation calculated,
+      // which is absolutely correct. By setting the width to
+      // exactly this value, the element writes it explicitly into
+      // its style attributes. Once this is done, Foundation seems
+      // not to recalculate this value again when the element
+      // gets visiable again after being hidden. Exactly what we need!
+      //
+      // We cannot call this method on init - Angular cannot guarantee
+      // its DOM load order - and indeed, on init the behaviour is
+      // unstable - it might work or might not.
+      // We just do it on every toggle. Is redundant most of time,
+      // but it seems to fix our problem at least...
+      panel.width(panel.width());
       if (self.folded) show(); else hide();
       self.folded = !self.folded;
     };
