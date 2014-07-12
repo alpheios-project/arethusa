0a0ad31a7ce5a2d0ed64f5ed03c41da212ef588c
LFDM <1986gh@gmail.com>
2014-07-12 20:36:41 +0200

    Start to parse the morph

--- app/js/arethusa.hebrew_morph/hebrew_morph.js
+++ app/js/arethusa.hebrew_morph/hebrew_morph.js
@@ -12,6 +12,57 @@ angular.module('arethusa.hebrewMorph').service('hebrewMorph', [
 
     configure();
 
+    function Form(id, score) {
+      this.id = id;
+      this.score = score;
+    }
+
+    function Morph(string) {
+      this.string = string;
+      this.forms = [];
+    }
+
+    function parsePrefix(form) {
+      var prefix = form.prefix;
+      var res = {};
+      if (prefix) {
+        res.string = prefix._surface;
+      }
+      return res;
+    }
+    function parseSuffix(form) {
+      var suffix = form.suffix;
+      var res = {};
+      return res;
+    }
+    function parseBase(form) {
+      var base = form.base;
+      var res = {};
+      if (base) {
+        res.string = base._lexiconItem;
+      }
+      return res;
+    }
+
+    this.parse = function(xmlToken, token) {
+      var morph = new Morph(token.string);
+      token.morphology = morph;
+      var analyses = arethusaUtil.toAry(xmlToken.analysis);
+      angular.forEach(analyses, function(anal, i) {
+        var form = new Form(anal._id, anal._score);
+        form.base = parseBase(anal);
+        form.prefix = parsePrefix(anal);
+        form.suffix = parseSuffix(anal);
+        morph.forms.push(form);
+      });
+    };
+
+    this.currentSelection = function() {
+      return arethusaUtil.inject({}, state.selectedTokens, function(memo, id, type) {
+        memo[id] = state.getToken(id).morphology;
+      });
+    };
+
     this.init = function() {
       configure();
     };
