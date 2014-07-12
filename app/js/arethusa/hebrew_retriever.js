1f9b8153b567424f83df29ea4b48b51d0aff3975
LFDM <1986gh@gmail.com>
2014-07-12 19:34:03 +0200

    Use idMap in hebrewRetriever

--- app/js/arethusa/hebrew_retriever.js
+++ app/js/arethusa/hebrew_retriever.js
@@ -5,16 +5,27 @@ angular.module('arethusa').factory('HebrewRetriever', [
   'configurator',
   'idHandler',
   'languageSettings',
-  function (documentStore, configurator, idHandler, languageSettings) {
+  'hebrewMorph',
+  function (documentStore, configurator, idHandler, languageSettings, hebrewMorph) {
     return function (conf) {
       var self = this;
       var resource = configurator.provideResource(conf.resource);
       var docIdentifier = conf.docIdentifier;
 
-      function Token(id, string, terminator) {
+      function Token(id, string, map, terminator) {
         this.id = id;
         this.string = string;
+        this.idMap = map;
         this.terminator = terminator;
+      }
+
+      function createIds(id) {
+        var match = /^ref\.(\d+\.){3}(\d+)\.(\d+)$/.exec(id);
+        var internalId = match[2] + '-' + match[3];
+        var sourceId   = id;
+        var idMap = new idHandler.Map();
+        idMap.add(docIdentifier, internalId, sourceId);
+        return { map: idMap, id: internalId };
       }
 
       function extractTokens(paragraph) {
@@ -24,10 +35,10 @@ angular.module('arethusa').factory('HebrewRetriever', [
           var tokens = arethusaUtil.toAry(sentence.token);
           var lastTokenI = tokens.length - 1;
           angular.forEach(tokens, function(token, otherI) {
-            var id = token._id;
+            var ids = createIds(token._id);
             var string = token._surface;
             var term = otherI === lastTokenI;
-            result[id] = new Token(id, string, term);
+            result[ids.id] = new Token(ids.id, string, ids.map, term);
           });
         });
 
