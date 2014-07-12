c7a2f685fd5d48e3ca7d35400d5edeac8b4165a9
LFDM <1986gh@gmail.com>
2014-07-12 15:20:44 +0200

    Delegate ngModel and ngChange in greekKeys

--- app/js/arethusa.core/directives/greek_keys.js
+++ app/js/arethusa.core/directives/greek_keys.js
@@ -5,9 +5,14 @@ angular.module('arethusa.core').directive('greekKeys',[
   function (keyCapture, languageSettings) {
     return {
       restrict: 'A',
+      scope: {
+        ngChange: '&',
+        ngModel: '@'
+      },
       link: function (scope, element, attrs) {
-        element.on('keydown', function (event) {
+        var parent = scope.$parent;
 
+        element.on('keydown', function (event) {
           var lang = (languageSettings.getFor('treebank') || {}).lang;
           var input = event.target.value;
           if (lang == "gr") {
@@ -19,6 +24,10 @@ angular.module('arethusa.core').directive('greekKeys',[
               return true;
             } else {
               event.target.value = input + gr;
+              scope.$apply(function() {
+                parent.$eval(scope.ngModel + ' = i + g', { i: input, g: gr });
+                scope.ngChange();
+              });
               return false;
             }
           } else {
