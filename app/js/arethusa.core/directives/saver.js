cd30c2b5ab8fd9af315f92442996dcc4fa4efbb2
LFDM <1986gh@gmail.com>
2014-07-18 22:44:16 +0200

    Only add save watch if we really need it

--- app/js/arethusa.core/directives/saver.js
+++ app/js/arethusa.core/directives/saver.js
@@ -7,17 +7,28 @@ angular.module('arethusa.core').directive('saver', [
       restrict: 'A',
       link: function(scope, element, attrs) {
         scope.saver = saver;
+        var saveWatch;
 
         element.bind('click', function() {
           scope.$apply(saver.save());
         });
 
         scope.$watch('saver.canSave', function(newVal, oldVal) {
-          if (newVal) element.show(); else element.hide();
+          if (newVal) {
+            element.show();
+            addSaveWatch();
+          } else {
+            element.hide();
+            removeSaveWatch();
+          }
         });
 
-        scope.$watch('saver.needsSave', function(newVal, oldVal) {
-          if (saver.canSave) {
+        function addSaveWatch() {
+          // Safety measure, so that we never register the watch twice, which
+          // might never happen anyway, but who knows.
+          removeSaveWatch();
+
+          saveWatch = scope.$watch('saver.needsSave', function(newVal, oldVal) {
             if (newVal) {
               element.addClass('alert');
               element.removeClass('disabled');
@@ -25,8 +36,12 @@ angular.module('arethusa.core').directive('saver', [
               element.addClass('disabled');
               element.removeClass('alert');
             }
-          }
-        });
+          });
+        }
+
+        function removeSaveWatch() {
+          if (saveWatch) saveWatch();
+        }
       }
     };
   }
