584b60650d2825bb6b8494fa7d8b9a39a3161801
LFDM <1986gh@gmail.com>
2014-07-19 09:32:29 +0200

    Refactor TreebankPersister and update spec

--- spec/services/treebank_persister_spec.js
+++ spec/services/treebank_persister_spec.js
@@ -3,6 +3,8 @@
 describe('treebank persister', function() {
   var $httpBackend;
   var documentStore;
+  var TreebankPersister;
+
   var mockConfigurator = {
     configurationFor: function(name) {
       return {};
@@ -52,8 +54,9 @@ describe('treebank persister', function() {
     $provide.value('navigator', mockNavigator);
   }));
 
-  beforeEach(inject(function($injector, _documentStore_) {
+  beforeEach(inject(function($injector, _documentStore_, _TreebankPersister_) {
     $httpBackend = $injector.get('$httpBackend');
+    TreebankPersister = _TreebankPersister_;
     documentStore = _documentStore_;
     documentStore.addDocument('some-treebank', {
       json: {
@@ -83,19 +86,45 @@ describe('treebank persister', function() {
     });
   }));
 
-  it('save data', inject(function(TreebankPersister) {
-    var conf = {
-      'resource' : 'test-resource',
-      'docIdentifier' : 'some-treebank'
-    };
-    var persister = new TreebankPersister(conf);
+  describe('this.saveData()', function() {
+    it('saves data when when changes are present', function() {
+      var conf = {
+        'resource' : 'test-resource',
+        'docIdentifier' : 'some-treebank'
+      };
+      var persister = new TreebankPersister(conf);
 
-    expect(persister).toBeDefined();
 
-    persister.saveData(function() {});
+      expect(persister).toBeDefined();
 
-    var updatedJson = documentStore.store['some-treebank'];
-    expect(updatedJson).toBeDefined();
-    expect(updatedJson.json.treebank.sentence.word[0]._head).toBe(2);
-  }));
+      mockNavigator.sentencesById['2'].changed = true;
+
+      persister.saveData(function() {});
+
+      var updatedJson = documentStore.store['some-treebank'];
+      expect(updatedJson).toBeDefined();
+      expect(updatedJson.json.treebank.sentence.word[0]._head).toBe(2);
+    });
+
+    it('does not update when chunk is not marked as changed', function() {
+      var conf = {
+        'resource' : 'test-resource',
+        'docIdentifier' : 'some-treebank'
+      };
+      var persister = new TreebankPersister(conf);
+
+
+      expect(persister).toBeDefined();
+
+      mockNavigator.sentencesById['2'].changed = false;
+
+      var updatedJson = documentStore.store['some-treebank'];
+      var valBeforeSave = updatedJson.json.treebank.sentence.word[0]._head;
+
+      persister.saveData(function() {});
+
+      expect(updatedJson).toBeDefined();
+      expect(updatedJson.json.treebank.sentence.word[0]._head).toBe(valBeforeSave);
+    });
+  });
 });
