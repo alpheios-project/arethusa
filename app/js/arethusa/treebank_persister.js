64d437c15b9a0ca53b9a6dcffa206f84531f2635
LFDM <1986gh@gmail.com>
2014-07-19 09:23:45 +0200

    Only update sentences with changes in persister

--- app/js/arethusa/treebank_persister.js
+++ app/js/arethusa/treebank_persister.js
@@ -55,6 +55,9 @@ angular.module('arethusa').factory('TreebankPersister', [
 
         angular.forEach(stored, function(sentence, i) {
           var updated = sentences[sentence._id];
+
+          if (!updated.changed) return;
+
           var wordsInXml = arethusaUtil.toAry(sentence.word);
           // We create a new object that holds all tokens of a sentence,
           // identified by their mappings in the original document.
