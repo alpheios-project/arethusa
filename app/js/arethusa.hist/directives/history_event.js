f0a50dd9feaa66c880bcaed3b6eaa5066b8f3176
LFDM <1986gh@gmail.com>
2014-07-19 23:34:39 +0200

    Update historyEvent for added and removed tokens

--- app/js/arethusa.hist/directives/history_event.js
+++ app/js/arethusa.hist/directives/history_event.js
@@ -4,6 +4,12 @@ angular.module('arethusa.hist').directive('historyEvent', [
   'idHandler',
   '$compile',
   function(idHandler, $compile) {
+    var predicates = {
+      change: 'Changed',
+      add: 'Added',
+      remove: 'Removed'
+    };
+
     return {
       restrict: 'A',
       scope: {
@@ -25,13 +31,17 @@ angular.module('arethusa.hist').directive('historyEvent', [
           return val || 'nothing';
         }
 
-        scope.blocked = false;
-
         scope.token = scope.event.token;
         scope.id    = scope.token.id;
-        scope.property = scope.event.property;
-        scope.oldVal = valToString(scope.event.oldVal);
-        scope.newVal = valToString(scope.event.newVal);
+        scope.type  = scope.event.type;
+        scope.predicate = predicates[scope.type];
+
+        if (scope.type === 'change') {
+          scope.blocked = false;
+          scope.property = scope.event.property;
+          scope.oldVal = valToString(scope.event.oldVal);
+          scope.newVal = valToString(scope.event.newVal);
+        }
 
         scope.formatId = function(id) {
           return idHandler.formatId(id, '%w');
