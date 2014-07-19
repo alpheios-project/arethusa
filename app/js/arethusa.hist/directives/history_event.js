136eea4c7a048ef1231b22a93cd12d6085d895c8
LFDM <1986gh@gmail.com>
2014-07-19 16:14:52 +0200

    Fix some confusion with leftover files

--- app/js/arethusa.hist/directives/history_event.js
+++ app/js/arethusa.hist/directives/history_event.js
@@ -6,11 +6,13 @@ angular.module('arethusa.hist').directive('historyEvent', [
     return {
       restrict: 'A',
       scope: {
-        event: '=historyElement'
+        event: '=historyEvent'
       },
       link: function(scope, element, attrs) {
         scope.token = scope.event.token;
         scope.id    = scope.token.id;
+        scope.oldVal = scope.event.oldVal;
+        scope.newVal = scope.event.newVal;
         scope.formatId = function(id) {
           return idHandler.formatId(id, '%w');
         };
