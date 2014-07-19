12c400ad6134139c0e9e36309c871ce034af63ef
LFDM <1986gh@gmail.com>
2014-07-19 16:03:19 +0200

    Remove message handling in StateChange

--- app/js/arethusa.core/state_change.js
+++ app/js/arethusa.core/state_change.js
@@ -11,15 +11,6 @@ angular.module('arethusa.core').factory('StateChange', [
       }
     }
 
-    function buildMessage(token, property, newVal, oldVal) {
-      // We should check here if newVal and oldVal are objects, otherwise
-      // the user won't see anything helpful. Ideally these objects come
-      // with a toString function we could check for?
-      return "Changed " + property +" of " +
-        token.string + " at " + token.id +
-        " from " +oldVal +" to " + newVal;
-    }
-
     return function(state, tokenOrId, property, newVal, undoFn, preExecFn) {
       var self = this;
 
@@ -31,8 +22,6 @@ angular.module('arethusa.core').factory('StateChange', [
       this.newVal = newVal;
       this.oldVal = get(self.token);
       this.time = new Date();
-
-      this.message = buildMessage(self.token, self.property, self.newVal, self.oldVal);
 
       function inverse() {
         state.change(self.token, property, self.oldVal);
