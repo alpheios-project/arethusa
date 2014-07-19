790ba141f9e2ba943f414df4d9ecd3bab5691bec
LFDM <1986gh@gmail.com>
2014-07-19 12:09:12 +0200

    Refactor uTH to use the new and mighty state.watch()

--- app/js/arethusa.dep_tree/directives/unused_token_highlighter.js
+++ app/js/arethusa.dep_tree/directives/unused_token_highlighter.js
@@ -11,7 +11,6 @@ angular.module('arethusa.depTree').directive('unusedTokenHighlighter', [
       },
       link: function(scope, element, attrs) {
         var unusedTokens;
-        var headWatches = [];
         var style = scope.style || { "background-color": "rgb(255, 216, 216)" }; // a very light red
         var highlightMode = !!scope.highlightMode;
         scope.s = state;
@@ -37,48 +36,27 @@ angular.module('arethusa.depTree').directive('unusedTokenHighlighter', [
           angular.forEach(state.tokens, checkIfUnused);
         }
 
-        function initHeadWatch(token, id) {
-          var childScope = scope.$new();
-          childScope.head = token.head;
-          childScope.id   = id;
-          childScope.$watch('head.id', function(newVal, oldVal) {
-            if (newVal !== oldVal) {
-              if (newVal) {
-                // Check if the token was used before!
-                if (!oldVal) {
-                  scope.unusedCount--;
-                  delete unusedTokens[id];
-                  if (highlightMode) removeStyle(id);
-                }
-              } else {
-                scope.unusedCount++;
-                unusedTokens[id] = true;
-                if (highlightMode) state.addStyle(id, style);
-              }
+        state.watch('head.id', function(newVal, oldVal, event) {
+          var id = event.token.id;
+          if (newVal) {
+            // Check if the token was used before!
+            if (!oldVal) {
+              scope.unusedCount--;
+              delete unusedTokens[id];
+              if (highlightMode) removeStyle(id);
             }
-          });
-          headWatches.push(childScope);
-        }
-
-        function initHeadWatches() {
-          destroyOldHeadWatches();
-          angular.forEach(state.tokens, initHeadWatch);
-        }
-
-        function destroyOldHeadWatches() {
-          angular.forEach(headWatches, function(childScope, i) {
-            childScope.$destroy();
-          });
-          headWatches = [];
-        }
-
+          } else {
+            scope.unusedCount++;
+            unusedTokens[id] = true;
+            if (highlightMode) state.addStyle(id, style);
+          }
+        });
 
         function init() {
           scope.total = state.totalTokens;
           scope.unusedCount = 0;
           unusedTokens = {};
           findUnusedTokens();
-          initHeadWatches();
           if (highlightMode) applyHighlighting();
         }
 
@@ -118,7 +96,6 @@ angular.module('arethusa.depTree').directive('unusedTokenHighlighter', [
           var id = token.id;
           scope.total++;
           checkIfUnused(token, id);
-          initHeadWatch(token, id);
         });
 
         scope.$on('tokenRemoved', function(event, token) {
