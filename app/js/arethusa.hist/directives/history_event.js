d8f33701d779fe923ee8b4a4ee0f15ba4fd8cf31
LFDM <1986gh@gmail.com>
2014-07-19 16:29:10 +0200

    Add some compile magic to historyEvent

--- app/js/arethusa.hist/directives/history_event.js
+++ app/js/arethusa.hist/directives/history_event.js
@@ -2,20 +2,35 @@
 
 angular.module('arethusa.hist').directive('historyEvent', [
   'idHandler',
-  function(idHandler) {
+  '$compile',
+  function(idHandler, $compile) {
     return {
       restrict: 'A',
       scope: {
         event: '=historyEvent'
       },
       link: function(scope, element, attrs) {
+        var tokenTemplate = '\
+          <span\
+            class="text"\
+            token="token"\
+            colorize="true"\
+            click="true"\
+            hover="true">\
+          </span>\
+        ';
+
         scope.token = scope.event.token;
         scope.id    = scope.token.id;
-        scope.oldVal = scope.event.oldVal;
-        scope.newVal = scope.event.newVal;
+        scope.property = scope.event.property;
+        scope.oldVal = scope.event.oldVal || 'nothing';
+        scope.newVal = scope.event.newVal || 'nothing';
+
         scope.formatId = function(id) {
           return idHandler.formatId(id, '%w');
         };
+
+        element.find('#token').append($compile(tokenTemplate)(scope));
       },
       templateUrl: 'templates/arethusa.hist/history_event.html'
     };
