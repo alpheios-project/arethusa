b1ee920078c5cfebf96f7c9c3173a549f73d2639
LFDM <1986gh@gmail.com>
2014-07-19 14:49:39 +0200

    Add keyCaptures to undo and redo

--- app/js/arethusa.hist/history.js
+++ app/js/arethusa.hist/history.js
@@ -1,7 +1,8 @@
 'use strict';
 angular.module('arethusa.hist').service('history', [
   'configurator',
-  function (configurator) {
+  'keyCapture',
+  function (configurator, keyCapture) {
     var self = this;
 
     function configure() {
@@ -9,28 +10,43 @@ angular.module('arethusa.hist').service('history', [
       self.maxSize = self.maxSize || 20;
     }
 
+    this.undo = function() {
+    };
+
+    this.redo = function() {
+    };
+
+    keyCapture.initCaptures(function(kC) {
+      return {
+        history: [
+          kC.create('undo', self.undo),
+          kC.create('redo', self.redo)
+        ]
+      };
+    });
+
     this.init = function() {
       configure();
     };
 
     /* global HistoryObj */
-    var hist = new HistoryObj(this.maxSize);
-    this.history = hist.elements;
-    this.save = function (event) {
-      hist.save(event);
-    };
-    this.undo = function () {
-      hist.undo();
-    };
-    this.canBeUndone = function () {
-      return hist.canBeUndone();
-    };
-    this.redo = function () {
-      hist.redo();
-    };
-    this.canBeRedone = function () {
-      return hist.canBeRedone();
-    };
-    this.catchEvent = this.save;
+    //var hist = new HistoryObj(this.maxSize);
+    //this.history = hist.elements;
+    //this.save = function (event) {
+      //hist.save(event);
+    //};
+    //this.undo = function () {
+      //hist.undo();
+    //};
+    //this.canBeUndone = function () {
+      //return hist.canBeUndone();
+    //};
+    //this.redo = function () {
+      //hist.redo();
+    //};
+    //this.canBeRedone = function () {
+      //return hist.canBeRedone();
+    //};
+    //this.catchEvent = this.save;
   }
 ]);
