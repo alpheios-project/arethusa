fe6b05dac1db472464149ff78cb62e2d16031326
LFDM <1986gh@gmail.com>
2014-07-20 13:05:22 +0200

    Adjust navbar collapse a little

--- app/js/arethusa.core/directives/arethusa_navbar.js
+++ app/js/arethusa.core/directives/arethusa_navbar.js
@@ -40,7 +40,7 @@ angular.module('arethusa.core').directive('arethusaNavbar', [
         }
 
         function setLogo() {
-          var icon = scope.windowWidth > 1100 ? '' : 'icon-';
+          var icon = scope.windowWidth > 1140 ? '' : 'icon-';
           scope.logo = "images/arethusa-" + icon + "small.png";
         }
 
