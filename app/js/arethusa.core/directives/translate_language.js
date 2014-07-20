f5b349c1c95acb0759e9aa4796323b1acd100527
LFDM <1986gh@gmail.com>
2014-07-20 12:44:23 +0200

    Normalize langKeys ins translateLanguage
    We don't make a difference between e.g. en_US and en for now.

--- app/js/arethusa.core/directives/translate_language.js
+++ app/js/arethusa.core/directives/translate_language.js
@@ -7,7 +7,14 @@ angular.module('arethusa.core').directive('translateLanguage', [
       restrict: 'A',
       scope: {},
       link: function(scope, element, attrs) {
-        scope.langKey = $translate.use();
+        function normalizedCurrentKey(key) {
+          return key.split('_')[0];
+        }
+
+        function useKey(key) {
+          scope.langKey = key || normalizedCurrentKey($translate.use());
+          $translate.use(scope.langKey);
+        }
 
         var langs = ['en', 'de'];
 
@@ -15,14 +22,14 @@ angular.module('arethusa.core').directive('translateLanguage', [
           var i;
           i = langs.indexOf(scope.langKey) + 1;
           i = i > langs.length - 1 ? 0 : i;
-          var lang = langs[i];
-          scope.langKey = lang;
-          $translate.use(lang);
+          useKey(langs[i]);
         }
 
         element.bind('click', function() {
           scope.$apply(toggleLang);
         });
+
+        useKey();
       },
       templateUrl: 'templates/arethusa.core/translate_language.html'
     };
