c5a455c82312304e11de13c8f54172a1c38087af
LFDM <1986gh@gmail.com>
2014-07-21 11:17:25 +0200

    Refactor deselector directive

--- app/js/arethusa.core/directives/deselector.js
+++ app/js/arethusa.core/directives/deselector.js
@@ -4,12 +4,16 @@ angular.module('arethusa.core').directive('deselector', [
   function (state) {
     return {
       restrict: 'AE',
+      scope: {},
       link: function (scope, element, attrs) {
         element.bind('click', function (e) {
           state.deselectAll();
           scope.$apply();
         });
-      }
+        var hint = arethusaUtil.formatKeyHint(state.activeKeys.deselect);
+        scope.title = "Deselect all " + hint;
+      },
+      template: '<i title="{{ title }}" class="fi-unlock"/>'
     };
   }
 ]);
