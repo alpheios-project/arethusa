682f30f7a49866169b51fc9b8959817ebd488711
LFDM <1986gh@gmail.com>
2014-07-24 17:12:04 +0200

    Add exitDirective

---
+++ app/js/arethusa.core/directives/exit.js
@@ -0,0 +1,29 @@
+"use strict";
+
+angular.module('arethusa.core').directive('exit', [
+  'exitHandler',
+  function(exitHandler) {
+    return {
+      restrict: 'A',
+      scope: {},
+      link: function(scope, element, attrs) {
+        if (exitHandler.defined) {
+          element.bind('click', function(event) {
+            var targetWin;
+            var button = event.button;
+            // Mirroring the different behavior of left and middle click
+            if (button === 0) targetWin = "_self";
+            if (button === 1) targetWin = "_blank";
+
+            scope.$apply(exitHandler.leave(targetWin));
+          });
+          element.attr("title", "Exit to " + exitHandler.title);
+        } else {
+          element.hide(); // or even remove?
+        }
+
+      },
+      template: '<i class="fa fa-sign-out"></i>'
+    };
+  }
+]);
