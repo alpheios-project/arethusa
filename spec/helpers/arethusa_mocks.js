231859cb7fc91fd8ceaf4205080fc1bba6ab08ba
LFDM <1986gh@gmail.com>
2014-07-24 21:53:15 +0200

    Allow customizations to arethusaMocks

--- spec/helpers/arethusa_mocks.js
+++ spec/helpers/arethusa_mocks.js
@@ -4,19 +4,23 @@ function ArethusaMocks() {
   var mockFn = function () {};
   var mockFnWithObject = function() { return {}; };
 
-  this.configurator = function() {
-    return {
+  function withCustomizations(obj, overrides) {
+    return angular.extend(obj, overrides);
+  }
+
+  this.configurator = function(customizations) {
+    return withCustomizations({
       getConfAndDelegate: mockFn,
       provideResource: mockFnWithObject,
       configurationFor: mockFnWithObject,
       getRetrievers: mockFnWithObject
-    };
+    }, customizations);
   };
 
-  this.keyCapture = function() {
-    return {
+  this.keyCapture = function(customizations) {
+    return withCustomizations({
       initCaptures: mockFnWithObject
-    };
+    }, customizations);
   };
 }
 
