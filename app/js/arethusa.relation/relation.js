97e073e017bc6338cfe18fb276961dc1922771ce
LFDM <1986gh@gmail.com>
2014-07-29 09:06:45 +0200

    Access state.change in relation service

--- app/js/arethusa.relation/relation.js
+++ app/js/arethusa.relation/relation.js
@@ -23,13 +23,14 @@ angular.module('arethusa.relation').service('relation', [
     };
 
     // Label handling
-    function splitLabel(relation) {
-      var split = relation.label.split('_');
+    function splitLabel(relation, label) {
+      label = angular.isDefined(label) ? label : relation.label;
+      var split = label.split('_');
       relation.prefix = split[0] || '';
       relation.suffix = split[1] || '';
     }
 
-    this.buildLabel = function (relation) {
+    this.buildLabel = function (relation, doNotSet) {
       var elements = [
           relation.prefix,
           relation.suffix
@@ -39,7 +40,12 @@ angular.module('arethusa.relation').service('relation', [
             memo.push(el);
           }
         });
-      relation.label = clean.join('_');
+
+      var label = clean.join('_');
+      if (!doNotSet) {
+        relation.label = label;
+      }
+      return label;
     };
 
     this.prefixWithAncestors = function(relation) {
@@ -115,12 +121,6 @@ angular.module('arethusa.relation').service('relation', [
       this.multiChanger = self.relationTemplate();
     };
 
-    this.applyMultiChanger = function () {
-      angular.forEach(self.currentLabels(), function (obj, id) {
-        angular.extend(obj.relation, self.multiChanger);
-      });
-    };
-
     this.multiChangePossible = function () {
       // We check for the prefix, as only a suffix, which would
       // fill the label already would not be allowed.
@@ -130,9 +130,75 @@ angular.module('arethusa.relation').service('relation', [
         state.hasSelections();
     };
 
+    // Pretty ridiculous changes to gain compatibility with the
+    // new state eventing.
+    //
+    // There is some duplicate stuff happening, to blame is the
+    // whole nestedMenu directive logic, which has to be redone
+    // for other reasons as well.
+    //
+    // Prefix/Suffix and Ancestors have already been built by
+    // the directive when we enter relation.stateChange, only
+    // the label has to be built.
+    //
+    // Because we need to define proper values for prefix/suffix
+    // and ancestors when we want to undo/redo our changes, we
+    // have to recreate them during during stateChange, which
+    // leads to a redefinition on the initial event. It's not
+    // really a problem, it's just not very pretty...
+    //
+    this.applyMultiChanger = function () {
+      // We have to copy the multiChanger, so that its model
+      // stays intact, when we remove the label. We need to do
+      // this because we will rebuild it through changeState.
+      var cleanChanger = angular.copy(self.multiChanger);
+      delete cleanChanger.label;
+
+      state.doBatched(function() {
+        angular.forEach(self.currentLabels(), function (obj, id) {
+          var oldAncestors = obj.relation.ancestors;
+          angular.extend(obj.relation, cleanChanger);
+          self.changeState(obj.relation, oldAncestors);
+        });
+      });
+    };
+
+    function undoFn(obj, val, oldAncestors) {
+      oldAncestors = oldAncestors || angular.copy(obj.ancestors);
+      return function() {
+        splitLabel(obj, val);
+        obj.ancestors = oldAncestors;
+        state.change(obj.id, 'relation.label', val);
+      };
+    }
+
+    function preExecFn(obj, val) {
+      var newAncestors = angular.copy(obj.ancestors);
+      return function() {
+        obj.ancestors = newAncestors;
+        splitLabel(obj, val);
+      };
+    }
+
+    this.changeState = function(relObj, oldAncestors) {
+      var id = relObj.id;
+      var oldVal = relObj.label;
+      var newVal = self.buildLabel(relObj, !!id);
+
+      if (id) {
+        state.change(id, 'relation.label', newVal,
+                    undoFn(relObj, oldVal, oldAncestors),
+                    preExecFn(relObj, newVal));
+      }
+    };
+
     // Init
     function addToInternalState(container, id, token) {
       if (!token.relation) token.relation = self.relationTemplate();
+      // Passing the id is a hacky shortcut to allow access to
+      // state.change. If we ever have to change ids on the fly,
+      // this will call for trouble. Watch out.
+      token.relation.id = id;
       container[id] = {
         string: token.string,
         relation: self.expandRelation(token.relation || '')
