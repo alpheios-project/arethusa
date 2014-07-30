8f3a5da95bb9c2bfdc26f414ac28855b8094b622
LFDM <1986gh@gmail.com>
2014-07-30 16:53:10 +0200

    First steps in comments service

--- app/js/arethusa.comments/comments.js
+++ app/js/arethusa.comments/comments.js
@@ -5,15 +5,31 @@ angular.module('arethusa.comments').service('comments', [
   'configurator',
   function(state, configurator) {
     var self = this;
+    var retriever;
+
+    this.defaultConf = {
+      name: "comments",
+      template: "templates/arethusa.comments/comments.html"
+    };
 
     function configure() {
       configurator.getConfAndDelegate('comments', self);
+      retriever = configurator.getRetriever(self.conf.retriever);
     }
 
     configure();
 
+    function retrieveComments() {
+      self.comments = {};
+      // hardcoded for development
+      retriever.getData('1', function(comments) {
+        angular.extend(self.comments, comments);
+      });
+    }
+
     this.init = function() {
       configure();
+      retrieveComments();
     };
   }
 ]);
