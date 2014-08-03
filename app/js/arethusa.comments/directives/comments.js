62beffa23446f239d5134cdb6d4e424c0eb8817b
LFDM <1986gh@gmail.com>
2014-08-03 07:31:53 +0200

    Refacotr comments directive and templates

--- app/js/arethusa.comments/directives/comments.js
+++ app/js/arethusa.comments/directives/comments.js
@@ -14,25 +14,11 @@ angular.module('arethusa.comments').directive('comments', [
       compile: function(tElement, tAttrs, transclude) {
         return {
           pre: function(scope, iElement, iAttrs) {
-            function findNonSequentials() {
-              angular.forEach(scope.tokens, function(token, i) {
-                var next = scope.tokens[i + 1];
-                if (next) {
-                  if (idHandler.decrement(next.id) !== token.id) {
-                    scope.nonSequential[i] = true;
-                  }
-                }
-              });
-            }
-
             // Need to define the token in a pre-compile function,
             // otherwise the directive in the template cannot render!
             scope.tokens = arethusaUtil.map(scope.comments.ids, function(id) {
               return state.getToken(id);
             });
-
-            scope.nonSequential = {};
-            findNonSequentials();
           },
           post: function(scope, iElement, iAttrs) {
             scope.formatId = function(id) {
