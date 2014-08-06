bf408a448b398606cc936016f5f9b5cbaedfb362
srdee <srdee4@gmail.com>
2014-08-06 13:30:35 +0200

    Add rest of new commits from upstream

--- app/js/arethusa.js
+++ app/js/arethusa.js
@@ -4,7 +4,6 @@ angular.module('arethusa', [
   'angulartics.google.analytics',
   'mm.foundation',
   'ngRoute',
-  'pascalprecht.translate',
   'arethusa.core',
   'arethusa.contextMenu',
   'arethusa.confEditor',
@@ -13,13 +12,14 @@ angular.module('arethusa', [
   'arethusa.hist',
   'arethusa.review',
   'arethusa.search',
+  'arethusa.ne',
   'arethusa.exercise',
   'arethusa.relation',
   'arethusa.sg',
   'arethusa.text',
   'arethusa.hebrewMorph',
   'arethusa.artificialToken',
-  'arethusa.ne'
+  'arethusa.comments'
 ]);
 
 angular.module('arethusa').config([
@@ -40,21 +40,11 @@ angular.module('arethusa').config([
         suffix: '.json'
       })
 
-      // Right now we only support English. Still many strings we need to
-      // change to the new translate syntax. Once we've made progress with it,
-      // we activate it again.
-      // The reason we include it right now is that we want to use it for all
-      // future strings. We're too far behind on l10n/i18n matters - don't want
-      // to make the situation worse.
-      //
-      //.registerAvailableLanguageKeys(['en', 'de'], {
-        //'en_*' : 'en',
-        //'de_*' : 'de'
-      //})
-      //.determinePreferredLanguage()
-      //.fallbackLanguage('en');
-      .registerAvailableLanguageKeys(['en'])
-      .preferredLanguage('en');
+      .registerAvailableLanguageKeys(['en', 'de'], {
+        'en_*' : 'en',
+        'de_*' : 'de'
+      })
+      .determinePreferredLanguage()
+      .fallbackLanguage('en');
   },
-  
 ]);
