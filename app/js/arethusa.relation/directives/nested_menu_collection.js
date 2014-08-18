83cbf9a7f32847e535ef9d58ceb9526583024004
LFDM <1986gh@gmail.com>
2014-08-18 13:28:28 +0200

    Reposition nestedMenuCollection when it outgrows

--- app/js/arethusa.relation/directives/nested_menu_collection.js
+++ app/js/arethusa.relation/directives/nested_menu_collection.js
@@ -1,50 +1,66 @@
 "use strict";
 
-angular.module('arethusa.relation').directive('nestedMenuCollection', function() {
-  return {
-    restrict: 'A',
-    replace: 'true',
-    scope: {
-      current: '=',
-      all: '=',
-      property: '=',
-      ancestors: '=',
-      emptyVal: '@',
-      labelAs: "=",
-    },
-    link: function(scope, element, attrs) {
-      scope.emptyLabel = "";
-      scope.emptyObj = {};
+angular.module('arethusa.relation').directive('nestedMenuCollection', [
+  '$window',
+  function($window) {
+    return {
+      restrict: 'A',
+      replace: 'true',
+      scope: {
+        current: '=',
+        all: '=',
+        property: '=',
+        ancestors: '=',
+        emptyVal: '@',
+        labelAs: "=",
+      },
+      link: function(scope, element, attrs) {
+        var win = angular.element($window);
 
-      scope.labelView = function(labelObj) {
-        if (scope.labelAs) {
-          return labelObj[scope.labelAs];
-        } else {
-          return labelObj.short;
+        scope.emptyLabel = "";
+        scope.emptyObj = {};
+
+        scope.labelView = function(labelObj) {
+          if (scope.labelAs) {
+            return labelObj[scope.labelAs];
+          } else {
+            return labelObj.short;
+          }
+        };
+
+        if (element.hasClass('top-menu')) {
+          var items = Object.keys(scope.all).length + 1; // an empty val
+          var menuHeight = items * 18; // hard to access, we therefore hardcode...
+          var maxHeight = win.height();
+          var topPos = element.parent().offset().top;
+          var bottom = topPos + menuHeight;
+          if (bottom > maxHeight - 10) {
+            element.css({ top: 'auto', bottom: '100%'});
+          }
         }
-      };
-    },
-    template: '\
-      <ul>\
-        <li ng-if="emptyVal"\
-          nested-menu\
-          property="property"\
-          rel-obj="current"\
-          ancestors="ancestors"\
-          label="emptyLabel"\
-          label-obj="emptyObj">\
-        </li>\
-        <li\
-          ng-repeat="label in all | keys"\
-          nested-menu\
-          property="property"\
-          rel-obj="current"\
-          ancestors="ancestors"\
-          label="labelView(all[label])"\
-          label-as="labelAs"\
-          label-obj="all[label]">\
-        </li>\
-      </ul>\
-    '
-  };
-});
+      },
+      template: '\
+        <ul>\
+          <li ng-if="emptyVal"\
+            nested-menu\
+            property="property"\
+            rel-obj="current"\
+            ancestors="ancestors"\
+            label="emptyLabel"\
+            label-obj="emptyObj">\
+          </li>\
+          <li\
+            ng-repeat="label in all | keys"\
+            nested-menu\
+            property="property"\
+            rel-obj="current"\
+            ancestors="ancestors"\
+            label="labelView(all[label])"\
+            label-as="labelAs"\
+            label-obj="all[label]">\
+          </li>\
+        </ul>\
+      '
+    };
+  }
+]);
