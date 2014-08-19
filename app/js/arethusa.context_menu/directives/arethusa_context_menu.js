3b34790a839e0766782ebd938f0488f3d57db883
LFDM <1986gh@gmail.com>
2014-08-19 09:24:13 +0200

    Properly read token.status in arethusaContextMenu

--- app/js/arethusa.context_menu/directives/arethusa_context_menu.js
+++ app/js/arethusa.context_menu/directives/arethusa_context_menu.js
@@ -8,7 +8,6 @@ angular.module('arethusa.contextMenu').directive('arethusaContextMenu', [
         plugins: '='
       },
       link: function (scope, element, attrs) {
-        scope.status  = scope.token.status;
       },
       templateUrl: 'templates/arethusa.context_menu/arethusa_context_menu.html'
     };
