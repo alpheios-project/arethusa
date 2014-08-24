0c135347babaf30fa0e5b42b498c1b45b293183c
LFDM <1986gh@gmail.com>
2014-08-24 08:56:05 +0200

    Add global colorizer setting

--- app/js/arethusa.core/globalSettings.js
+++ app/js/arethusa.core/globalSettings.js
@@ -6,30 +6,42 @@ angular.module('arethusa.core').service('globalSettings', [
     var self = this;
 
     var confKeys = [
-      "alwaysDeselect"
+      "alwaysDeselect",
+      "colorizer"
     ];
+
+    self.defaultConf = {
+      alwaysDeselect: false,
+      colorizer: 'morph'
+    };
 
     function configure() {
       self.conf = configurator.configurationFor('main').globalSettings || {};
       configurator.delegateConf(self, confKeys, true); // true makes them sticky
 
       self.settings = {};
+      self.colorizers = {};
+
       defineSettings();
     }
 
-    function Conf(property, type) {
+    function Conf(property, type, options) {
       this.property = property;
       this.label = "globalSettings." + property;
       this.type = type || 'checkbox';
+
+      if (this.type === 'select') {
+        this.options = options;
+      }
     }
 
-    function defineSettings(args) {
+    function defineSettings() {
       defineSetting('alwaysDeselect');
-
+      defineSetting('colorizer', 'select', self.colorizers);
     }
 
-    function defineSetting(property, type) {
-      self.settings[property] = new Conf(property, type);
+    function defineSetting(property, type, options) {
+      self.settings[property] = new Conf(property, type, options);
     }
 
     this.toggle = function() {
