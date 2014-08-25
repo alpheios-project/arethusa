7b7b64f8770bba96569a6500d57b09bf77274ffa
LFDM <1986gh@gmail.com>
2014-08-25 12:50:29 +0200

    Add globalSettings to helpPanel

--- app/js/arethusa.core/directives/helpPanel.js
+++ app/js/arethusa.core/directives/helpPanel.js
@@ -3,13 +3,15 @@
 angular.module('arethusa.core').directive('helpPanel', [
   'help',
   'keyCapture',
-  function(help, keyCapture) {
+  'globalSettings',
+  function(help, keyCapture, globalSettings) {
     return {
       restrict: 'A',
       scope: {},
       link: function(scope, element, attrs) {
         scope.help = help;
         scope.kC = keyCapture;
+        scope.gS = globalSettings;
 
         scope.$watch('help.active', function(newVal, oldVal) {
           scope.active = newVal;
