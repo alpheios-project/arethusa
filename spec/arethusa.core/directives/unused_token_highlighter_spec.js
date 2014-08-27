df941f79f03b5b21121850968e032ad5cea802da
LFDM <1986gh@gmail.com>
2014-08-27 18:33:54 +0200

    More uTH specs

--- spec/arethusa.core/directives/unused_token_highlighter_spec.js
+++ spec/arethusa.core/directives/unused_token_highlighter_spec.js
@@ -43,5 +43,40 @@ describe("unusedTokenHighlighter", function() {
       state.addToken({ id: 28, head: { id: '02' } }, 28);
       expect(scope.total).toEqual(total + 1);
     });
+
+    it('when a token was removed', function() {
+      var total = state.totalTokens;
+      state.removeToken('01');
+      expect(scope.total).toEqual(total - 1);
+    });
+  });
+
+  describe('keeps track of unused tokens', function() {
+    it('when everything is used', function() {
+      // All tokens in Arethusa mocks have heads
+      expect(scope.unusedCount).toEqual(0);
+    });
+
+    it('when not everything is used', function() {
+      state.change('01', 'head.id', '');
+      expect(scope.unusedCount).toEqual(1);
+    });
+
+    it('when things change often', function() {
+      state.change('01', 'head.id', '');
+      expect(scope.unusedCount).toEqual(1);
+
+      state.change('02', 'head.id', '');
+      expect(scope.unusedCount).toEqual(2);
+
+      state.change('02', 'head.id', '01');
+      expect(scope.unusedCount).toEqual(1);
+    });
+
+    it('when things are changed that were used before', function() {
+      expect(scope.unusedCount).toEqual(0);
+      state.change('02', 'head.id', '01');
+      expect(scope.unusedCount).toEqual(0);
+    });
   });
 });
