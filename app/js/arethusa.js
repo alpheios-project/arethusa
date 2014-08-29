b13947fe1ef0fdaf20272a3f1372036beeacc4a4
LFDM <1986gh@gmail.com>
2014-08-29 10:28:50 +0200

    Add oc.lazyload

--- app/js/arethusa.js
+++ app/js/arethusa.js
@@ -4,6 +4,7 @@ angular.module('arethusa', [
   'angulartics.google.analytics',
   'mm.foundation',
   'ngRoute',
+  'oc.lazyLoad',
   'arethusa.core',
   'arethusa.contextMenu',
   'arethusa.confEditor',
