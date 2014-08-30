4baf875a37caeb2627b1ee8ff0758385627394d1
LFDM <1986gh@gmail.com>
2014-08-30 16:10:31 +0200

    Wrap $ocLazyLoad in dependencyLoader

---
+++ app/js/arethusa.core/dependency_loader.js
@@ -0,0 +1,25 @@
+"use strict";
+
+angular.module('arethusa.core').service('dependencyLoader', [
+  '$ocLazyLoad',
+  '$q',
+  function($ocLazyLoad, $q) {
+    this.load = function(args) {
+      return $ocLazyLoad.load(args);
+    };
+
+    this.loadSync = function(args) {
+      var start = $q.defer();
+      var promises = [start.promise];
+      angular.forEach(args, function(el, i) {
+        var defer = $q.defer();
+        promises.push(defer.promise);
+        promises[i].then(function() {
+          $ocLazyLoad.load(el)['finally'](defer.resolve());
+        });
+      });
+      start.resolve();
+      return aU.last(promises);
+    };
+  }
+]);
