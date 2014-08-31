414c14c540e3b4839682fef007e39d3c2a6ebc05
LFDM <1986gh@gmail.com>
2014-09-01 00:17:09 +0200

    Move i18n to dist folder

--- app/js/arethusa.js
+++ app/js/arethusa.js
@@ -24,7 +24,7 @@ angular.module('arethusa').config([
 
     $translateProvider
       .useStaticFilesLoader({
-        prefix: 'static/i18n/',
+        prefix: '../dist/i18n/',
         suffix: '.json'
       })
 
