dc6c5f8794ed4d59f0fefea166bef9acd23c7120
LFDM <1986gh@gmail.com>
2014-09-01 00:36:06 +0200

    Start to use locator again

--- app/js/arethusa.core/locator.js
+++ app/js/arethusa.core/locator.js
@@ -3,15 +3,15 @@
 angular.module('arethusa.core').service('locator', [
   '$location',
   function ($location) {
-    // TODO
-    // - Retrieve the uris from the route on startup
-    // - Populate the locators obj from the config file
-    var locators = { treebankRetriever: 'doc' };
-    this.getUri = function (name) {
-      return $location.search()[locators[name]];
+    var noUrlParams;
+    var manualParams = {};
+
+    this.get = function(name) {
+      return noUrlParams ? manualParams[name] : $location.search()[name];
     };
-    this.setUri = function (name, uri) {
-      $location.search(name, uri);
+
+    this.watchUrl = function(bool) {
+      noUrlParams = bool;
     };
   }
 ]);
