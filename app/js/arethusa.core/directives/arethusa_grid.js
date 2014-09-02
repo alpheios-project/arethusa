be39a414139f89119697aff4c3c64ef62ac6de4e
LFDM <1986gh@gmail.com>
2014-09-02 11:52:12 +0200

    Add gridSettings when arethusaGrid is active

--- app/js/arethusa.core/directives/arethusa_grid.js
+++ app/js/arethusa.core/directives/arethusa_grid.js
@@ -3,13 +3,31 @@
 angular.module('arethusa.core').directive('arethusaGrid', [
   'arethusaGrid',
   'plugins',
-  function(arethusaGrid, plugins) {
+  'globalSettings',
+  '$timeout',
+  function(arethusaGrid, plugins, globalSettings, $timeout) {
     return {
       restrict: 'A',
       scope: {},
       link: function(scope, element, attrs) {
         angular.element(document.body).css('overflow', 'auto');
+
         scope.grid = arethusaGrid;
+
+        function addSettings() {
+          globalSettings.defineSetting('grid', 'custom', 'grid-setting');
+        }
+
+        function removeSettings() {
+          globalSettings.removeSetting('grid');
+        }
+
+        // We need to timeout this, so that we can give globalSettings
+        // time to define its own default settings - only afterwards
+        // we add our own grid settings
+        $timeout(addSettings);
+
+        scope.$on('$destroy', removeSettings);
       },
       templateUrl: 'templates/arethusa.core/arethusa_grid.html'
     };
