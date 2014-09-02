779e85a945e2314fb74a569c598b7b8bbe3a4832
LFDM <1986gh@gmail.com>
2014-09-02 09:55:15 +0200

    More animations - sexy beyond belief

--- app/js/arethusa.core/routes/main.js
+++ app/js/arethusa.core/routes/main.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('arethusa.core').constant('MAIN_ROUTE', {
   controller: 'ArethusaCtrl',
-  template: '<div ng-include="template"></div>',
+  template: '<div ng-include="template" class="fade"></div>',
   resolve: {
     loadConfiguration: ['$http', 'confUrl', 'configurator', function ($http, confUrl, configurator) {
       var url = confUrl(true);
