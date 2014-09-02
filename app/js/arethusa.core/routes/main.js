4faf211e7357c1c4ed62ed6f4fbd8e5a162aba87
LFDM <1986gh@gmail.com>
2014-09-02 10:37:35 +0200

    Move navbar to route template

--- app/js/arethusa.core/routes/main.js
+++ app/js/arethusa.core/routes/main.js
@@ -1,7 +1,15 @@
 'use strict';
 angular.module('arethusa.core').constant('MAIN_ROUTE', {
   controller: 'ArethusaCtrl',
-  template: '<div ng-include="gS.layout" class="fade"></div>',
+  template: '\
+    <div>\
+      <arethusa-navbar></arethusa-navbar>\
+      <div\
+        ng-include="gS.layout"\
+         class="fade slow">\
+      </div>\
+    </div>\
+  ',
   resolve: {
     loadConfiguration: ['$http', 'confUrl', 'configurator', function ($http, confUrl, configurator) {
       var url = confUrl(true);
