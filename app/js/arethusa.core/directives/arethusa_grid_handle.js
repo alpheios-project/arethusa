e23a40064468c27be76dbb27efd4e4f2ac91ece5
LFDM <1986gh@gmail.com>
2014-09-05 12:34:59 +0200

    Fix mouse events in arethusaGridHandle

--- app/js/arethusa.core/directives/arethusa_grid_handle.js
+++ app/js/arethusa.core/directives/arethusa_grid_handle.js
@@ -6,7 +6,19 @@ angular.module('arethusa.core').directive('arethusaGridHandle', [
       restrict: 'E',
       scope: true,
       link: function(scope, element, attrs, ctrl, transclude) {
+        function mouseEnter() {
+          scope.$apply(function() { scope.visible = true; });
+        }
 
+        function mouseLeave(event) {
+          scope.$apply(function() { scope.visible = false; });
+        }
+
+        var trigger = element.find('.drag-handle-trigger');
+        var handle  = element.find('.drag-handle');
+
+        trigger.bind('mouseenter', mouseEnter);
+        handle.bind('mouseleave', mouseLeave);
       },
       templateUrl: 'templates/arethusa.core/arethusa_grid_handle.html'
     };
