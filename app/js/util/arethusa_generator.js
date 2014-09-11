e3b80778bf188ac19ab4d698621864d52819085f
LFDM <1986gh@gmail.com>
2014-09-12 00:29:07 +0200

    Refactor panel triggers for speed

--- app/js/util/arethusa_generator.js
+++ app/js/util/arethusa_generator.js
@@ -7,17 +7,22 @@ function ArethusaGenerator() {
   this.panelTrigger = function panelTrigger(service, trsl, trslKey, template) {
     return {
       restrict: 'A',
-      link: function(scope, element, attrs) {
-        function toggle() {
-          scope.$apply(service.toggle());
+      compile: function(element) {
+        var parent = element.parent();
+
+        function updateTitle(translation) {
+          parent.attr('title', translation);
         }
 
-        var parent = element.parent();
-        trsl(trslKey, function(translation) {
-          parent.attr('title', translation);
-        });
+        return function link(scope, element, attrs) {
+          function toggle() {
+            scope.$apply(service.toggle());
+          }
 
-        element.bind('click', toggle);
+          trsl(trslKey, updateTitle);
+
+          element.bind('click', toggle);
+        };
       },
       template: template
     };
