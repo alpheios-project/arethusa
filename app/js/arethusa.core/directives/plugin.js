81c9e057d8466d97e86e105fc5d85e56288a5b2d
LFDM <1986gh@gmail.com>
2014-09-11 13:30:21 +0200

    Add pluginSettings directive

--- app/js/arethusa.core/directives/plugin.js
+++ app/js/arethusa.core/directives/plugin.js
@@ -13,6 +13,7 @@ angular.module('arethusa.core').directive('plugin', [
 
         scope.name = nameMap[attrs.name] || attrs.name;
         scope.plugin = plugins.get(scope.name);
+        scope.withSettings = attrs.withSettings;
 
         scope.$on('pluginAdded', function(event, name, plugin) {
           if (name === scope.name) {
