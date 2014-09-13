6c235fef30a332c17497de9515b15225b991842f
LFDM <1986gh@gmail.com>
2014-09-14 01:09:51 +0200

    Change chunkSizes on the fly

--- app/js/arethusa.core/directives/chunk_mode_switcher.js
+++ app/js/arethusa.core/directives/chunk_mode_switcher.js
@@ -2,14 +2,27 @@
 
 angular.module('arethusa.core').directive('chunkModeSwitcher', [
   'navigator',
-  'globalSettings',
-  function(navigator, globalSettings) {
+  'notifier',
+  function(navigator, notifier) {
     return {
       restrict: 'A',
       scope: {},
       link: function(scope, element, attrs) {
         scope.navi = navigator;
-        scope.label = globalSettings.settings.chunkMode.label;
+        scope.size = navigator.chunkSize;
+
+        scope.tryToSetChunkSize = function() {
+          var size = scope.size;
+          if (navigator.chunkSize === size) {
+            return;
+          }
+          if (size < 1 || size > 5) {
+            notifier.error('Only chunk sizes between 1 and 5 are currently supported');
+            scope.size = navigator.chunkSize;
+          } else {
+            navigator.changeChunkSize(scope.size);
+          }
+        };
       },
       templateUrl: 'templates/arethusa.core/chunk_mode_switcher.html'
     };
