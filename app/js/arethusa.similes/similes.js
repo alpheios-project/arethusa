e688eda96acf881be30d0a3fa4212abce2a8c594
LFDM <1986gh@gmail.com>
2014-09-14 13:38:20 +0200

    Quick and dirty setup for a similes demo

--- app/js/arethusa.similes/similes.js
+++ app/js/arethusa.similes/similes.js
@@ -7,16 +7,38 @@ angular.module('arethusa.similes').service('similes', [
     var self = this;
     this.name = 'similes';
 
+
+    this.labelAs = "long";
+    this.defineAncestors = true;
+
     this.defaultConf = {
       template: 'templates/arethusa.similes/similes.html'
     };
 
+    var props = [
+      'relations' // change this to whatever needed later
+    ];
+
     function configure() {
-      configurator.getConfAndDelegate(self);
+      configurator.getConfAndDelegate(self, props);
+      self.similes = self.relations.labels;
+    }
+
+    this.currentSelection = function() {
+      return arethusaUtil.inject({}, state.selectedTokens, function(memo, id, token) {
+        memo[id] = self.interal[id];
+      });
+    };
+
+    function createInternalState() {
+      self.interal = arethusaUtil.inject({}, state.tokens, function(memo, id, token) {
+        memo[id] = {};
+      });
     }
 
     this.init = function() {
       configure();
+      createInternalState();
     };
   }
 ]);
