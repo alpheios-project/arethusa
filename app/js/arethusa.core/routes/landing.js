d469ff892f139d04c4ba44b2176424b98c7c9e6e
LFDM <1986gh@gmail.com>
2014-09-19 08:16:18 +0200

    Add a landing route

---
+++ app/js/arethusa.core/routes/landing.js
@@ -0,0 +1,17 @@
+'use strict';
+angular.module('arethusa.core').constant('LANDING', {
+  controller: 'LandingCtrl',
+  template: '\
+    <div style="height: 100%; overflow: auto">\
+      <div\
+        ng-include="template"\
+         class="fade slow">\
+      </div>\
+    </div>\
+  ',
+  resolve: {
+    scrollBody: function() {
+      angular.element(document.body).css('overflow', 'auto');
+    }
+  }
+});
