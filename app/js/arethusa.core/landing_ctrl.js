8963a03a59056d46eadbdc40676247d0bb9f4978
LFDM <1986gh@gmail.com>
2014-09-19 10:14:27 +0200

    Make examples dynamic

--- app/js/arethusa.core/landing_ctrl.js
+++ app/js/arethusa.core/landing_ctrl.js
@@ -2,7 +2,27 @@
 
 angular.module('arethusa.core').controller('LandingCtrl', [
   '$scope',
-  function ($scope) {
+  '$window',
+  function ($scope, $window) {
     $scope.template = 'templates/landing_page.html';
+
+
+    function Example(name, caption, url) {
+      this.name = name;
+      this.caption = caption;
+      this.url = url;
+    }
+
+    $scope.useCases = {
+      Treebanking: [
+        new Example('...', '', 'example_tb_create?doc=athenaeus1'),
+        new Example('Review', '', 'example_tb_review?doc=11&gold=1'),
+        new Example('...', '', 'example_tb_create?doc=clean'),
+      ]
+    };
+
+    $scope.goTo = function(url) {
+      $window.open('/app/#/' + url);
+    };
   }
 ]);
