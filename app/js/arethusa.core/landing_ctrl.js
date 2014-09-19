4185667c26b49dadc8ad2521c4b3365019e9f5fd
LFDM <1986gh@gmail.com>
2014-09-19 10:50:19 +0200

    More work on landing page

--- app/js/arethusa.core/landing_ctrl.js
+++ app/js/arethusa.core/landing_ctrl.js
@@ -7,22 +7,29 @@ angular.module('arethusa.core').controller('LandingCtrl', [
     $scope.template = 'templates/landing_page.html';
 
 
-    function Example(name, caption, url) {
+    function Example(name, caption, img, url) {
       this.name = name;
       this.caption = caption;
-      this.url = url;
+      this.img = '../dist/examples/images/' + img;
+      this.url = '/app/#/' + url;
     }
 
-    $scope.useCases = {
-      Treebanking: [
-        new Example('...', '', 'example_tb_create?doc=athenaeus1'),
-        new Example('Review', '', 'example_tb_review?doc=11&gold=1'),
-        new Example('...', '', 'example_tb_create?doc=clean'),
-      ]
-    };
+    function UseCase(name, examples) {
+      this.name = name;
+      this.examples = examples;
+    }
+
+    $scope.useCases = [
+      new UseCase('Treebanking', [
+        new Example('Dependency Trees', '', 'create1.png', 'example_tb_create?doc=athenaeus12'),
+        new Example('Review', '', 'tb_review.png', 'example_tb_review?doc=11&gold=1'),
+        new Example('Try it yourself!', '', 'create2.png', 'example_tb_create?doc=clean')
+      ]),
+      //new UseCase('Reading Environmnent', [])
+    ];
 
     $scope.goTo = function(url) {
-      $window.open('/app/#/' + url);
+      $window.open(url);
     };
   }
 ]);
