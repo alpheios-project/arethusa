2a156420467bd57ded1fef59fab5a395262317c0
Christof Sirk <christof.sirk@gmail.com>
2014-09-28 19:56:18 +0200

    Add example documentation (not yet working)

--- app/js/arethusa.core/directives/key_capture.js
+++ app/js/arethusa.core/directives/key_capture.js
@@ -1,4 +1,30 @@
 'use strict';
+
+/**
+ * @ngdoc directive
+ * @name keyCapture
+ * @restrict A
+ *
+ * @description
+ * Captures keydown and keyup events on the element and
+ * delegates the event handling to the keyCapture service.
+ *
+ * @example
+   <example module="arethusa.core">
+     <file name="service.js">
+        angular.module('arethusa.core').service('keyCapture', function() {
+          this.keydown = function(event) {
+            console.log(event);
+          };
+        });
+     </file>
+     <file name="index.html">
+      <div keyCapture>
+        Some awesome stuff
+      </div>
+     </file>
+   </example>
+ */
 angular.module('arethusa.core').directive('keyCapture', [
   'keyCapture',
   function (keyCapture) {
