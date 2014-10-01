eab025ca3c52f78a3ef1e4999b04c2b6198c1b4d
LFDM <1986gh@gmail.com>
2014-10-01 14:32:22 +0200

    Add comment about future locales

--- app/js/arethusa.core/locales.js
+++ app/js/arethusa.core/locales.js
@@ -1,5 +1,19 @@
 "use strict";
 
+// This constant is used to grant access to different localizations
+// of the Arethusa interface.
+//
+// We plan to at least support the following in the future:
+//
+//    de
+//    en
+//    es
+//    fa
+//    fr
+//    hr
+//    it
+//    pt
+//
 angular.module('arethusa.core').constant('LOCALES', [
   'en',
   'de',
