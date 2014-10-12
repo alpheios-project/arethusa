234fd2888abc71e6d52f88faab6138aa0d6e77ac
Christof Sirk <christof.sirk@gmail.com>
2014-10-12 17:02:14 +0200

    Extract tokenSelector directive

---
+++ app/js/arethusa.core/directives/token_selector.js
@@ -0,0 +1,37 @@
+"use strict";
+
+angular.module('arethusa.core').directive('tokenSelector', [
+  'state',
+  function(state) {
+    return {
+      restrict: 'A',
+      scope: {
+        tokens: "=tokenSelector"
+      },
+      link: function(scope, element, attrs) {
+        scope.changeSelection = function() {
+          if (scope.hasNoSelectedTokens()) {
+            state.multiSelect(Object.keys(scope.tokens));
+          } else {
+            state.deselectAll();
+          }
+        };
+
+        scope.hasNoSelectedTokens = function() {
+          return !state.hasClickSelections();
+        };
+
+        scope.hasSomeSelectedTokens = function() {
+          var selected = state.hasClickSelections();
+          return selected > 0 && selected != state.totalTokens;
+        };
+
+        scope.hasAllSelectedTokens = function() {
+          return state.hasClickSelections() == state.totalTokens;
+        };
+      },
+      templateUrl: 'templates/arethusa.core/token_selector.html'
+    };
+  }
+]);
+
--- app/js/arethusa.dep_tree/dep_tree.js
+++ app/js/arethusa.dep_tree/dep_tree.js
@@ -47,14 +47,6 @@ angular.module('arethusa.depTree').service('depTree', [
       self.diffMode = !self.diffMode;
     };
 
-    this.deselectAll = function() {
-      state.deselectAll();
-    };
-
-    this.hasNoSelectedTokens = function() {
-      return !state.hasClickSelections();
-    };
-
     // We have three things we can colorize as wrong in the tree
     //   Label
     //   Head
