0aa792c5f1f87f522c0fbb6975a9a3fd0e5b40ac
LFDM <1986gh@gmail.com>
2014-10-23 09:20:31 +0200

    Update and refactor outputter

--- app/js/arethusa.core/directives/outputter.js
+++ app/js/arethusa.core/directives/outputter.js
@@ -34,20 +34,28 @@ angular.module('arethusa.core').directive('outputterItem', [
       link: function(scope, element, attrs) {
         element.addClass('item');
 
+        // This scope will be destroyed once the modal is destroyed.
+        // When a user wants to look at a document once, we can therefore
+        // cache it - it can never go out of sync, because making changes
+        // to it would require leaving the modal.
+        var data;
+        scope.data = function() {
+          if (!data) data = scope.obj.output();
+          return data;
+        };
+
         scope.togglePreview = function() { scope.preview = !scope.preview; };
 
         var downloader;
         scope.download = function() {
           if (!downloader) downloader = document.createElement('a');
-          var str = encodeURIComponent(scope.obj.output());
+          var str = encodeURIComponent(scope.data());
           var type = 'data:' + scope.obj.mimeType + ' ;charset=utf-8,';
           var fileName = scope.obj.identifier + '.' + scope.obj.fileType;
           downloader.setAttribute('href', type + str);
           downloader.setAttribute('download', fileName);
           downloader.click();
         };
-
-
       },
       templateUrl: 'templates/arethusa.core/outputter_item.html',
     };
