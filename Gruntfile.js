670d09406d4009c01fc8af7594a06dc5c2bdec30
LFDM <1986gh@gmail.com>
2014-10-23 10:07:41 +0200

    Add highlight js files for karma spec suite

--- Gruntfile.js
+++ Gruntfile.js
@@ -359,6 +359,8 @@ module.exports = function(grunt) {
             './vendor/mm-foundation/mm-foundation-tpls-0.1.0.min.js',
             './vendor/dagre-d3/dagre-d3.min.js',
             './vendor/angularJS-toaster/toaster.min.js',
+            "./vendor/highlight/highlight.pack.js",
+            "./vendor/angular-highlightjs/angular-highlightjs.min.js",
             // Some source files we'll need to include manually, otherwise
             // the load order is wrong
             'app/js/*.js',
