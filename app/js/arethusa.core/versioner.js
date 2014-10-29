55c353fc67e643845db5e13270066caf98b0e1c7
LFDM <1986gh@gmail.com>
2014-10-29 17:28:22 +0100

    Refactor and update versioner

--- app/js/arethusa.core/versioner.js
+++ app/js/arethusa.core/versioner.js
@@ -3,7 +3,7 @@
 angular.module('arethusa.core').service('versioner', [
   'VERSION',
   function(VERSION) {
-    this.revision = VERSION.revision;
-    this.date = VERSION.date;
+    angular.extend(this, VERSION);
+    this.commitUrl = this.repository + '/tree/' + VERSION.revision;
   }
 ]);
