7b3fe542f17f315de6f33ff3c298f6e17c8722bb
LFDM <1986gh@gmail.com>
2014-11-01 23:18:39 +0100

    Add branch name to about section in helpPanel

--- app/js/arethusa.core/versioner.js
+++ app/js/arethusa.core/versioner.js
@@ -3,7 +3,14 @@
 angular.module('arethusa.core').service('versioner', [
   'VERSION',
   function(VERSION) {
+    var self = this;
+
+    function gitTree(path) {
+      return self.repository + '/tree/' + path;
+    }
+
     angular.extend(this, VERSION);
-    this.commitUrl = this.repository + '/tree/' + VERSION.revision;
+    this.commitUrl = gitTree(VERSION.revision);
+    this.branchUrl = gitTree(VERSION.branch);
   }
 ]);
