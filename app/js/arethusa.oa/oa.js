109d28023147f17a85384befed9b575e855d9fa0
LFDM <1986gh@gmail.com>
2014-11-05 13:50:18 +0100

    Add persister to oa service

--- app/js/arethusa.oa/oa.js
+++ app/js/arethusa.oa/oa.js
@@ -7,7 +7,7 @@ angular.module('arethusa.oa').service('oa', [
     var self = this;
     this.name = 'oa';
 
-    var retriever;
+    var retriever, persister;
 
     this.defaultConf = {
       template: 'templates/arethusa.oa/oa.html'
@@ -16,15 +16,14 @@ angular.module('arethusa.oa').service('oa', [
     function configure() {
       configurator.getConfAndDelegate(self);
       retriever = configurator.getRetriever(self.conf.retriever);
+      persister = configurator.getPersister(self.conf.persister);
     }
 
     this.init = function() {
       configure();
 
-      // Just to see if the current setup works
-      retriever.get(function(data) {
-        console.log(data);
-      });
+      console.log(retriever, persister);
+      retriever.get(function(data) { console.log(data); });
     };
   }
 ]);
