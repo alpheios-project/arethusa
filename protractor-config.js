24dee8910d28f50380a5191ebe1cfd4ded157f62
LFDM <1986gh@gmail.com>
2014-11-17 21:53:02 +0100

    Let protractor start Selenium

--- protractor-config.js
+++ protractor-config.js
@@ -2,11 +2,13 @@
 
 var devServerPort = 8081;
 var specE2eFiles = 'spec-e2e/**/*.js';
+
 /* global exports */
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: [specE2eFiles],
-  multiCapabilities: [{'browserName': 'firefox'}, {'browserName': 'chrome'}],
-  //capabilities: {'browserName': 'firefox'},
+  multiCapabilities: [
+    {'browserName': 'firefox'},
+    {'browserName': 'chrome'}
+  ],
   baseUrl: 'http://localhost:' + devServerPort
 };
