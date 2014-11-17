e6315e4607640f9cc4d12f605164c23c81787d3f
LFDM <1986gh@gmail.com>
2014-11-17 16:15:13 +0100

    Trigger a need for a save when gloss is updated

--- app/js/arethusa.morph/morph.js
+++ app/js/arethusa.morph/morph.js
@@ -6,8 +6,9 @@ angular.module('arethusa.morph').service('morph', [
   'globalSettings',
   'keyCapture',
   'morphLocalStorage',
+  'saver',
   function (state, configurator, plugins, globalSettings,
-            keyCapture, morphLocalStorage) {
+            keyCapture, morphLocalStorage, saver) {
     var self = this;
     this.name = 'morph';
 
@@ -535,6 +536,7 @@ angular.module('arethusa.morph').service('morph', [
 
     this.updateGloss = function(id, form) {
       if (self.gloss) {
+        saver.needsSave = true;
         var gloss = self.analyses[id].gloss;
         if (gloss) {
           form = form || selectedForm(id);
