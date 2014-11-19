6df07fba50dacdbc3a05a617b06ffa87bca32334
LFDM <1986gh@gmail.com>
2014-11-19 12:34:17 +0100

    Start with ConstituentTreebankRetriever

---
+++ app/js/arethusa/constituent_treebank_retriever.js
@@ -0,0 +1,90 @@
+"use strict";
+
+angular.module('arethusa').factory('ConstituentTreebankRetriever', [
+  'configurator',
+  'documentStore',
+  function(configurator, documentStore) {
+    // Parse functions
+
+    function parseDocument(json, docId) {
+      return parseSentences(json.book.sentence, docId);
+    }
+
+    function parseSentences(sentences, docId) {
+      return aU.toAry(sentences).map(function(sentence) {
+        return parseSentence(sentence, docId);
+      });
+    }
+
+    function parseSentence(sentence, docId) {
+      var sentenceId = sentence._ID;
+      var constituents = new Container();
+      var tokens = new Container();
+
+      // Hack to resolve the ambiguity between sentence and subject
+      var wgNode = sentence.wg;
+      wgNode._role = 'sent';
+
+      parseWordGroup(wgNode, docId, sentenceId, constituents, tokens);
+
+      return aC.sentence(sentenceId, tokens.container, constituents.container);
+    }
+
+    function parseWordGroup(wg, docId, sentenceId, constituents, tokens, parentId) {
+      var id = wg._nodeId;
+      var constituent = aC.constituent(
+        wg._class,
+        wg._role,
+        id,
+        parentId
+      );
+
+      constituents.add(constituent);
+
+      angular.forEach(aU.toAry(wg.wg), function(childWg, i) {
+        parseWordGroup(childWg, docId, sentenceId, constituents, tokens, id);
+      });
+
+      angular.forEach(aU.toAry(wg.w), function(w, i) {
+        parseWord(w, docId, sentenceId, tokens, id);
+      });
+
+    }
+
+    function parseWord(w, docId, sentenceId, tokens, parentId) {
+      var token = aC.token(w.__text, sentenceId);
+      token.id = w._morphId;
+      tokens.add(token);
+    }
+
+    // Helpers
+
+    function Container() {
+      var self = this;
+      this.container = {};
+      this.add = function(el) {
+        self.container[el.id] = el;
+      };
+    }
+
+    return function(conf) {
+      var self = this;
+      var resource = configurator.provideResource(conf.resource);
+      var docId = conf.docIdentifier;
+
+      this.parse = function(xml, callback) {
+        var json = arethusaUtil.xml2json(xml);
+        documentStore.addDocument(docId, new aC.doc(xml, json));
+
+        callback(parseDocument(json, docId));
+      };
+
+      this.get = function(callback) {
+        resource.get().then(function(res) {
+          var data = res.data;
+          self.parse(res.data, callback);
+        });
+      };
+    };
+  }
+]);
