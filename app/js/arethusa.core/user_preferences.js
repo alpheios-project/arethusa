286f08f99145c66567558b87bf842a5e334e00b6
LFDM <1986gh@gmail.com>
2014-11-26 20:54:26 +0100

    Start to implement userPreferences service

---
+++ app/js/arethusa.core/user_preferences.js
@@ -0,0 +1,32 @@
+"use strict";
+
+angular.module('arethusa.core').service('userPreferences', [
+  'globalSettings',
+  'arethusaLocalStorage',
+  function(globalSettings, arethusaLocalStorage) {
+    var self = this;
+
+
+    var localStorageKey = 'preferences';
+
+    function key(k) {
+      return localStorageKey + '.' + k;
+    }
+
+    function set(plugin, setting, value) {
+      arethusaLocalStorage.set(key(plugin + '.' + setting, value));
+    }
+
+    function get(plugin, setting) {
+      return arethusaLocalStorage.get(key(plugin + '.' + setting));
+    }
+
+    this.set = function(plugin, setting, value) {
+      if (globalSettings.persistSettings) {
+        set(plugin, setting, value);
+      }
+    };
+
+    this.get = get;
+  }
+]);
