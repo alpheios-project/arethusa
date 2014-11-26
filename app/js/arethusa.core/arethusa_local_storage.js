432027a6728ce1ffa43360722eff70c604a7d25d
LFDM <1986gh@gmail.com>
2014-11-26 23:40:03 +0100

    Coerce booleans from localStorage

--- app/js/arethusa.core/arethusa_local_storage.js
+++ app/js/arethusa.core/arethusa_local_storage.js
@@ -3,7 +3,19 @@
 angular.module('arethusa.core').service('arethusaLocalStorage', [
   'localStorageService',
   function(localStorageService) {
-    this.get = localStorageService.get;
+    this.get = function(key) {
+      return coerce(localStorageService.get(key));
+    };
+
     this.set = localStorageService.set;
+
+    var JSONBooleans = ['true', 'false', 'null'];
+    function coerce(value) {
+      if (JSONBooleans.indexOf(value) === -1) {
+        return value;
+      } else {
+        return JSON.parse(value);
+      }
+    }
   }
 ]);
