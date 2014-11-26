b61094acb8fd157c47a59892ba64dceb6df3d4fd
LFDM <1986gh@gmail.com>
2014-11-26 22:16:57 +0100

    Minor refactor in arethusaLocalStorage

--- app/js/arethusa.core/arethusa_local_storage.js
+++ app/js/arethusa.core/arethusa_local_storage.js
@@ -3,8 +3,6 @@
 angular.module('arethusa.core').service('arethusaLocalStorage', [
   'localStorageService',
   function(localStorageService) {
-    var self = this;
-
     this.get = localStorageService.get;
     this.set = localStorageService.set;
   }
