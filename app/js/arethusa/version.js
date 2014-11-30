07f234af9ba00ee3080d6359c0466003638bc4c1
Christof Sirk <christof.sirk@gmail.com>
2014-11-30 15:56:49 +0100

    Remove version.js

--- app/js/arethusa/version.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('arethusa').constant('VERSION', {
-  revision: '1773ae4a9537a57d544dad5ea055c50724852422',
-  branch: 'deselect-button',
-  date: '2014-11-17T18:55:07.193Z',
-  repository: 'http://github.com/latin-language-toolkit/arethusa'
-});
