23dedfbe9898f58cca3f6718955734cd7f5f03e4
LFDM <1986gh@gmail.com>
2014-12-02 14:27:32 +0100

    Add logger.remoteLog()

--- app/js/arethusa.util/logger.js
+++ app/js/arethusa.util/logger.js
@@ -2,9 +2,19 @@
 /* global console */
 
 angular.module('arethusa.util').service('logger', [
-  function() {
+  'configurator',
+  function(configurator) {
+    var conf = configurator.configurationFor('logger');
+    var resource = configurator.provideResource(conf.resource);
+
     this.log = function(msg) {
       console.log(msg);
+    };
+
+    this.remoteLog = function(msg) {
+      if (resource) {
+        resource.post(msg, 'text/plain');
+      }
     };
   }
 ]);
