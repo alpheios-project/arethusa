cf4b279ab2ae38c4e1792658c354f9a8a1fbb8b5
LFDM <1986gh@gmail.com>
2014-12-11 18:38:17 +0100

    Add logger docs

--- app/js/arethusa.util/logger.js
+++ app/js/arethusa.util/logger.js
@@ -1,8 +1,27 @@
 'use strict';
 /* global console */
 
+/**
+ * @ngdoc service
+ * @name arethusa.util.logger
+ *
+ * @description
+ * Simple logging wrapper.
+ *
+ */
 angular.module('arethusa.util').service('logger', [
   function() {
+    /**
+     * @ngdoc function
+     * @name arethusa.util.logger#log
+     * @methodOf arethusa.util.logger
+     *
+     * @description
+     * Wrapper around `console.log`
+     *
+     * @param {String} msg Message to log
+     *
+     */
     this.log = function(msg) {
       console.log(msg);
     };
