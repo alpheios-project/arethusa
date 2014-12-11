a3cf5cb292af3b38cdf9de358418427a65ab0220
LFDM <1986gh@gmail.com>
2014-12-11 22:35:53 +0100

    Add documentation for plugin directive

--- app/js/arethusa.core/directives/plugin.js
+++ app/js/arethusa.core/directives/plugin.js
@@ -1,4 +1,26 @@
 'use strict';
+
+/**
+ * @ngdoc directive
+ * @name arethusa.core.directives:plugin
+ * @restrict AE
+ * @scope
+ *
+ * @description
+ * Renders a plugin, identified by the plugin's name.
+ *
+ * The template found in the plugins `template` property is used to do this.
+ *
+ * The plugin itself is bound to the newly created child scope. Templates can
+ * access it through the `plugin` scope variable.
+ *
+ * @param {string} plugin The name of the plugin used in this scope.
+ * @param {boolean} [withSettings=false] Determines if the template should
+ *   include plugin settings through the use of the {@link arethusa.core.directives:pluginSettings pluginSettings}
+ *   directive.
+ *
+ * @requires plugins
+ */
 angular.module('arethusa.core').directive('plugin', [
   'plugins',
   function (plugins) {
