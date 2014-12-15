e470ede85bbabdb228198a42d73091e2b5789376
LFDM <1986gh@gmail.com>
2014-12-15 11:57:23 -0500

    React to mode in formSelector

--- app/js/arethusa.morph/directives/form_selector.js
+++ app/js/arethusa.morph/directives/form_selector.js
@@ -1,36 +1,52 @@
 'use strict';
-angular.module('arethusa.morph').directive('formSelector', function () {
-  return {
-    restrict: 'A',
-    link: function(scope, element, attrs) {
-      var id = scope.id;
+angular.module('arethusa.morph').directive('formSelector', [
+  'morph',
+  function (morph) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        var id = scope.id;
 
-      function action(event) {
-        event.stopPropagation();
-        scope.$apply(function() {
-          if (scope.form.selected) {
-            scope.plugin.unsetState(id);
-          } else {
-            scope.plugin.setState(id, scope.form);
-          }
+        function action(event) {
+          event.stopPropagation();
+          scope.$apply(function() {
+            if (scope.form.selected) {
+              scope.plugin.unsetState(id);
+            } else {
+              scope.plugin.setState(id, scope.form);
+            }
+          });
+          // remove focus so shortcut keys can work
+          event.target.blur();
+        }
+
+        scope.$watch('form.selected', function(newVal, oldVal) {
+          scope.iconClass = newVal ? 'minus' : 'plus';
+          scope.title     = newVal ? 'deselect' : 'select';
         });
-        // remove focus so shortcut keys can work
-        event.target.blur();
-      }
 
-      scope.$watch('form.selected', function(newVal, oldVal) {
-        scope.iconClass = newVal ? 'minus' : 'plus';
-        scope.title     = newVal ? 'deselect' : 'select';
-      });
+        element.bind('click', action);
 
-      element.bind('click', action);
-    },
-    template: '\
-      <input\
-        type="checkbox"\
-        class="postag-selector"\
-        ng-checked="form.selected">\
-      </input>\
-    '
-  };
-});
+        function checkMode() {
+          if (morph.canEdit() === 'editor') {
+            scope.editsAllowed = true;
+          }
+        }
+
+        // This event doesn't exist right now. Once
+        // we add support to change modes on the fly
+        // it will be broadcasted through $rootScope.
+        scope.$on('modeChanged', checkMode);
+        checkMode();
+      },
+      template: '\
+        <input\
+          ng-if="editsAllowed"\
+          type="checkbox"\
+          class="postag-selector"\
+          ng-checked="form.selected">\
+        </input>\
+      '
+    };
+  }
+]);
