d001e22a2a46d72d8d01ac722199d91829360f9f
LFDM <1986gh@gmail.com>
2014-12-15 11:55:46 -0500

    Update morphForm directive

--- app/js/arethusa.morph/directives/morph_form.js
+++ app/js/arethusa.morph/directives/morph_form.js
@@ -1,9 +1,17 @@
-'use strict';
-// unused right now
-angular.module('arethusa.morph').directive('morphForm', function () {
-  return {
-    restrict: 'E',
-    scope: true,
-    templateUrl: 'templates/morph_form.html'
-  };
-});
+"use strict";
+
+angular.module('arethusa.morph').directive('morphForm', [
+  'morph',
+  function(morph) {
+    return {
+      restrict: 'A',
+      scope: {
+        form: "=morphForm"
+      },
+      link: function(scope, element, attrs) {
+        scope.plugin = morph;
+      },
+      templateUrl: 'templates/arethusa.morph/morph_form.html'
+    };
+  }
+]);
