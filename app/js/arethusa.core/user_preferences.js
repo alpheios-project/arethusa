ce0072b9bf40eee643d69afeed17b4ab64b41858
LFDM <1986gh@gmail.com>
2014-12-16 12:34:11 +0100

    Add userPreferences documentation

--- app/js/arethusa.core/user_preferences.js
+++ app/js/arethusa.core/user_preferences.js
@@ -1,5 +1,15 @@
 "use strict";
 
+/**
+ * @ngdoc service
+ * @name arethusa.core.userPreferences
+ *
+ * @description
+ * Provides an API to get and set user preferences.
+ *
+ * @requires arethusa.core.globalSettings
+ * @requires arethusa.core.arethusaLocalStorage
+ */
 angular.module('arethusa.core').service('userPreferences', [
   'globalSettings',
   'arethusaLocalStorage',
@@ -21,12 +31,34 @@ angular.module('arethusa.core').service('userPreferences', [
       return arethusaLocalStorage.get(key(plugin + '.' + setting));
     }
 
+    /**
+     * @ngdoc function
+     * @name arethusa.core.userPreferences#set
+     * @methodOf arethusa.core.userPreferences
+     *
+     * @param {} name description
+     * @param {String} category Category under which the preference is stored,
+     *   often the name of a plugin
+     * @param {String} property Property name of the preference
+     * @param {*} value The new preference
+     */
     this.set = function(plugin, setting, value) {
       if (globalSettings.persistSettings) {
         set(plugin, setting, value);
       }
     };
 
+    /**
+     * @ngdoc function
+     * @name arethusa.core.userPreferences#get
+     * @methodOf arethusa.core.userPreferences
+     *
+     * @param {String} category Category under which the preference is stored,
+     *   often the name of a plugin
+     * @param {String} property Property name of the preference
+     *
+     * @returns {*} A user preference
+     */
     this.get = get;
   }
 ]);
