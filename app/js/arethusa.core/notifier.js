b9eaebd1afe3f85fb21ddd905d805f7056ccef7a
LFDM <1986gh@gmail.com>
2014-12-16 10:36:18 +0100

    Document notifier

--- app/js/arethusa.core/notifier.js
+++ app/js/arethusa.core/notifier.js
@@ -1,4 +1,27 @@
 'use strict';
+/**
+ * @ngdoc service
+ * @name arethusa.core.notifier
+ *
+ * @description
+ * Service to launch notifications.
+ *
+ * Uses [AngularJS-Toaster](http://github.com/jirikavi/AngularJS-Toaster) to bring
+ * notifications immediately to the screen.  Also stores past notifications (cf. {@link arethusa.core.notifier#properties_messages notifier.messages}).
+ *
+ *
+ * *Example Configuration*
+ * ```
+ * "notifier" : {
+ *   "disable" : false,
+ *   "maxMessages" : 10
+ * }
+ * ```
+ *
+ * @requires arethusa.core.configurator
+ * @requires toaster
+ * @requires $timeout
+ */
 angular.module('arethusa.core').service('notifier', [
   'configurator',
   'toaster',
@@ -8,11 +31,48 @@ angular.module('arethusa.core').service('notifier', [
 
     function configure() {
       self.conf = configurator.configurationFor('notifier');
+
+      /**
+       * @ngdoc property
+       * @name disable
+       * @propertyOf arethusa.core.notifier
+       *
+       * @description
+       * ***Configurable property***
+       *
+       * Set to true to disable all notifications.
+       */
       self.disable = self.conf.disable;
       self.duration = self.conf.duration || 10000;
+
+      /**
+       * @ngdoc property
+       * @name maxMessages
+       * @propertyOf arethusa.core.notifier
+       *
+       * @description
+       * ***Configurable property***
+       *
+       * Maximum number of messages stored within the service. Defaults to 15.
+       */
       self.maxMessages = self.conf.maxMessages || 15;
     }
 
+    /**
+     * @ngdoc property
+     * @name messages
+     * @propertyOf arethusa.core.notifier
+     *
+     * @description
+     * Array of messages objects. Acts stack-like when {@link arethusa.core.notifier#properties_maxMessages maxMessages} are reached.
+     *
+     * The objects come with the following properties:
+     *
+     * - *type*
+     * - *message*
+     * - *title*
+     * - *time*
+     */
     this.messages = [];
 
     function Message(type, message, title) {
@@ -30,10 +90,79 @@ angular.module('arethusa.core').service('notifier', [
       };
     }
 
-    var debouncer = {};
+    /**
+     * @ngdoc function
+     * @name arethusa.core.notifier#success
+     * @methodOf arethusa.core.notifier
+     *
+     * @description
+     * Generates a *success* notification.
+     *
+     * @param {String} message Message body of the notification
+     * @param {String} [title] Optional title/heading of the notification
+     * @param {Number} [blockDuration] Optional duration during which a subsequent
+     *   attempts to issue the exact same notification will supressed.
+     */
 
+    /**
+     * @ngdoc function
+     * @name arethusa.core.notifier#info
+     * @methodOf arethusa.core.notifier
+     *
+     * @description
+     * Generates an *info* notification.
+     *
+     * @param {String} message Message body of the notification
+     * @param {String} [title] Optional title/heading of the notification
+     * @param {Number} [blockDuration] Optional duration during which a subsequent
+     *   attempts to issue the exact same notification will supressed.
+     */
+
+    /**
+     * @ngdoc function
+     * @name arethusa.core.notifier#wait
+     * @methodOf arethusa.core.notifier
+     *
+     * @description
+     * Generates a *wait* notification.
+     *
+     * @param {String} message Message body of the notification
+     * @param {String} [title] Optional title/heading of the notification
+     * @param {Number} [blockDuration] Optional duration during which a subsequent
+     *   attempts to issue the exact same notification will supressed.
+     */
+
+    /**
+     * @ngdoc function
+     * @name arethusa.core.notifier#warning
+     * @methodOf arethusa.core.notifier
+     *
+     * @description
+     * Generates a *warning* notification.
+     *
+     * @param {String} message Message body of the notification
+     * @param {String} [title] Optional title/heading of the notification
+     * @param {Number} [blockDuration] Optional duration during which a subsequent
+     *   attempts to issue the exact same notification will supressed.
+     */
+
+    /**
+     * @ngdoc function
+     * @name arethusa.core.notifier#error
+     * @methodOf arethusa.core.notifier
+     *
+     * @description
+     * Generates an *error* notification.
+     *
+     * @param {String} message Message body of the notification
+     * @param {String} [title] Optional title/heading of the notification
+     * @param {Number} [blockDuration] Optional duration during which a subsequent
+     *   attempts to issue the exact same notification will supressed.
+     */
     var types = ['success', 'info', 'wait', 'warning', 'error'];
     angular.forEach(types, generate);
+
+    var debouncer = {};
 
     function messageKey(type, message, title) {
       return [type, message, title].join('||');
