4f7c31045cacbd63548c1c71db1bb2c64ce266ef
LFDM <1986gh@gmail.com>
2014-12-17 10:16:58 +0100

    Finish history service documentation

--- app/js/arethusa.history/history.js
+++ app/js/arethusa.history/history.js
@@ -5,8 +5,9 @@
  *
  * @description
  * Tracks and stores all changes happening through the
- * {@link arethusa.core.state state} API
+ * {@link arethusa.core.state state} API.
  *
+ * Provides means to undo and redo such changes.
  *
  *
  * @requires arethusa.core.configurator
@@ -88,6 +89,13 @@ angular.module('arethusa.history').service('history', [
      *
      * @description
      * Undoes the last event.
+     *
+     * Calls the current event's `undo` function and advances the
+     * {@link arethusa.history.history#properties_position pointer } to the
+     * {@link arethusa.history.history#properties_events history events}.
+     *
+     * Mind that no new event is added to the
+     * {@link arethusa.history.history#properties_events events} stack.
      */
     this.undo = function() {
       if (self.canUndo) {
@@ -105,6 +113,14 @@ angular.module('arethusa.history').service('history', [
      *
      * @description
      * Redoes the last event.
+     *
+     * Decreases the
+     * {@link arethusa.history.history#properties_position pointer} to the
+     * {@link arethusa.history.history#properties_events history events}
+     * and calls the `exec` function of the next event.
+     *
+     * Mind that no new event is added to the
+     * {@link arethusa.history.history#properties_events events} stack.
      */
     this.redo = function() {
       if (self.canRedo) {
@@ -153,6 +169,26 @@ angular.module('arethusa.history').service('history', [
      *
      * @description
      * Adds a new event to the history.
+     *
+     * Typically not called directly. The history service listen to
+     * all changes happening through the
+     * {@link arethusa.core.state#methods_change state.change} API
+     * as well as to the {@link arethusa.core.state#events_tokenRemoved tokenRemoved }
+     * and {@link arethusa.core.state#events_tokenAdded tokenAdded } events.
+     *
+     * Events pushed onto the {@link arethusa.history.history#properties_events events}
+     * stack need to conform with the API of
+     * {@link arethusa.core.StateChange StateChange} objects.
+     *
+     * When {@link arethusa.core.state state's} `batchMode` is active, all events will
+     * be collected and pushed to the stack as a **single** event, that can be undone
+     * and redone in a single step. This is achieved by wrapping all events in a custom
+     * object (cf. the constructor of `BatchEvent` in the source code), which is
+     * polymorphic to {@link arethusa.core.StateChange StateChange}, as it also
+     * provides an `undo` and an `exec` function.
+     *
+     * @param {*} event A {@link arethusa.core.StateChange StateChange} event
+     *   or a polymorphic equivalent.
      */
     var batchedEvent = new BatchEvent();
     this.saveEvent = function(event) {
