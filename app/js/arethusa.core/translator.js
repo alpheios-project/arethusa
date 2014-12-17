1a3ad682a7fe33b8bef9d90bd977cf41371e40e0
LFDM <1986gh@gmail.com>
2014-12-17 23:16:34 +0100

    Start documention of translator

--- app/js/arethusa.core/translator.js
+++ app/js/arethusa.core/translator.js
@@ -1,5 +1,53 @@
 "use strict";
 
+/**
+ * @ngdoc function
+ * @name arethusa.core.translator
+ *
+ * @description
+ * Flexible Wrapper around `angular-translate`'s `$translate` service.
+ *
+ * Requires {@link arethusa.core.translatorNullInterpolator nullInterpolator}
+ *  to be active.
+ *
+ * @param {String|Array|Object} id The prime job ob this argument is to define
+ *   the `translationId(s)` used with the `$translate` service. The argument
+ *   can either be
+ *
+ *   - a String to define a single id. When a string is given, the third
+ *     argument to this function is mandatory.
+ *   - an Array to define multiple ids at once. The ids are not only used
+ *     for lookup, but also as identifier to store the result in the Object
+ *     of the second argument to this function
+ *   - an Object used as map of `translationIds` and identifiers used to store
+ *     the results. The keys serve as lookup ids, while the values are used
+ *     as keys in the resulting object of this function.
+ * @param {Object|Function} [objOrFn={}] When this argument is an object or not
+ *   given at all - in which case an empty object will be used as default - it
+ *   will be the data container to store the results of the translation process
+ *   to.
+ *
+ *   When this argument is a Function, the
+ *   first argument should be a string, although other combinations are not
+ *   explicitly prohibited.
+ * @param {String} [propertyPath] Only effective when the first argument
+ *   is a string. Generally deprecated - better use the Object or Array options
+ *   to define this path.
+ *
+ * @returns {Object or Function} Returns the second argument of the function,
+ *   which is either the given `Object`, the given `Function` or the newly
+ *   generated `Object`, when no second argument was given.
+ *
+ *   The object is a dictionary where its keys are defined through the
+ *   `propertyPath` (either directly through the third argument or indirectly
+ *   through the first argument). Its values are {@link $interpolate} functions
+ *   that take a an optional context object. The string returned by this
+ *   function will be properly localized.
+ *
+ * @requires $rootScope
+ * @requires $translate
+ * @requires $interpolate
+ */
 angular.module('arethusa.core').factory('translator', [
   '$rootScope',
   '$translate',
@@ -11,11 +59,7 @@ angular.module('arethusa.core').factory('translator', [
         if (angular.isFunction(objOrFn)) {
           objOrFn(interpolate);
         } else {
-          arethusaUtil.setProperty(
-            objOrFn,
-            propertyPath,
-            interpolate
-          );
+          arethusaUtil.setProperty(objOrFn, propertyPath, interpolate);
         }
       });
     }
