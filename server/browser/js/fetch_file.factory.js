92b009807f44ea594a334d2005bf088b28d7441a
LFDM <1986gh@gmail.com>
2014-12-24 22:03:43 +0100

    Add fetchStats function in FetchFileFactory

--- server/browser/js/fetch_file.factory.js
+++ server/browser/js/fetch_file.factory.js
@@ -4,8 +4,18 @@ angular.module('fileBrowserApp').factory('FetchFileFactory', ['$http',
   function($http) {
     var _factory = {};
 
+    var apiPath = '/browse/api';
+
+    function getResource(path, file) {
+      return $http.get(apiPath + '/' + path + '?resource=' + encodeURIComponent(file));
+    }
+
     _factory.fetchFile = function(file) {
-      return $http.get('/browse/api/resource?resource=' + encodeURIComponent(file));
+      return getResource('resource', file);
+    };
+
+    _factory.fetchStats = function(file) {
+      return getResource('stats', file);
     };
 
     return _factory;
