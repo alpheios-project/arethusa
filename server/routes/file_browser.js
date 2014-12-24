fbdac336603e6b4aadf01303556addd4bfcb5eda
LFDM <1986gh@gmail.com>
2014-12-24 10:08:46 +0100

    Import fileBrowser api

---
+++ server/routes/file_browser.js
@@ -0,0 +1,75 @@
+'use strict';
+
+// heavily inspired by ...
+
+/* global __dirname */
+
+var express    = require('express'),
+    fs         = require('fs'),
+    path       = require('path'),
+    bodyParser = require('body-parser'),
+    router  = express.Router();
+
+var base = path.resolve(__dirname, '../../examples/data');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(express.static(path.join(__dirname, '..', 'browser')));
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+/* Serve the Tree */
+router.get('/api/tree', function(req, res) {
+  var _p;
+  if (req.query.id == 1) {
+    _p = base;
+    processReq(_p, res);
+
+  } else {
+    if (req.query.id) {
+      _p = req.query.id;
+      processReq(_p, res);
+    } else {
+      res.json(['No valid data found']);
+    }
+  }
+});
+
+/* Serve a Resource */
+router.get('/api/resource', function(req, res) {
+  res.send(fs.readFileSync(req.query.resource, 'UTF-8'));
+});
+
+function processReq(_p, res) {
+  var resp = [];
+  fs.readdir(_p, function(err, list) {
+    for (var i = list.length - 1; i >= 0; i--) {
+      resp.unshift(processNode(_p, list[i]));
+    }
+    res.json(resp);
+  });
+}
+
+function processNode(_p, f) {
+  var s = fs.statSync(path.join(_p, f));
+  return {
+    "id": path.join(_p, f),
+    "text": f,
+    "icon" : s.isDirectory() ? 'jstree-custom-folder' : 'jstree-custom-file',
+    "state": {
+      "opened": false,
+      "disabled": false,
+      "selected": false
+    },
+    "li_attr": {
+      "base": path.join(_p, f),
+      "isLeaf": !s.isDirectory()
+    },
+    "children": s.isDirectory()
+  };
+}
+
+module.exports = router;
