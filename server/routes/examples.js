d4ce40d86d9ca15084ad531a0cd7992f49891c45
LFDM <1986gh@gmail.com>
2014-12-24 23:07:58 +0100

    Add comments route to examples router

--- server/routes/examples.js
+++ server/routes/examples.js
@@ -49,9 +49,12 @@ function post(route, fileType) {
   return function(req, res) { writeFile(req, res, route, fileType); };
 }
 
+function docRoute(route) {
+  return '/' + route + '/:doc';
+}
+
 var exampleFileRoutes = {
   'treebanks': 'xml',
-  'comments': 'json',
   'translations': 'json',
   'tei' : 'xml',
   'oa' : 'json',
@@ -62,8 +65,31 @@ var exampleFileRoutes = {
 
 for (var route in exampleFileRoutes) {
   var fileType = exampleFileRoutes[route];
-  router.get( '/' + route + '/:doc', get(route, fileType));
-  router.post('/' + route + '/:doc', post(route, fileType));
+  router.get( docRoute(route), get(route, fileType));
+  router.post(docRoute(route), post(route, fileType));
 }
+
+router.get( docRoute('comments'), get('comments', 'json'));
+router.post(docRoute('comments'), function(req, res) {
+  var comment, comments;
+  var path = docPath(req, 'comments', 'json');
+  var now = new Date().toJSON();
+  req.on('data', function(data) { comment  = JSON.parse(data); });
+  req.on('end', function() {
+    fs.readFile(path, function(err, file) {
+      if (err) { comments = []; } else { comments = JSON.parse(file); }
+      var ids = comments.map(function(el) { return el.comment_id; }).sort();
+      delete comment.ids;
+      delete comment.sentenceId;
+      comment.comment_id = (ids[ids.length - 1] || 0) + 1;
+      comment.reason = 'general';
+      comment.created_at = now;
+      comment.updated_at = now;
+      comment.user = 'you';
+      comments.push(comment);
+      fs.writeFile(path, prettify(comments, req), function() { res.json(comment); });
+    });
+  });
+});
 
 module.exports = router;
