331c87b4e45269f81273c5ae5720406f89680f43
LFDM <1986gh@gmail.com>
2014-12-24 10:47:16 +0100

    Add fileBrowserApp

---
+++ server/browser/js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+window.app = angular.module('fileBrowserApp', [
+  'ngRoute',
+  'jsTree.directive'
+])
+.config([
+  '$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+    when('/', {
+      templateUrl: '/server/browser/templates/home.html',
+      controller: 'BrowserController'
+    }).
+    otherwise({
+      redirectTo: '/home'
+    });
+  }
+]);
+
