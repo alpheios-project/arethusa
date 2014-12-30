96035e151779d0ab0aa116d8fe0c329b9d6ba0d6
Bridget Almas <balmas@gmail.com>
2014-12-30 18:03:24 -0500

    documenting

--- app/js/arethusa.core/error_dialog.js
+++ app/js/arethusa.core/error_dialog.js
@@ -1,19 +1,22 @@
 "use strict";
 
+/**
+ * @ngdoc service
+ * @name arethusa.core.errorDialog
+ *
+ * @description
+ * A general purpose error dialog service
+ * which offers the user the option to email
+ * a stack trace.
+ *
+ * @requires $modal
+ * @requires $rootScope
+ */
 angular.module('arethusa.core').service('errorDialog', [
   '$modal',
   '$rootScope',
   function($modal, $rootScope) {
-    this.sendError = function(message) {
-      // this comes from the stacktrace-js library
-      var trace = printStackTrace();
-      // it's a little pointless to do this as a modal dialog really
-      // the idea was to send the stack trace on accept but it is a pain
-      // to get the coordination of the modal dialog with the uservoice widget right
-      // so the errordialog has the user voice widget embedded in it for now
-      this.ask(message,trace).then((function(){ }));
-    };
-    this.ask = function(message,trace) {
+    function ask(message,trace) {
       var scope = $rootScope.$new();
       scope.message = message;
       scope.trace = trace;
@@ -24,6 +27,31 @@ angular.module('arethusa.core').service('errorDialog', [
       }).result;
       promise['finally'] = function() { scope.$destroy(); };
       return promise;
+    }
+
+/***************************************************************************
+ *                            Public Functions                             *
+ ***************************************************************************/
+    /**
+     * @ngdoc function
+     * @name arethusa.core.errorDialog#sendError
+     * @methodOf arethusa.core.errorDialog
+     *
+     * @description
+     * Displays a dialog notification of an error and provides an option
+     * for the user to send an email with a stacktrace from the calling code
+     *
+     * @param {String} message a brief description of the message
+     *
+     */
+    this.sendError = function(message) {
+      // this comes from the stacktrace-js library
+      var trace = printStackTrace();
+      // it's a little pointless to do this as a modal dialog really
+      // the idea was to send the stack trace on accept but it is a pain
+      // to get the coordination of the modal dialog with the uservoice widget right
+      // so the errordialog has the user voice widget embedded in it for now
+      this.ask(message,trace).then((function(){ }));
     };
   }
 ]);
