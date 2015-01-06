d78684406cb9a9462fb227eb2dba8c79e2ea8540
Bridget Almas <balmas@gmail.com>
2015-01-06 12:10:32 -0500

    see comment on bb487adab35a9a9a4e11f225a235cc8b91e83ca3

--- app/js/arethusa.tei/directives/tei_parser.js
+++ app/js/arethusa.tei/directives/tei_parser.js
@@ -11,7 +11,12 @@ angular.module('arethusa.tei').directive('teiParser', [
       },
       link: function(scope, element, attrs) {
         scope.$watch('template', function(newVal, oldVal) {
-          if (newVal !== oldVal) {
+          // @balmas TODO - need to confirm when we should expect
+          // newVal to equal oldVal and when not
+          // as currently coded anyway the watch seems only to 
+          // execute when both are equal -- this came up when
+          // coding the text_context directive as well
+          if (newVal) {
             element.empty();
             init();
           }
