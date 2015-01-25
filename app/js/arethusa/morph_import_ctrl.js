e1909b426ac9940a95f24d6e654334ffdc95b0c7
LFDM <1986gh@gmail.com>
2015-01-25 23:07:44 +0100

    Don't add duplicate forms

--- app/js/arethusa/morph_import_ctrl.js
+++ app/js/arethusa/morph_import_ctrl.js
@@ -8,9 +8,11 @@ angular.module('arethusa').controller('MorphImportCtrl', [
   'configurator',
   'MORPH_IMPORTS',
   '$http',
+  '$injector',
   function($scope, plugins, latinAttrs, greekAttrs,
-           configurator, MORPH_IMPORTS, $http) {
-    var morph;
+           configurator, MORPH_IMPORTS, $http, $injector) {
+    var morph, localStorage;
+
     var attrs = {
       lat: latinAttrs.data,
       grc: greekAttrs.data
@@ -30,21 +32,21 @@ angular.module('arethusa').controller('MorphImportCtrl', [
 
     plugins.start(['morph']).then(function() {
       morph = plugins.get('morph');
+      localStorage = $injector.get('morphLocalStorage');
       $scope.ready = true;
     });
 
     $scope.files = MORPH_IMPORTS;
 
-    var userCache = {
-    };
-
     function loadForms(data, filter) {
+      resetStatus();
       var lines = data.split('\n');
       for (var i=0; i < lines.length; i++) {
         // Fields are organized like
         // Form       - Lemma   - Postag   - User
         // Caesaris   - Caesar  - .......  - ....
         var fields = lines[i].split(',');
+        var str = fields[0];
         var form = {
           lemma: fields[1],
           postag: fields[2]
@@ -52,14 +54,36 @@ angular.module('arethusa').controller('MorphImportCtrl', [
         };
 
         morph.postagToAttributes(form);
-        morph.addToLocalStorage(fields[0], form);
+        if (formNotDuplicate(str, form)) {
+          morph.addToLocalStorage(fields[0], form);
+          $scope.status.count += 1;
+        }
       }
     }
 
+    function formNotDuplicate(str, form) {
+      var forms;
+      localStorage.retriever.getData(str, function(f) { forms = f; });
+      if (forms) {
+        return !aU.find(forms, function(otherForm) {
+          return localStorage.comparator(form, otherForm);
+        });
+      }
+      return true;
+    }
+
+    function resetStatus() {
+      $scope.status = {
+        count: 0
+      };
+    }
+
     $scope.loadFile = function(fileObj) {
+      $scope.importStarted = true;
       $http.get(fileObj.route).then(function(res) {
         useLanguage(fileObj.language);
         loadForms(res.data);
+        $scope.importStarted = false;
       });
     };
   }
