7aee3e8aa38c3228a66bd386eef62f7b0bdf3528
LFDM <1986gh@gmail.com>
2015-01-25 20:33:18 +0100

    Add test data from Vanessa

--- app/js/arethusa/morph_imports.js
+++ app/js/arethusa/morph_imports.js
@@ -10,5 +10,10 @@ angular.module('arethusa').constant('MORPH_IMPORTS', [
     name: 'Greek Morphology',
     language: 'grc',
     route: '...'
+  },
+  {
+    name: 'Test Data from Vanessa',
+    language: 'grc',
+    route: 'http://www.perseids.org/morph/masterlocal.csv'
   }
 ]);
