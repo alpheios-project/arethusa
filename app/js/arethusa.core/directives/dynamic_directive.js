62d3f23f26f0cc59e3c18412bf8c06327501bbb7
LFDM <1986gh@gmail.com>
2015-02-01 15:25:43 +0100

    Document dynanicDirective
    Closes #596

--- app/js/arethusa.core/directives/dynamic_directive.js
+++ app/js/arethusa.core/directives/dynamic_directive.js
@@ -1,5 +1,49 @@
 "use strict";
 
+
+/**
+ * @ngdoc directive
+ * @name arethusa.core.directives:dynamicDirective
+ * @restrict A
+ * @scope
+ *
+ * @description
+ * Allows to dynamically add a self-contained directive
+ *
+ * A very dumb directive which just takes an input string and tries to
+ * append this input string as compiled directive.
+ *
+ * Mind that this is not very sophisticated yet (limited to our current
+ * use case):
+ *
+ * - The directive needs to be self contained, i.e. it cannot take any
+ *   arguments on its own.
+ * - It needs to be valid as attribute directive
+ * - The compilation is only done once - no watcher is placed on the
+ *   attribute, so if its value changes nothing happens.
+ *
+ * The directive tries to honor the tag the directive was placed on,
+ * two examples:
+ *
+ * ```html
+ * <div dynamic-directive="layout-setting"></div>
+ *
+ * <span dynamic-direcitve="layout-setting"></span>
+ * ```
+ *
+ * The first `layoutSetting` directive will be compiled on a `div` element,
+ * the second on a `span` element.
+ *
+ * ```html
+ * <div layout-setting></div>
+ *
+ * <span layout-setting></span>
+ * ```
+ *
+ * @param {string} dynamicDirective The name of the directive to be compiled
+ *
+ * @requires $compile
+ */
 angular.module('arethusa.core').directive('dynamicDirective', [
   '$compile',
   function($compile) {
