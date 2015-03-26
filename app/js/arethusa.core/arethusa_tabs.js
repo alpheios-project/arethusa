9632398d1a42cb61d3ff63e1709b10943195a38a
LFDM <1986gh@gmail.com>
2015-03-26 19:41:41 +0100

    Save tab conf in local storage

--- app/js/arethusa.core/arethusa_tabs.js
+++ app/js/arethusa.core/arethusa_tabs.js
@@ -3,35 +3,42 @@
 angular.module('arethusa.core').directive('arethusaTabs', [
   'plugins',
   'state',
+  'arethusaLocalStorage',
   '_',
   function(
     plugins,
     state,
+    arethusaLocalStorage,
     _
   ) {
+    var LOCAL_STORAGE_KEY = 'tabsConfiguration';
+
     return {
       restrict: 'A',
       scope: {
         tabs: "=arethusaTabs"
       },
       link: function(scope, element, attrs) {
-        var activeTabs = {};
+        var activeTabs = getFromLocalStorage();
 
         scope.plugins = plugins;
         scope.state = state;
-
-        scope.$watch('tabs', function(tabs) {
-          if (!tabs) return;
-          activateSettings();
-          doInitialActivation();
-          updateVisibleTabs();
-          //createDragAndDropList(tabs);
-        });
 
         scope.moveTab = moveTab;
         scope.toggleTab = toggleTab;
 
         scope.isActive = isActive;
+
+        scope.$watch('tabs', init);
+
+        function init(tabs) {
+          if (!tabs) return;
+
+          activateSettings();
+          doInitialActivation();
+          updateVisibleTabs();
+        }
+
 
         function activate(tab) {
           activeTabs[tab.name] = true;
@@ -57,6 +64,7 @@ angular.module('arethusa.core').directive('arethusaTabs', [
             activate(tab);
           }
           updateVisibleTabs();
+          setLocalStorage();
         }
 
         function activateSettings() {
@@ -65,6 +73,8 @@ angular.module('arethusa.core').directive('arethusaTabs', [
 
         function doInitialActivation() {
           _.forEach(scope.tabs, function(tab) {
+            // If a setting is already present, don't do anything,
+            // otherwise activate it.
             if (!angular.isDefined(isActive(tab))) {
               activate(tab);
             }
@@ -75,6 +85,14 @@ angular.module('arethusa.core').directive('arethusaTabs', [
           scope.visibleTabs =_.filter(scope.tabs, function(tab) {
             return isActive(tab);
           });
+        }
+
+        function getFromLocalStorage() {
+          return arethusaLocalStorage.get(LOCAL_STORAGE_KEY) || {};
+        }
+
+        function setLocalStorage() {
+          arethusaLocalStorage.set(LOCAL_STORAGE_KEY, activeTabs);
         }
       },
       templateUrl: 'templates/arethusa.core/arethusa_tabs.html'
