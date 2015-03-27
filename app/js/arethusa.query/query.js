0f25e763976a97e8ab97faec7d1b00a15a35f5e4
LFDM <1986gh@gmail.com>
2015-03-27 19:37:32 +0100

    query: Make it work

--- app/js/arethusa.query/query.js
+++ app/js/arethusa.query/query.js
@@ -7,53 +7,104 @@ angular.module('arethusa.query').service('query', [
     var self = this, retriever;
     this.name = 'query';
 
-    this.defaultConf = {
-      template: 'templates/arethusa.query/query.html',
-      queryLimit: 10
-    };
-
     this.currentPage = 0;
     this.query = query;
+    this.getNextPage = getNextPage;
+    this.getPage = getPage;
+
+    this.defaultConf = {
+      template: 'templates/arethusa.query/query.html',
+      queryLimit: 20
+    };
+
+    var props = [
+      'queryLimit'
+    ];
 
     function configure() {
-      configurator.getConfAndDelegate(self);
+      configurator.getConfAndDelegate(self, props);
       retriever = configurator.getRetriever(self.conf.retriever);
     }
 
     function query() {
       if (self.queryString) {
-        resetPaginator();
-        retrieveQuery();
+        retrieveQuery(1, function(reply) {
+          setQueryStats(reply);
+        });
       }
     }
 
     function getNextPage() {
-      self.currentPage++;
-      retrieveQuery();
+      getPage(self.queryStats.page + 1);
     }
 
-    function retrieveQuery() {
-      var params = getQueryParams(self.queryString);
-      retriever.get(params, function(results) {
-        console.log(results);
+    function getPreviousPage() {
+      getPage(self.queryStats.page - 1);
+    }
+
+    function getPage(i) {
+      setPage(i);
+      var offset = (i - 1) * self.queryLimit + 1;
+      retrieveQuery(offset);
+    }
+
+    function setPage(i) {
+      self.queryStats.page = i;
+    }
+
+    function retrieveQuery(offset, cb) {
+      var params = getQueryParams(self.queryString, offset);
+      doSpinning(function() {
+        return retriever.get(params, function(data) {
+          var reply = data.reply;
+          if (cb) { cb(reply); }
+          parseQueryData(reply);
+        });
       });
     }
 
-    function resetPaginator() {
-      self.currentPage = 1;
+    function parseQueryData(res) {
+      self.currentPage = res;
+    }
+
+    function setQueryStats(res) {
+      self.queryStats = {
+        total: res.count,
+        pages: createPages(Math.ceil(res.count / res.limit)),
+        page: 1
+      };
+    }
+
+    function createPages(count) {
+      // This is basically just an array of empty elements to ng-repeat
+      // over them.
+      var result = [];
+      for (var i = 0; i  < count; i ++) {
+        result.push(i + 1);
+      }
+      return result;
     }
 
     // Obtain the namespace of a active CTS urn and allow user to limit it
     // (to the language repository, the author, the work, the book etc)
     function getNamespace() {
+      return 'urn:cts:latinLit';
     }
 
     function getQueryParams(str, start) {
       return {
         query: str,
         limit: self.queryLimit,
-        start: self.currentPage
+        start: start,
+        urn: getNamespace()
       };
+    }
+
+    function doSpinning(cb) {
+      var promise = cb();
+      self.queryInProgress = true;
+      promise['finally'](function() { self.queryInProgress = false; });
+      return promise;
     }
 
 
