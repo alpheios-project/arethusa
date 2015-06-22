ae7f5b0811dc0559a4f2f7d01735daba9f33f7f6
Bridget Almas <balmas@gmail.com>
2015-06-22 10:12:43 -0400

    Merge branch 'Masoumeh-master-2' (excluding staging.json change)

--- app/js/arethusa.morph/morph.js
+++ app/js/arethusa.morph/morph.js
@@ -576,7 +576,7 @@ angular.module('arethusa.morph').service('morph', [
     }
 
     this.setState = function (id, form) {
-      self.updateGloss(id);
+      self.updateGloss(id,form);
       state.change(id, 'morphology', form, undoFn(id), preExecFn(id, form));
     };
 
