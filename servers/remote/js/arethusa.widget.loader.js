d1ad03891b8ed992b7578364f6c36e863b23d0bb
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-05-17 05:51:49 -0400

    Example for deps in loal/index.html

--- servers/remote/js/arethusa.widget.loader.js
+++ servers/remote/js/arethusa.widget.loader.js
@@ -12,18 +12,16 @@
 })(jQuery);
 
 function loadArethusaWidget (id,url,conf,deps) {
-    widget = {};
     $.when(
         $.getStylesheet(deps.css.arethusa), //arethusa.min.css
         $.getStylesheet(deps.css.foundation), //vendor/foundation-icons/foundation-icons.css
         $.getStylesheet(deps.css.font_awesome), //vendor/font-awesome-4.1.0/css/font-awesome.min.css
         $.getStylesheet(deps.css.colorpicker), //vendor/angular-foundation-colorpicker/css/colorpicker.css
-        $.getScript(deps.js.packages, function(){$.when(
+        $.getScript(deps.js.packages, function(){$.when( //arethusa_packages.min.js
             $.getScript(deps.js.arethusa) //arethusa.min.js
         ).then(function () {
-            widget = new Arethusa();
-            widget.on(id).from(url).with(conf);//.start();
-        })}) //arethusa_packages.min.js
+            var widget = new Arethusa();
+            widget.on(id).from(url).with(conf).start();
+        })})
     )
-    return widget;
 }
