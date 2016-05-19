ba640a59ff0e6be67b3752296cafcfcb374ee3f4
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-05-19 11:04:57 -0400

    Refactored arethusa.core/services/locator

--- app/js/arethusa.core/services/locator.js
+++ app/js/arethusa.core/services/locator.js
@@ -1,19 +1,39 @@
 'use strict';
 // deprecated for now
+// but still in use, no?
+
+/**
+ * This service wraps url parameters for routed views and
+ * provides a unified access to url- and manually-set parameters
+ */
 angular.module('arethusa.core').service('locator', [
   '$location',
   function ($location) {
     var noUrlParams;
     var manualParams = {};
 
+    /**
+     * Acess parameters by name
+     * @param name
+     * @returns {*}
+       */
     this.get = function(name) {
       return noUrlParams ? manualParams[name] : $location.search()[name];
     };
 
+    /**
+     * Toggle url- or manually-set parameters
+     * @param bool
+       */
     this.watchUrl = function(bool) {
       noUrlParams = !bool;
     };
 
+    /**
+     * Manually set parameters.
+     * @param paramOrParams
+     * @param value
+       */
     this.set = function(paramOrParams, value) {
       if (value) {
         manualParams[paramOrParams] = value;
