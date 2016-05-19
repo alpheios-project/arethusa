d1aa30262c6219556a383fb0a38663bed7a211f9
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-05-19 10:56:28 -0400

    Refactored arethusa.core/services/document_store

--- app/js/arethusa.core/services/document_store.js
+++ app/js/arethusa.core/services/document_store.js
@@ -1,21 +1,37 @@
 'use strict';
+
+/**
+ * Global hash table for Document instances
+ */
 angular.module('arethusa.core').service('documentStore', function () {
   var self = this;
 
+  /**
+   * Remove documents and configurations
+   */
   this.reset = function () {
     this.store = {};
     this.confs = {};
   };
 
-  function extractConf(doc) {
-    angular.extend(self.confs, doc.conf);
-  }
-
+  /**
+   * Store Document instance in associative array
+   * @param location
+   * @param doc
+     */
   this.addDocument = function (location, doc) {
     self.store[location] = doc;
     extractConf(doc);
+
+    function extractConf(doc) {
+      angular.extend(self.confs, doc.conf);
+    }
   };
 
+  /**
+   * Check if documentStore has stored configs from the documents
+   * @returns {boolean}
+     */
   this.hasAdditionalConfs = function() {
     return !angular.equals(self.confs, {});
   };
