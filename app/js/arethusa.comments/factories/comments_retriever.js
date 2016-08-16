3fb7ecf58efc6d89b640ad4bce02f7dfbf4c5d90
Bridget Almas <balmas@gmail.com>
2016-08-16 10:30:48 -0400

    fixes # 770

--- app/js/arethusa.comments/factories/comments_retriever.js
+++ app/js/arethusa.comments/factories/comments_retriever.js
@@ -115,7 +115,7 @@ angular.module('arethusa.comments').factory('CommentsRetriever', [
         } else {
           resource.get().then(function(res) {
             parseComments(res.data);
-            callback(comments[chunkId]);
+            callback(comments[chunkId] || []);
           });
           alreadyLoaded = true;
         }
