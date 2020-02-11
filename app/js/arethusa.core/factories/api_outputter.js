1634be0bcbeceb2463d6aa01580e9391db79eeb3
Bridget Almas <balmsa@gmail.com>
2020-02-11 13:52:13 -0500

    fleshing out the morph api

---
+++ app/js/arethusa.core/factories/api_outputter.js
@@ -0,0 +1,101 @@
+"use strict";
+
+angular.module('arethusa.core').factory('apiOutputter', [
+  function (uuid2) {
+    return function (uuid2) {
+      var self = this;
+
+      this._attributesToAlpheios = function(attributes,morph) {
+        var infl = {}
+        angular.forEach(attributes, function (value, key) {
+          value = morph.longAttributeValue(key,value)
+          if (key === 'pos') {
+            key = 'pofs'
+            if (value === 'verb' && attributes.mood === 'participle') {
+              value = 'verb participle'
+            } else if (value === 'adposition') {
+              value = 'preposition'
+            }
+          } 
+          if (key === 'degree') {
+            key = 'comp'
+          } 
+          if (key === 'tense' && value ==='plusquamperfect') {
+              value = 'pluperfect'
+          }
+          if (key === 'voice'  && value ==='medio-passive') {
+            value = 'mediopassive'
+          }
+          infl[key] = { $ : value }
+        });
+        return infl
+      }
+
+      this.outputMorph = function (token,lang,morph) {
+        // TODO Decide whether or not to bother with the whole OAC Annotation Wrapper or not
+        var resp = { 
+          RDF: { 
+            Annotation: { 
+              about: "urn:uuid:" + uuid2.newuuid(),
+              creator: {
+                Agent: { 
+                  about: ""
+                }
+              },
+              created: { 
+                $: ""
+              },
+              rights: {
+                $: ""
+              },
+              hasTarget: {
+                Description: {
+                  about: ""
+                }
+              }
+            } 
+          } 
+        }
+        var infl = token.morphology
+        if (infl) {
+          var entry = {}
+          var uuid = "urn:uuid:" + uuid2.newuuid()
+          if (angular.isDefined(infl.lemma)) { 
+            entry.dict = { hdwd: { lang: lang, $: infl.lemma } }
+          }
+          if (angular.isDefined(infl.postag)) {
+            entry.infl = this._attributesToAlpheios(infl.attributes,morph)
+          }
+          var glosses = []
+          if (angular.isDefined(infl.gloss)) {
+            glosses.push({ $: infl.gloss})
+          }
+          if (angular.isDefined(infl.alternateGloss)) { 
+            glosses.push({ $: infl.alternateGloss})
+          }
+          if (glosses.length > 1) {
+            entry.mean = glosses
+          } else if (glosses.length == 1) {
+            entry.mean = glosses[0]
+          }
+          if (angular.isDefined(infl.notes)) { 
+            entry.note = { $: infl.notes }
+          }
+          resp.RDF.Annotation.hasBody = {
+            resource: uuid,
+          }
+          resp.RDF.Annotation.Body = { 
+            about: uuid,
+            type: { 
+              resource: "cnt:ContentAsXML"
+            },
+            rest: { entry: entry } 
+          }
+        }
+        return  resp
+      };
+    };
+  }
+]);
+
+
--- app/js/arethusa.core/services/api.js
+++ app/js/arethusa.core/services/api.js
@@ -3,14 +3,40 @@
 angular.module('arethusa.core').service('api', [
   'state',
   'idHandler',
-  function(state,idHandler) {
+  'apiOutputter',
+  'documentStore',
+  'plugins',
+  'uuid2',
+  function(state,idHandler,apiOutputter,documentStore,plugins,uuid2) {
     var self = this;
+    this.outputter = new apiOutputter(uuid2);
+
+
+    var lazyMorph;
+    function morph() {
+      if (!lazyMorph) lazyMorph = plugins.get('morph');
+      return lazyMorph;
+    }
+
+    var lazyLang;
+    function lang() {
+      if (!lazyLang) {
+        var document = documentStore.store['treebank'];
+        if ( document !== undefined ) {
+          var doc = document.json.treebank || document.json.book;
+          if (doc) {
+            lazyLang = doc["_xml:lang"];
+          }
+        }
+      }
+      return lazyLang;
+    }
 
     this.getMorph = function(sentenceId,wordId) {
       if (!state.arethusaLoaded) {
         console.error("Api called before Arethusa was loaded")
       }
-      return state.getToken(idHandler.getId(wordId,sentenceId));
+      return this.outputter.outputMorph(state.getToken(idHandler.getId(wordId,sentenceId)),lang(),morph());
     };
 
   }
