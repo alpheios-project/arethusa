3cb7a8914bf00dd72cd53066e808d7dd431b779e
zfletch <zfletch2@gmail.com>
2020-02-28 16:29:37 -0500

    add d3 to deploy script

--- deploy_widget.sh
+++ deploy_widget.sh
@@ -4,12 +4,12 @@ then
   deploy_dir=dist_widget
 fi
 echo "Deploying to $deploy_dir"
-if [ ! -e $deploy_dir ]
-then
-  mkdir $deploy_dir
-  mkdir $deploy_dir/dist
-fi
+
+mkdir -p $deploy_dir/dist
+mkdir -p $deploy_dir/../vendor/d3-3.4.13/
+
 cp dist/arethusa.min.js $deploy_dir
 cp dist/arethusa.min.map $deploy_dir
 cp dist/arethusa_packages.min.js $deploy_dir/arethusa.packages.min.js
 cp dist/* $deploy_dir/dist
+cp vendor/d3-3.4.13/d3.min.js $deploy_dir/../vendor/d3-3.4.13/d3.min.js
